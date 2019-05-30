b92f7c1f75696751a6236e6cc6cb281639d5a885
root <willvix@outlook.com>
2019-05-30 17:28:48 -0300

    Criação do arquivo p/ o banco de dados especifíco.

---
+++ phpmyadmin.php
@@ -0,0 +1,9 @@
+<?php
+
+define("DB_SERVER", "localhost"); 
+define("DB_USERNAME", "id8414870_admin"); 
+define("DB_PASSWORD", "ProjetoAlfa@2019"); 
+define("DB_DATABASE", "gestaodesempenho"); 
+$conexao = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE); 
+
+// you could test connection eventually using a if and else conditional statement, // feel free to take out the code below once you see Connected! if ($db) { echo "Connected!"; } else { echo "Connection Failed"; } 
