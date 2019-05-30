1fb09fd3f4388de3407dcde9f3f5523b5d4cd575
root <willvix@outlook.com>
2019-05-30 15:37:01 -0300

    Select dos filtros e botão finalizado.

--- report.php
+++ report.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 include('verifica_login.php');
+$menuInicio="is-active";
 include('menu.php');
-$menuInicio="no-active";
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,52 +10,100 @@ $menuInicio="no-active";
 	<title>Gestão de Desempenho - Relatórios</title>
 </head>
 <body>
-<div style='margin-bottom: 20px;'>
+<div>
+	<br/>
 	<form id='form1' action='report.php' method='GET' >
-		<table cellpadding="2" cellspacing="0"  style='border-spacing: 30px 0; margin: 0 -30px'>								
-			<tr>
-				<td>
-					<label for='periodo'>Mês: </label>
-					</td>
-						<td>
-							<select name='periodo'>
-								<option value='proximo'><?php echo strftime('%h', strtotime("+1 months"))?></option>
-								<option selected='selected' value='atual'><?php echo strftime('%h')?></option>
-								<option value='ultimo'><?php echo strftime('%h', strtotime("-1 months"))?></option>
-								<option value='penultimo'><?php echo strftime('%h', strtotime("-2 months"))?></option>	
-							</select>
-						</td>
-					<td>
-					<label for='atividade'>Atividade: </label>
-				</td>
-				<td>
-					<select name='atividade'>
-						<option selected='selected' value='agrupado'>Agrupado</option>	
-						<option value='separado'>Separado</option>						
-				</td>
-					<td>
-						<label for='ordernacao'>Ordenação: </label>
-					</td>
-				<td>					
-					<select name='ordenacao'>
-						<option value='nome'>Nome</option>
-						<option value='alcancado desc, nome'>Alçancado</option>
-				</td>
-				<td>
-					<label for='meta'>Meta: </label>
-					<td>
-						<select name='meta'>
-							<option selected='selected'value=''>Ambos</option>
-							<option value='and b.alcancado>=100'>Atingida</option>
-							<option value='and b.alcancado<100'>Não atingida ;/</option>
-					</td>
-				</td>					
-				<td rowspan='2' style='vertical-align:top;padding-left:-30px'>
-					<input type='button' id='submitQuery' value='Filtrar' />
-				</td>
-			</tr>						
-		</table>
-	</form>
+		<div class="field is-horizontal">
+			<!--SELEÇÃO PERÍODO-->
+			<div class="field-label is-normal">
+				<label class="label" for="periodo">Período:</label>
+			</div>
+			<div class="field-body">
+			<div class="field is-grouped">							
+				<div class="control">
+					<div class="select">
+						<select name='periodo'>
+							<option value='proximo'><?php echo strftime('%h', strtotime("+1 months"))?></option>
+							<option selected='selected' value='atual'><?php echo strftime('%h')?></option>
+							<option value='ultimo'><?php echo strftime('%h', strtotime("-1 months"))?></option>
+							<option value='penultimo'><?php echo strftime('%h', strtotime("-2 months"))?></option>
+						</select>	
+					</div>
+				</div>
+			</div>
+			</div>
+			<!--SELEÇÃO ATIVIDADE-->
+			<div class="field-label is-normal">
+				<label class="label">Atividade:</label>
+			</div>
+			<div class="field-body">
+				<div class="field is-grouped">							
+					<div class="control">
+						<div class="select">
+							<select name="atividade">
+								<option selected="selected" value="agrupado">Agrupado</option>	
+								<option value="separado">Separado</option>	
+							</select>	
+						</div>
+					</div>					
+				</div>						
+			</div>
+			<!--SELEÇÃO ORDENAÇÃO-->
+			<div class="field-label is-normal">
+				<label for="odernacao" class="label">Ordenação:</label>
+			</div>
+			<div class="field-body">
+				<div class="field is-grouped">							
+					<div class="control">
+						<div class="select">
+							<select name="atividade">
+								<option selected="selected" value="agrupado">Nome</option>	
+								<option value="separado">Alcançado</option>	
+							</select>	
+						</div>
+					</div>					
+				</div>						
+			</div>
+			<!--SELEÇÃO TURNO-->
+			<div class="field-label is-normal">
+				<label for="odernacao" class="label">Turno:</label>
+			</div>
+			<div class="field-body">
+				<div class="field is-grouped">							
+					<div class="control">
+						<div class="select">
+							<select name="atividade">
+								<option selected="selected" value="agrupado">Todos</option>	
+								<option value="separado">Matutino</option>
+								<option value="separado">Vespetino</option>	
+							</select>	
+						</div>
+					</div>					
+				</div>						
+			</div>	
+			<!--SELEÇÃO META-->
+			<div class="field-label is-normal">
+				<label class="label" for="meta">Meta:</label>
+			</div>
+				<div class="field-body">
+					<div class="field is-grouped">							
+						<div class="control">
+							<div class="select">
+								<select name='meta'>
+									<option selected="selected"value="">Ambos</option>
+									<option value="and b.alcancado>=100">Atingida</option>
+									<option value="and b.alcancado<100">Não atingida ;/</option>
+								</select>	
+							</div>
+						</div>
+					<div class="control">
+						<!--<button type="submit" class="button is-primary">Filtrar</button>-->
+						<input type="submit" class="button is-primary" id="submitQuery" value="Filtrar"/>
+					</div>
+				</div>						
+			</div>
+		</div>
+	</form>		
 </div>
 </body>
 </html>
