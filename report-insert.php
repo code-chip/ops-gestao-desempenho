23049a6299238aa243c4455fc437ccca6fcb1efb
root <willvix@outlook.com>
2019-06-11 15:19:40 -0300

    Ajuste no layout.

--- report-insert.php
+++ report-insert.php
@@ -24,6 +24,8 @@ $totalAlcancado=0;
 <html>
 <head>
 	<title>Gestão de Desempenho - Inserir Desempenho</title>
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 <body>
 	<br/>
