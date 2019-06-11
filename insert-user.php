6339f5dff4076d47decd9047f5f25c5472765ea0
root <willvix@outlook.com>
2019-06-11 15:18:59 -0300

    Correções no formulário.

--- insert-user.php
+++ insert-user.php
@@ -18,6 +18,30 @@ $totalAlcancado=0;
 <html>
 <head>
 	<title>Gestão de Desempenho - Cadastrar Usuário</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <script language="Javascript">
+		function validacaoEmail(field) {
+		usuario = field.value.substring(0, field.value.indexOf("@"));
+		dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+		if ((usuario.length >=1) &&
+		    (dominio.length >=3) && 
+		    (usuario.search("@")==-1) && 
+		    (dominio.search("@")==-1) &&
+		    (usuario.search(" ")==-1) && 
+		    (dominio.search(" ")==-1) &&
+		    (dominio.search(".")!=-1) &&      
+		    (dominio.indexOf(".") >=1)&& 
+		    (dominio.lastIndexOf(".") < dominio.length - 1)) {
+		document.getElementById("msgemail").innerHTML="E-mail válido";
+		//alert("email valido");
+		}
+		else{
+		document.getElementById("msgemail").innerHTML="<font color='red'>Email inválido </font>";
+		alert("E-mail invalido");
+		}
+		}
+	</script>
 </head>
 <body>
 <div>	
@@ -35,25 +59,52 @@ $totalAlcancado=0;
 		</div>
 		<div class="field">
 			<label class="label" for="numberInput">Login</label>
-				<div class="control">
-					<input type="text" class="input" id="textInput" placeholder="ana.clara">
-				</div>
+				<div class="control has-icons-left has-icons-right">
+					<input name="login" class="input" type="text" id="textInput" placeholder="ana.clara">				
+					<span class="icon is-small is-left">
+				      	<i class="fas fa-user"></i>
+				    </span>
+				    <span class="icon is-small is-right">
+				      	<i class="fas fa-check"></i>
+				    </span>
+				</div>    
 			<p class="help">Use de preferência texto caixa baixa, primeiro e último nome p/ manter a padronização de nome de login.</p>
 		</div>
+		<!--<div class="field">
+		  	<label class="label">Username</label>
+		  		<div class="control has-icons-left has-icons-right">
+		    		<input class="input is-success" type="text" placeholder="Text input" value="bulma">
+		    		<span class="icon is-small is-left">
+		      			<i class="fas fa-user"></i>
+		    		</span>
+		    		<span class="icon is-small is-right">
+		      			<i class="fas fa-check"></i>
+		    		</span>
+		  		</div>
+		  	<p class="help is-success">This username is available</p>
+		</div>-->
 		<div class="field">
 			<label class="label" for="numberInput">Senha</label>
 				<div class="control">
-					<input type="password" class="input" id="textInput" placeholder="">
+					<input name="senha" type="password" class="input" id="textInput" placeholder="">
 				</div>			
 		</div>
 		<div class="field">
-			<label class="label" for="numberInput">E-mail</label>
-				<div class="control">
-					<input type="text" class="input" id="textInput" placeholder="anaclara@gmail.com">
-				</div>			
+		  	<label class="label">Email</label>
+		  	<div class="control has-icons-left has-icons-right">
+		    	<input name="email" class="input is-danger" type="text" placeholder="anaclara@gmail.com" value="" onblur="validacaoEmail(form1.email)"  maxlength="60" size='65'>
+		    	<span class="icon is-small is-left">
+		      		<i class="fas fa-envelope"></i>
+		    	</span>
+		    	<span class="icon is-small is-right">
+		      		<i class="fas fa-exclamation-triangle"></i>
+		    	</span>		    	
+		    	<div id="msgemail"></div>
+		  	</div>
+		  	<p class="help is-danger">E-mail inválido</p>
 		</div>
 		<!--DIVISÃO EM HORIZONTAL-->
-			<div class="field is-horizontal">
+		<div class="field is-horizontal">
 			<!--SELEÇÃO CARGO-->
 			<div class="field-label is-normal">
 				<label class="label" for="periodo">Cargo:</label>
@@ -116,6 +167,64 @@ $totalAlcancado=0;
 					</div>					
 				</div>						
 			</div>
+			<!--SELEÇÃO SETOR-->
+			<div class="field-label is-normal">
+				<label class="label">Setor</label>
+			</div>
+			<div class="field-body">
+				<div class="field is-grouped">							
+					<div class="control">
+						<div class="select">
+							<select name="setor">
+								<option selected="selected" value="">Operação</option>	
+								<option value="matutino">Administrativo</option>
+								<option value="vespetino">Recebimento</option>
+								<option value="comercial">Expedição</option>
+								<option value="comercial">Devolução</option>
+								<option value="comercial">Avarias</option>
+							</select>	
+						</div>
+					</div>					
+				</div>						
+			</div>		
+		</div><!--FINAL DIVISÃO EM HORIZONTAL-->	
+		<!--DIVISÃO EM HORIZONTAL 2-->
+		<div class="field is-horizontal">			
+			<!--CAMPO MATRICULA-->
+			<div class="field-label is-normal">
+				<label class="label" for="periodo">Matricula:</label>
+			</div>
+			<div class="field-body">
+				<div class="field is-grouped">							
+					<div class="control">
+						<input type="text" class="input" id="textInput" placeholder="635">
+					</div>
+				</div>
+			</div>
+			<!--CAMPO EFETIVADO-->
+			<div class="field-label is-normal">
+				<label class="label" for="periodo">Efetivação:</label>
+			</div>
+			<div class="field-body">
+				<div class="field is-grouped">							
+					<div class="control">
+						<input type="text" class="input" id="textInput" placeholder="2019-05-29">
+					</div>
+				</div>
+			</div>
+			<!--CAMPO DATA E HORA DE CADASTRO-->
+			<fieldset disabled>
+				<div class="field-label is-normal">
+					<label class="label" for="periodo">Cadastro:</label>
+				</div>
+				<div class="field-body">
+					<div class="field is-grouped">							
+						<div class="control">
+							<input type="text" class="input" id="textInput" placeholder=""><?php echo date('Y-m-d H:i:s')?></input>
+						</div>
+					</div>
+				</div>
+			</fieldset>
 			<!--DIVISÃO SITUAÇÃO-->
 			<div class="field-label is-normal">
 				<label class="label">Situação</label>
@@ -134,8 +243,7 @@ $totalAlcancado=0;
 					</div>					
 				</div>						
 			</div>
-			<!--FINAL DIVISÃO EM HORIZONTAL-->
-		</div>	
+		</div><!--FINAL DIVISÃO EM HORIZONTAL 2-->	
 		<!---->		
 		<div class="field">
 			<label class="label" for="numberInput">Observação</label>
@@ -150,6 +258,21 @@ $totalAlcancado=0;
 			</div>				
 		</form>
 		<!--FINAL DO FORMULÁRIO-->
+		<!--<form name="f1">
+			<h3> Validação de E-mail com JavaScript</h3>
+			<hr color='gray'>
+			<table>
+				<tr>
+				<td>E-mail:
+					<input type="text" name="email" onblur="validacaoEmail(f1.email)"  maxlength="60" size='65'>
+				</td>
+				<td>
+				<div id="msgemail"></div>
+				</td>
+				</tr>
+			</table>
+			<hr color='gray'>
+		</form>-->
 	</main>	
 </div>
 </section>
