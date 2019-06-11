5dd18ab5e5fa8102cc28389584f986a54c108a66
root <willvix@outlook.com>
2019-06-11 15:18:28 -0300

    Adição do javascript no head.

--- index.php
+++ index.php
@@ -13,7 +13,9 @@ session_start();
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/personal.css" />
     <link rel="stylesheet" href="css/bulma.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/login.css">	 
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <!---<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">-->
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>	 
 </head>
 
 <body>
