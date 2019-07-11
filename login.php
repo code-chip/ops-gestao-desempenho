cc4045198923b7e3d53c7e9ac5a9d63bc11c64d3
root <willvix@outlook.com>
2019-07-11 19:58:25 -0300

    Apontamento para o banco de dados gd e atualização da query p/ login e senha.

--- login.php
+++ login.php
@@ -6,11 +6,12 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])){
 	exit();
 }
 //funcao p/ proteção de ataque sqlinject
-$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
-$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
-$query="select * from id8414870_projetoalfa.USUARIO where login='".$_POST['usuario']."' and senha=md5(".$_POST['senha'].")";
+$usuario = mysqli_real_escape_string($phpmyadmin, $_POST['usuario']);
+$senha = mysqli_real_escape_string($phpmyadmin, $_POST['senha']);
+//$query="select * from id8414870_projetoalfa.USUARIO where login='".$_POST['usuario']."' and senha=md5(".$_POST['senha'].")";
+$query="SELECT * FROM gd.USUARIO WHERE LOGIN='".$_POST['usuario']."' AND SENHA=md5(".$_POST['senha'].")";
 
-$result = mysqli_query($conexao, $query);
+$result = mysqli_query($phpmyadmin, $query);
  
 $row = mysqli_num_rows($result);
 //echo $row;
