5ad8b23f993e820f129b12c0269a61d503d331cb
root <willvix@outlook.com>
2019-08-01 12:34:14 -0300

    Adição de apontamento da referência dos arquivos nos itens de menu Consultar e Atualizar.

--- menu.php
+++ menu.php
@@ -40,9 +40,9 @@ $menuConfiguracoes;
 					<div href="#" class="navbar-item has-dropdown is-hoverable">
 						<a class="navbar-link <?php echo $menuDesempenho?>">Desempenho</a>					
 						<div class="navbar-dropdown">
-							<a href="report-private.php" class="navbar-item">Consultar</a>
+							<a href="report-query.php" class="navbar-item">Consultar</a>
 							<a href="report-insert.php" class="navbar-item">Inserir</a>
-							<a class="navbar-item">Atualizar</a>
+							<a href="report-update.php" class="navbar-item">Atualizar</a>
 							<a class="navbar-item">Remover</a>
 						</div>
 					</div>
