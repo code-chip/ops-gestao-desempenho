4f5ff0c549b6c3e8f2e4f282f13c80801de9b612
root <willvix@outlook.com>
2019-08-03 19:16:04 -0300

    Adição da captura do ID do usuário ao realizar o login.

--- login.php
+++ login.php
@@ -9,10 +9,11 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])){
 $usuario = mysqli_real_escape_string($phpmyadmin, $_POST['usuario']);
 $senha = mysqli_real_escape_string($phpmyadmin, $_POST['senha']);
 //$query="select * from id8414870_projetoalfa.USUARIO where login='".$_POST['usuario']."' and senha=md5(".$_POST['senha'].")";
-$query="SELECT * FROM gd.USUARIO WHERE LOGIN='".$_POST['usuario']."' AND SENHA=md5(".$_POST['senha'].") AND SITUACAO<>'Desligado'";
+$query="SELECT ID, NOME, SENHA FROM gd.USUARIO WHERE LOGIN='".$_POST['usuario']."' AND SENHA=md5(".$_POST['senha'].") AND SITUACAO<>'Desligado'";
 
 $result = mysqli_query($phpmyadmin, $query);
- 
+$cnx= $result->fetch_array();
+$_SESSION["loggedInUser"]=$cnx["ID"];
 $row = mysqli_num_rows($result);
 //echo $row;
 if($row == 1){
