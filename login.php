111dadef79fbe82923ae3dc459b62e4a9f6f3bbe
root <willvix@outlook.com>
2019-08-04 15:43:13 -0300

    Adição de captura de informações de número de acessos e último login.

--- login.php
+++ login.php
@@ -9,14 +9,18 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])){
 $usuario = mysqli_real_escape_string($phpmyadmin, $_POST['usuario']);
 $senha = mysqli_real_escape_string($phpmyadmin, $_POST['senha']);
 //$query="select * from id8414870_projetoalfa.USUARIO where login='".$_POST['usuario']."' and senha=md5(".$_POST['senha'].")";
-$query="SELECT ID, NOME, SENHA FROM gd.USUARIO WHERE LOGIN='".$_POST['usuario']."' AND SENHA=md5(".$_POST['senha'].") AND SITUACAO<>'Desligado'";
+$query="SELECT ID, NOME, SENHA, ACESSO FROM gd.USUARIO WHERE LOGIN='".$_POST['usuario']."' AND SENHA=md5(".$_POST['senha'].") AND SITUACAO<>'Desligado'";
 
 $result = mysqli_query($phpmyadmin, $query);
 $cnx= $result->fetch_array();
 $_SESSION["loggedInUser"]=$cnx["ID"];
+$_SESSION["nameUser"]=$cnx["NOME"];
+$count=1+$cnx["ACESSO"];
 $row = mysqli_num_rows($result);
 //echo $row;
 if($row == 1){
+	$logou="UPDATE gd.USUARIO SET ULTIMO_LOGIN='".date('Y-m-d H:i:s')."', ACESSO=".$count." WHERE ID=".$cnx["ID"].";";
+	$cx=mysqli_query($phpmyadmin, $logou);
 	$_SESSION['usuario'] = $_POST['usuario'];
 	header('Location: home.php');
 	exit();
@@ -25,5 +29,4 @@ else{
 	$_SESSION['nao_autenticado']=true;
 	header('Location: index.php');
 }
-
 ?>
