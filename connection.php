13633156a71a5e83871faa2f9827f4e8339b1442
root <willvix@outlook.com>
2019-08-06 21:22:33 -0300

    Teste com função de exibição de erros.

--- connection.php
+++ connection.php
@@ -9,6 +9,4 @@ define("DB_SERVER", "localhost");
 define("DB_USERNAME", "id8414870_admin"); 
 define("DB_PASSWORD", "ProjetoAlfa@2019"); 
 define("DB_DATABASE", "gestaodesempenho"); 
-$phpmyadmin= new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE); 
-
-// you could test connection eventually using a if and else conditional statement, // feel free to take out the code below once you see Connected! if ($db) { echo "Connected!"; } else { echo "Connection Failed"; } 
+$phpmyadmin= new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
