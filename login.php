a0b3a1c9c7c62789d4605c6becb6429947e6a9ac
root <willvix@outlook.com>
2019-08-13 08:30:14 -0300

    Correção de bug no tratamento de usuário e senha errado e adição da variável permissão.

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-//curl_errno();
 include('connection.php');
 if(empty($_POST['usuario']) || empty($_POST['senha'])){
 	header('Location: index.php');
@@ -9,17 +8,14 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])){
 //funcao p/ proteção de ataque sqlinject
 $usuario = mysqli_real_escape_string($phpmyadmin, $_POST['usuario']);
 $senha = mysqli_real_escape_string($phpmyadmin, $_POST['senha']);
-//$query="select * from id8414870_projetoalfa.USUARIO where login='".$_POST['usuario']."' and senha=md5(".$_POST['senha'].")";
-$query="SELECT ID, NOME, SENHA FROM USUARIO WHERE LOGIN='".$_POST['usuario']."' AND SENHA=md5(".$_POST['senha'].") AND SITUACAO<>'Desligado'";
-
+$query="SELECT ID, NOME, SENHA, PERMISSAO_ID FROM USUARIO WHERE LOGIN='".$_POST['usuario']."' AND SENHA=md5(".$_POST['senha'].") AND SITUACAO<>'Desligado'";
 $result = mysqli_query($phpmyadmin, $query);
-$cnx= $result->fetch_array();
-$_SESSION["loggedInUser"]=$cnx["ID"];
-$_SESSION["nameUser"]=$cnx["NOME"];
-//$count=1+$cnx["ACESSO"];
 $row = mysqli_num_rows($result);
-//echo $row;
 if($row == 1){//ADICIONAR INFORMAÇÕES DE ACESSO NA TABELA.
+	$cnx= $result->fetch_array();
+	$_SESSION["loggedInUser"]=$cnx["ID"];
+	$_SESSION["nameUser"]=$cnx["NOME"];
+	$_SESSION["permissao"]=$cnx["PERMISSAO_ID"];
 	$countCheck="SELECT MAX(ACESSO_TOTAL) AS ACESSO_TOTAL FROM ACESSO WHERE USUARIO_ID=".$cnx["ID"].";";
 	$monthCheck="SELECT ANO_MES, ACESSO FROM ACESSO WHERE USUARIO_ID=".$cnx["ID"]." AND ANO_MES='".date('Y-m')."';";
 	$check=mysqli_query($phpmyadmin, $countCheck);
@@ -27,7 +23,7 @@ if($row == 1){//ADICIONAR INFORMAÇÕES DE ACESSO NA TABELA.
 	$cc=$check->fetch_array();
 	$cm=$check2->fetch_array();	
 	$acessoTotal=$cc["ACESSO_TOTAL"]+1;		
-	if(mysqli_num_rows($check2)!=0){//SE JÁ HOUVE REGISTRO NO MÊS, O MESMO REGISTRO SERÁ ATUALIZADO.
+	if(mysqli_num_rows($check2)!=0){//SE JÁ HOUVE REGISTRO DE ACESSO NO MÊS, O MESMO REGISTRO SERÁ ATUALIZADO.
 		$acesso=$cm["ACESSO"]+1;
 		$upAcesso="UPDATE ACESSO SET ULTIMO_LOGIN='".date('Y-m-d H:i:s')."', ACESSO=".$acesso.", ACESSO_TOTAL=".$acessoTotal." WHERE USUARIO_ID=".$cnx["ID"]." AND ANO_MES='".date('Y-m')."';";
 		$up=mysqli_query($phpmyadmin, $upAcesso);		
