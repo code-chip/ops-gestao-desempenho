420bdb433a039601cc8ada3f58cf00767c47916c
root <willvix@outlook.com>
2019-08-23 17:06:03 -0300

    Criação do arquivo e estrutura base.

---
+++ manager-remove.php
@@ -0,0 +1,32 @@
+<?php 
+session_start();
+include('login-check.php');
+$menuConfiguracao="is-active";
+include('menu.php');
+header('Content-Type: text/html; charset=UTF-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-widht, initial-scale=1">
+	<title>Gestão de Desempenho - Remover Gestor</title>
+	<link rel="shortcut icon" href="img\favicon_codechip.ico"/>
+	<link rel="stylesheet" href="css/login.css" />
+	<!--<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">-->
+	<link rel="stylesheet" href="css/bulma.min.css"/>
+	<script defer scr="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+<body>
+	<div class="hero is-fullheight is-primary has-background">
+	  	<img alt="Fill Murray" class="hero-background is-transparent" src="img/em-construcao3.jpg" />
+	  	<div class="hero-body">
+	    	<div class="container">
+		     	<h1 class="title">
+		        	Novidades em breve...
+		      	</h1>
+	    	</div>
+	  	</div>
+	</div>
+</body>
+</html>
