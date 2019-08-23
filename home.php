d701d4cdd7edab4c0e950d21632cf028e45cee14
root <willvix@outlook.com>
2019-08-23 09:24:54 -0300

    Adição de código para exibir imagem de fundo.

--- home.php
+++ home.php
@@ -4,6 +4,8 @@ include('login-check.php');
 $menuInicio="is-active";
 include('menu.php');
 header('Content-Type: text/html; charset=UTF-8');
+$n=rand(1,23);
+$img="img/data-science".$n.".jpg";
 //print_r($_SESSION);exit();
 ?>
 <!DOCTYPE html>
@@ -13,16 +15,24 @@ header('Content-Type: text/html; charset=UTF-8');
 	<meta name="viewport" content="width=device-widht, initial-scale=1">
 	<title>Gestão de Desempenho</title>
 	<link rel="shortcut icon" href="img\favicon_codechip.ico"/>
-	<link rel="stylesheet" href="css/personal.css" />
+	<link rel="stylesheet" href="css/login.css" />
 	<!--<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">-->
 	<link rel="stylesheet" href="css/bulma.min.css"/>
 	<script defer scr="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
-	<section>
-	<div class="container">
-		<div class="has-text-left is-size-4">Bem vindo <?php echo $_SESSION["nameUser"]; ?></div>
-	</div>		
-	</section>
 <body>
-	
-</body>	
+	<div class="hero is-fullheight is-primary has-background">
+	  	<img alt="Fill Murray" class="hero-background is-transparent" src="<?php echo $img;?>" />
+	  	<div class="hero-body">
+	    	<div class="container">
+		     	<h1 class="title">
+		        	Bem vindo
+		      	</h1>
+		      	<h3 class="subtitle">
+		        	<?php echo $_SESSION["nameUser"]; ?>
+		      	</h3>
+	    	</div>
+	  	</div>
+	</div>
+</body>
+</html>
