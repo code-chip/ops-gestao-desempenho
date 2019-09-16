705bcd12de4f72c3590a788e1628b9e788ab0c1a
root <willvix@outlook.com>
2019-09-16 08:26:19 -0300

    Ajuste na query p/ ordenar por nome.

--- report-insert.php
+++ report-insert.php
@@ -117,7 +117,7 @@ $totalAlcancado=0;
 </div>
 <?php
 if( $turno != "" && $setor != ""){	
-	$query="SELECT ID, NOME FROM USUARIO WHERE TURNO_ID=".$turno." AND SETOR_ID=".$setor." AND SITUACAO='Ativo'";
+	$query="SELECT ID, NOME FROM USUARIO WHERE TURNO_ID=".$turno." AND SETOR_ID=".$setor." AND SITUACAO='Ativo' ORDER BY NOME";
 	$ajusteBD="set global sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';";
 	$ajustes= mysqli_query($phpmyadmin, $ajusteBD);
 	$x=0;
