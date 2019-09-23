0d6afd4f80835a2dafc03bd54b62721d6797e19f
root <willvix@outlook.com>
2019-09-23 17:26:09 -0300

    Adição de campo.

--- contact.php
+++ contact.php
@@ -10,11 +10,6 @@ include('menu.php');
 	<meta charset="UTF-8">	
 	<meta name="viewport" content="width=device-widht, initial-scale=1">
 	<title>Gestão de Desempenho - Reportar Bug</title>
-	<link rel="shortcut icon" href="img\favicon_codechip.ico"/>
-	<link rel="stylesheet" href="css/login.css" />
-	<!--<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">-->
-	<link rel="stylesheet" href="css/bulma.min.css"/>
-	<script defer scr="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
 <body>
 	<div>	
@@ -24,73 +19,88 @@ include('menu.php');
 		<hr>
 	<main>
 	<form id="form1" action="contact.php" method="POST">
-		<!-- Horizontal form -->
-				<div class="field is-horizontal">
-					<div class="field-label is-normal">
-						<label class="label">Nome</label>
-					</div>
-					<div class="field-body">
-						<div class="field">
-							<div class="control">
-								<input type="text" class="input" id="textInput" placeholder="Ana Clara">
-							</div>
-						</div>
-					</div>
-					<div class="field-label is-normal">
-						<label class="label">Email</label>
-					</div>
-					<div class="field-body">
-						<div class="field">
-							<div class="control">
-								<input type="text" class="input" id="textInput" placeholder="anaclara@gmail.com">
-							</div>
-						</div>
+		<div class="field is-horizontal">
+			<div class="field-label is-normal">
+				<label class="label">Nome</label>
+			</div>
+			<div class="field-body">
+				<div class="field">
+					<div class="control">
+						<input type="text" class="input" id="textInput" placeholder="Ana Clara">
 					</div>
 				</div>
-				<div class="field is-horizontal">
-					<div class="field-label is-normal">
-						<label class="label">Módulo</label>
+			</div>
+			<div class="field-label is-normal">
+				<label class="label">E-mail</label>
+			</div>
+			<div class="field-body">
+				<div class="field">
+					<div class="control">
+						<input type="text" class="input" id="textInput" placeholder="anaclara@gmail.com">
 					</div>
-					<div class="field-body">
-						<div class="field">							
-							<div class="control">
-								<div class="select">
-									<select name="situacao">
+				</div>
+			</div>
+		</div>
+		<div class="field is-horizontal">
+			<div class="field-label is-normal">
+				<label class="label">Módulo</label>
+			</div>
+			<div class="field-body">
+				<div class="field">							
+					<div class="control">
+						<div class="select">
+							<select name="situacao">
 								<option selected="selected" value="Ativo">Dashboard</option>	
 								<option value="Férias">Meta</option>
 								<option value="Licença">Desempenho</option>
 								<option value="Desligado">Relatório</option>
 								<option value="Desligado">Configurações</option>
 							</select>	
-								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="field-label is-normal">
+				<label class="label">Prioridade</label>
+			</div>
+			<div class="field-body">
+				<div class="field">							
+					<div class="control">
+						<div class="select">
+							<select name="situacao">
+								<option selected="selected" value="Ativo">Baixa</option>	
+								<option value="Férias">Normal</option>
+								<option value="Licença">Alta</option>
+								<option value="Desligado">Urgente</option>
+							</select>	
 							</div>
 						</div>
 					</div>
 				</div>
-				<div class="field is-horizontal">
-					<div class="field-label is-normal">
-						<label class="label">Mensagem</label>
-					</div>
-					<div class="field-body">
-						<div class="field">
-							<div class="control">
-								<textarea class="textarea" placeholder="Relate o problema apresentado..."></textarea>
-							</div>
+			</div>
+			<div class="field is-horizontal">
+				<div class="field-label is-normal">
+					<label class="label">Mensagem</label>
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<textarea class="textarea" placeholder="Relate o problema apresentado..."></textarea>
 						</div>
 					</div>
 				</div>
-				<div class="field is-horizontal">
-					<div class="field-label">						
-					</div>
-					<div class="field-body">
-						<div class="field">
-							<div class="control">
-								<button type="submit" class="button is-primary">Enviar</button>
-							</div>
+			</div>
+			<div class="field is-horizontal">
+				<div class="field-label">						
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<button type="submit" class="button is-primary">Enviar</button>
 						</div>
 					</div>
-				</div>		
-		<!---->				
+				</div>
+			</div>			
 		</form>
 	</main>	
 </div>
