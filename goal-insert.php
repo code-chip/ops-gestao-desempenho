ca487d68c3bfe1bb7774c6d5d09f5b9f6abef981
Lwcyano <lwcyano.will@evino.com.br>
2019-10-17 18:30:01 -0300

    Correções na funcionalidade.

--- goal-insert.php
+++ goal-insert.php
@@ -1,34 +1,43 @@
 <?php 
-$menuConfiguracoes="is-active";
+$menuMeta="is-active";
 include('menu.php');
-$opcao=trim($_POST['opcao']);
-$nome=trim($_POST['nome']);
-$situacao=trim($_POST['situacao']);
+$meta=trim($_POST['meta']);
+$atividade=trim($_POST['atividade']);
+$setor=trim($_POST['setor']);
+$descricao=trim($_POST['descricao']);
+$usuario=trim($_POST['usuario']);
+$expiracao=trim($_POST['expiracao']);
 
 if(isset($_POST["inserirMeta"])!=null){
-	if($opcao!="" && $nome!=""){
-		$checkMeta="SELECT NOME FROM ".$opcao." WHERE NOME='".$nome."';";
-		$cnx= mysqli_query($phpmyadmin, $checkOpcao);
-		if(mysqli_num_rows($cnx)==0){
-			$inserirOpcao="INSERT INTO ".$opcao."(NOME, SITUACAO) VALUES('".$nome."','".$situacao."');";
-			$cnx= mysqli_query($phpmyadmin, $inserirOpcao);
-			if(mysqli_error($phpmyadmin)==null){
-				echo "<script>alert('Opção cadastrada com sucesso!!')</script>";
-			}
-			else{
-				$erro=mysqli_error($phpmyadmin);
-				echo "<script>alert('Erro ".$erro."!!')</script>";
+	if($meta!="" && $descricao!="" && $expiracao!=""){
+		if($usuario=="Todos"){
+			$listIds="SELECT ID FROM USUARIO WHERE SETOR_ID=".$setor.";";
+			$cnx= mysqli_query($phpmyadmin, $listIds);
+			while ($idUsuario= $cnx->fetch_array()) {
+				$inserirMeta="INSERT INTO META(META, ATIVIDADE_ID, DESCRICAO, USUARIO_ID, EXPIRACAO) VALUES(".$meta.",".$atividade.",'".$descricao."',".$idUsuario["ID"].",'".$expiracao."');";
+				$cx= mysqli_query($phpmyadmin, $inserirMeta);
 			}
 		}
 		else{
-			echo "<script>alert('Já existe este nome cadastrado nesta opção!!')</script>";
+			$inserirMeta="INSERT INTO META(META, ATIVIDADE_ID, DESCRICAO, USUARIO_ID, EXPIRACAO) VALUES(".$meta.",".$atividade.",'".$descricao."',".$usuario.",'".$expiracao."');";			
+			$cnx= mysqli_query($phpmyadmin, $inserirMeta);
+		}
+		if(mysqli_error($phpmyadmin)==null){
+			echo "<script>alert('Meta's cadastrada com sucesso!!')</script>";
+		}
+		else{
+			$erro=mysqli_error($phpmyadmin);
+			echo "<script>alert('Erro ".$erro."!!')</script>";		
 		}	
 	}
-	else if($nome==""){
-		echo "<script>alert('Preencher o campo nome é obrigatório!!')</script>";
+	else if($meta==""){
+		echo "<script>alert('Preencher o campo Meta é obrigatório!!')</script>";
+	}
+	else if($descricao==""){
+		echo "<script>alert('Preencher o campo Descricao é obrigatório!!')</script>";
 	}
 	else{
-		echo "<script>alert('Selecionar a opção é obrigatório!!')</script>";
+		echo "<script>alert('Preencher o campo Expiração é obrigatório!!')</script>";
 	}	
 }
 ?>
@@ -38,14 +47,10 @@ if(isset($_POST["inserirMeta"])!=null){
 	<meta charset="UTF-8">	
 	<meta name="viewport" content="width=device-widht, initial-scale=1">
 	<title>Gestão de Desempenho - Inserir Meta</title>
-	<link rel="stylesheet" href="css/login.css" />
-	<link rel="stylesheet" href="css/bulma.min.css"/>
-	<script defer scr="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
 <body>
 	<section class="section">
 	  	<div class="container">
-	  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	   		<form action="goal-insert.php" method="POST">
 	    		<div class="field is-horizontal">
 					<div class="field-label is-normal">
@@ -54,7 +59,7 @@ if(isset($_POST["inserirMeta"])!=null){
 					<div class="field-body">
 						<div class="field" style="max-width:17em;">							
 							<div class="control">
-								<input type="text" class="input" name="nome">
+								<input type="text" class="input desempenho" name="meta">
 							</div>
 						</div>
 					</div>
@@ -67,11 +72,31 @@ if(isset($_POST["inserirMeta"])!=null){
 						<div class="field" >							
 							<div class="control" style="max-width:17em;">
 								<div class="select is-size-7-touch">
-									<select name="atividade[]">
+									<select name="atividade">
 									<?php $gdAtividade="SELECT ID, NOME FROM ATIVIDADE WHERE SITUACAO='Ativo' AND ID NOT IN(1,2,3,4) ORDER BY NOME";
 									$con = mysqli_query($phpmyadmin , $gdAtividade); $x=0; 
 									while($atividade = $con->fetch_array()):{?>
 									<option value="<?php echo $vtId[$x] = $atividade["ID"]; ?>"><?php echo $vtNome[$x] = $atividade["NOME"]; ?></option>
+									<?php $x;} endwhile;?>
+									</select>	
+								</div>
+							</div>						
+						</div>
+					</div>
+				</div>
+				<div class="field is-horizontal">
+					<div class="field-label is-normal">
+						<label class="label">Setor:</label>
+					</div>
+					<div class="field-body">
+						<div class="field" >							
+							<div class="control" style="max-width:17em;">
+								<div class="select is-size-7-touch">
+									<select name="setor">
+									<?php $gdSetor="SELECT ID, NOME FROM SETOR WHERE SITUACAO='Ativo' AND ID NOT IN(1,2,6,7) ORDER BY NOME";
+									$con = mysqli_query($phpmyadmin , $gdSetor); $x=0; 
+									while($setor = $con->fetch_array()):{?>
+									<option value="<?php echo $vtId[$x] = $setor["ID"]; ?>"><?php echo $vtNome[$x] = $setor["NOME"]; ?></option>
 									<?php $x;} endwhile;?>
 									</select>	
 								</div>
@@ -86,7 +111,7 @@ if(isset($_POST["inserirMeta"])!=null){
 					<div class="field-body">
 						<div class="field" style="max-width:17em;">							
 							<div class="control">
-								<textarea class="textarea"></textarea>
+								<textarea name="descricao" class="textarea" maxlenght="200"></textarea>
 							</div>						
 						</div>
 					</div>
@@ -99,12 +124,12 @@ if(isset($_POST["inserirMeta"])!=null){
 						<div class="field is-grouped" style="max-width:17em;">							
 							<div class="control">
 								<div class="select">
-									<select name="situacao">
-										<option selected="selected" value="Ativo">Todos</option>
-										<?php $gdAtividade="SELECT ID, NOME FROM USUARIO WHERE SETOR_ID IN(3,4,5,8) AND SITUACAO<>'Desligado' ORDER BY NOME;";
-									$con = mysqli_query($phpmyadmin , $gdAtividade); $x=0; 
-									while($atividade = $con->fetch_array()):{?>
-									<option value="<?php echo $vtId[$x] = $atividade["ID"]; ?>"><?php echo $vtNome[$x] = $atividade["NOME"]; ?></option>
+									<select name="usuario">
+										<option selected="selected" value="Todos">Todos do Setor</option>
+										<?php $gdUsuario="SELECT ID, NOME FROM USUARIO WHERE SETOR_ID IN(3,4,5,8) AND SITUACAO<>'Desligado' ORDER BY NOME;";
+									$con = mysqli_query($phpmyadmin , $gdUsuario); $x=0; 
+									while($usuario = $con->fetch_array()):{?>
+									<option value="<?php echo $vtId[$x] = $usuario["ID"]; ?>"><?php echo $vtNome[$x] = $usuario["NOME"]; ?></option>
 									<?php $x;} endwhile;?>																		
 									</select>	
 								</div>
@@ -119,11 +144,11 @@ if(isset($_POST["inserirMeta"])!=null){
 					<div class="field-body">
 						<div class="field is-grouped" style="max-width:17em;">							
 							<div class="control">
-								<input style="max-width:12.5em;" class="input registro" type="text" name="expiracao" value="<?php echo date('Y-m-d',strtotime('-1 day'))?>">
+								<input style="max-width:12.5em;" class="input registro" type="text" name="expiracao" value="<?php echo date('Y-m-d',strtotime('+1 day'))?>">
 							</div>
 							<div class="control">
-									<button name="inserirMeta" type="submit" class="button is-primary" value="Filtrar">Inserir</button>
-								</div>						
+								<button name="inserirMeta" type="submit" class="button is-primary" value="Filtrar">Inserir</button>
+							</div>						
 						</div>
 					</div>					
 				</div>
