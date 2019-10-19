3b671564d9396703f962c9988b1467e6ed77757f
Lwcyano <lwcyano.will@evino.com.br>
2019-10-19 13:48:28 -0300

    Correção de link no botão Nova Consulta.

--- goal-remove.php
+++ goal-remove.php
@@ -195,7 +195,7 @@ WHERE USUARIO_ID IN(SELECT ID FROM USUARIO WHERE NOME LIKE '%".$nome."%')
 					<input type="submit" class="button is-primary" id="submitQuery" onClick="history.go(0)" value="Atualizar"/>						
 				</div>
 				<div class="control">
-					<a href="report-update.php"><input name="Limpar" type="submit" class="button is-primary" value="Nova consulta"/></a>
+					<a href="goal-remove.php"><input name="Limpar" type="submit" class="button is-primary" value="Nova consulta"/></a>
 				</div>
 				<div class="control">
 					<input name="removerDados" type="submit" class="button is-primary" value="Deletar Dados"/>
