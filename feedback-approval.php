27adf2cff813c1fa39448731dca7c44a4359ab7b
Lwcyano <lwcyano.will@evino.com.br>
2019-11-08 17:14:46 -0300

    Adição de código p/ permitir atualização com status Enviado, Aprovado e Reprovado.

--- feedback-approval.php
+++ feedback-approval.php
@@ -4,8 +4,13 @@ include('menu.php');
 $situacao= trim($_POST["situacao"]);
 $contador = 0;
 if(isset($_POST['consultar'])){
-$query="SELECT F.ID, U.NOME AS REMETENTE, U2.NOME AS DESTINATARIO, TIPO, FEEDBACK, U3.NOME AS ATUALIZADO_POR FROM FEEDBACK F INNER JOIN USUARIO U ON U.ID=F.REMETENTE_ID
-INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID INNER JOIN USUARIO U3 ON U3.ID=F.ATUALIZADO_POR WHERE ".$situacao;
+	if($situacao=="'Enviado'"){
+		$query="SELECT F.ID, U.NOME AS REMETENTE, U2.NOME AS DESTINATARIO, F.TIPO, F.FEEDBACK FROM FEEDBACK F INNER JOIN USUARIO U ON U.ID=F.REMETENTE_ID INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID WHERE F.SITUACAO=".$situacao;
+	}
+	else{
+		$query="SELECT F.ID, U.NOME AS REMETENTE, U2.NOME AS DESTINATARIO, TIPO, FEEDBACK, U3.NOME AS ATUALIZADO_POR FROM FEEDBACK F INNER JOIN USUARIO U ON U.ID=F.REMETENTE_ID
+INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID INNER JOIN USUARIO U3 ON U3.ID=F.ATUALIZADO_POR WHERE F.SITUACAO=".$situacao;
+	}
 	$x=0;
 	$cnx=mysqli_query($phpmyadmin, $query);
 	while($feed= $cnx->fetch_array()){
@@ -48,9 +53,9 @@ INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID INNER JOIN USUARIO U3 ON U3.ID=
 						<div class="control">
 							<div class="select">
 								<select name="situacao">								
-									<option value="ATUALIZADO_POR IS NULL">Aguardando</option>
-									<option value="ATUALIZADO_POR=2">Reprovado</option>
-									<option value="ATUALIZADO_POR=1">Aprovado</option>
+									<option value="'Enviado'">Aguardando</option>
+									<option value="'Reprovado'">Reprovado</option>
+									<option value="'Aprovado'">Aprovado</option>
 								</select>
 							</div>
 						</div>
@@ -76,32 +81,57 @@ INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID INNER JOIN USUARIO U3 ON U3.ID=
 	<table class="table is-bordered pricing__table is-fullwidth is-size-7-touch">	
 	<tr>
 		<th>N°</th>
+		<th class="ocultaColunaId">ID</th>
 		<th>Remetente</th>
 		<th>Destinatário</th>
 		<th>Tipo</th>
 		<th>Feedback</th>
-		<?php if($situacao=="ATUALIZADO_POR IS NULL"):?><th>Aprovar</th><?php endif;?>
-		<?php if($situacao=="ATUALIZADO_POR=1"):?><th>Aprovado por</th><?php endif;?>
-		<?php if($situacao=="ATUALIZADO_POR=2"):?><th>Reprovado por</th><?php endif;?>    			
+		<?php if($situacao=="'Enviado'"):?><th>Aprovar</th><?php endif;?>
+		<?php if($situacao=="'Aprovado'"):?><th>Aprovado por</th><?php endif;?>
+		<?php if($situacao=="'Reprovado'"):?><th>Reprovado por</th><?php endif;?>
+		<?php if($situacao!="'Enviado'"):?><th>Aprovar</th><?php endif;?>    			
 	</tr>
 	<?php
  	for( $i = 0; $i < sizeof($vtRemetente); $i++ ) : ?>
 	<tr>
 		<td><?php echo $i+1;?></td>
+		<td class="field ocultaColunaId"><!--COLUNA VETOR-->
+			<div class="field">				
+				<div class="control">					
+		  			<input name="id[]" type="text" value="<?php echo $vtId[$i]?>">
+		  		</div>
+		  	</div>
+		</td>
 		<td><?php echo $vtRemetente[$i]?></td>
 		<td><?php echo $vtDestinatario[$i]?></td>
 		<td><?php echo $vtTipo[$i]?></td>
-		<td><?php echo $vtFeedback[$i]?></td>
-		<?php if($situacao=="ATUALIZADO_POR IS NULL"):?><td>
+		<td><?php echo $vtFeedback[$i]?></td>		
+  		<?php if($situacao!="'Enviado'"):?><td><?php echo $vtAprovadoPor[$i]?></td><?php endif;?>
+  		<?php if($situacao=="'Enviado'"):?><td>
   			<div class="select">
-  				<select name="id[]">
-  					<option value="">...</option>
-  					<option value="1">Sim</option>
-  					<option value="2">Não</option>
+  				<select name="upSituacao[]">
+  					<option value="Enviado">...</option>
+  					<option value="Aprovado">Sim</option>
+  					<option value="Reprovado">Não</option>
   				</select>					
 			</div>	
   		</td><?php endif;?>
-  		<?php if($situacao!="ATUALIZADO_POR IS NULL"):?><td><?php echo $vtAprovadoPor[$i]?></td><?php endif;?>
+  		<?php if($situacao=="'Aprovado'"):?><td>
+  			<div class="select">
+  				<select name="upSituacao[]">
+  					<option value="Aprovado">Sim</option>
+  					<option value="Reprovado">Não</option>
+  				</select>					
+			</div>	
+  		</td><?php endif;?>
+  		<?php if($situacao=="'Reprovado'"):?><td>
+  			<div class="select">
+  				<select name="upSituacao[]">
+  					<option value="Reprovado">Não</option>
+  					<option value="Aprovado">Sim</option>
+  				</select>					
+			</div>	
+  		</td><?php endif;?>
 	</tr>
 <?php endfor;?>
 	</table>	
@@ -118,7 +148,7 @@ INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID INNER JOIN USUARIO U3 ON U3.ID=
 					<a href="feedback-approval.php"><input name="Limpar" type="submit" class="button is-primary" value="Nova consulta"/></a>
 				</div>
 				<div class="control">
-					<input name="aprovar" type="submit" class="button is-primary" value="Aprovar"/>
+					<input name="aprovar" type="submit" class="button is-primary" value="Atualizar"/>
 				</div>					
 			</div>						
 		</div>
@@ -131,9 +161,10 @@ INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID INNER JOIN USUARIO U3 ON U3.ID=
 <?php 
 if(isset($_POST["aprovar"])){
 	$ids=array_filter($_POST["id"]);
+	$situacoes=array_filter($_POST["upSituacao"]);
 	$x=0;
 	while ($x< sizeof($ids)) {
-		$upFeedback="UPDATE FEEDBACK SET ATUALIZADO_POR=".$_SESSION["userId"].", SITUACAO='Aprovado' WHERE ID=".$ids[$x];
+		$upFeedback="UPDATE FEEDBACK SET ATUALIZADO_POR=".$_SESSION["userId"].", SITUACAO='".$situacoes[$x]."' WHERE ID=".$ids[$x];
 		$cnx= mysqli_query($phpmyadmin, $upFeedback);
 		$x++;
 	}
@@ -141,13 +172,13 @@ if(isset($_POST["aprovar"])){
 		$x=$x+1;
 		?><script type="text/javascript">			
 			alert('Foi atualizado(s) '<?php echo $x;?>' feedback(s)!');
-			window.location.href=window.location.href;
+			//window.location.href=window.location.href;
 		</script> <?php		
 	}
 	else{
 		?><script type="text/javascript">			
 			alert('Nenhum feedback foi atualizado!');
-			window.location.href=window.location.href;
+			//window.location.href=window.location.href;
 		</script> <?php	
 	}
 }
