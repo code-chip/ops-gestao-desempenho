331719ee1d6484b728c41ece4d2767744b273960
Lwcyano <lwcyano.will@evino.com.br>
2019-11-08 18:27:04 -0300

    Ajustes finais da funcionalidade.

---
+++ feedback-query.php
@@ -0,0 +1,123 @@
+<?php
+$menuFeedback="is-active";
+include('menu.php');
+$feedback= trim($_POST["feedback"]);
+$contador = 0;
+if(isset($_POST['consultar'])){
+	$query="SELECT F.ID, U.NOME AS REMETENTE, U2.NOME AS DESTINATARIO, F.TIPO, F.FEEDBACK, F.EXIBICAO, F.PROFISSIONAL, F.COMPORTAMENTAL, F.DESEMPENHO, F.SITUACAO FROM FEEDBACK F INNER JOIN USUARIO U ON U.ID=F.REMETENTE_ID INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID WHERE ".$feedback."=".$_SESSION["userId"]." ORDER BY F.REGISTRO DESC";
+	$x=0;
+	$cnx=mysqli_query($phpmyadmin, $query);
+	while($feed= $cnx->fetch_array()){
+		$vtId[$x]=$feed["ID"];
+		$vtRemetente[$x]=$feed["REMETENTE"];
+		$vtDestinatario[$x]=$feed["DESTINATARIO"];
+		$vtTipo[$x]=$feed["TIPO"];
+		$vtProfissional[$x]=$feed["PROFISSIONAL"];
+		$vtComportamental[$x]=$feed["COMPORTAMENTAL"];
+		$vtDesempenho[$x]=$feed["DESEMPENHO"];
+		$vtSituacao[$x]=$feed["SITUACAO"];
+		$vtFeedback[$x]=$feed["FEEDBACK"];
+		$x++;
+		$contador=$x;
+	}
+	if(mysqli_num_rows($cnx)==0){
+		?><script type="text/javascript">			
+			alert('Nenhum feedback encontrado nesta consulta!');
+			window.location.href=window.location.href;
+		</script> <?php		
+	}	
+}	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gestão de Desempenho - Consultar Feedback</title>
+	<script type="text/javascript" src="/js/lib/dummy.js"></script>
+    <link rel="stylesheet" type="text/css" href="/css/result-light.css">   
+</head>
+<body>	
+<hr/>
+<section class="section" id="topo">
+<?php if($situacao =="" && isset($_POST['consultar'])==null ): ?>	
+	<form id="form1" action="feedback-query.php" method="POST">
+		<div class="field is-horizontal">
+			<div class="field is-horizontal">
+				<div class="field-label is-normal">
+					<label class="label">Feedback:</label>
+				</div>
+				<div class="field-body">
+					<div class="field" style="max-width:17em;">							
+						<div class="control">
+							<div class="select">
+								<select name="feedback">								
+									<option value="REMETENTE_ID">Enviado(s)</option>
+									<option value="DESTINATARIO_ID">Recebido(s)</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="field is-horizontal">
+				<div class="field-label"></div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<button name="consultar" type="submit" class="button is-primary">Consultar</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>						
+	</form>
+<?php endif; ?>
+<?php if(isset($_POST['consultar']) && $contador!=0) : ?>	
+	<div class="table__wrapper">
+	<table class="table is-bordered pricing__table is-fullwidth is-size-7-touch">	
+	<tr>
+		<th>N°</th>
+		<?php if($feedback=="DESTINATARIO_ID"):?><th>Remetente</th><?php endif;?>
+		<?php if($feedback=="REMETENTE_ID"):?><th>Destinatário</th><?php endif;?>
+		<th>Tipo</th>
+		<th>Pro</th>
+		<th>Com</th>
+		<th>Des</th>
+		<th>Feedback</th>
+		<th>Situação</th>					
+	</tr>
+	<?php
+ 	for( $i = 0; $i < sizeof($vtId); $i++ ) : ?>
+	<tr>
+		<td><?php echo $i+1;?></td>		
+		<?php if($feedback=="DESTINATARIO_ID"):?><td><?php if($vtExibicao[$i]==1){echo $vtRemetente[$i];} else{echo "Anônimo";}?></td><?php endif;?>
+		<td><?php echo $vtDestinatario[$i]?></td>
+		<td><?php echo $vtTipo[$i]?></td>
+		<td><?php echo $vtProfissional[$i]?></td> 
+		<td><?php echo $vtComportamental[$i]?></td> 
+		<td><?php echo $vtDesempenho[$i]?></td> 
+		<td><?php echo $vtFeedback[$i]?></td>
+  		<td><?php echo $vtSituacao[$i]?></td>
+  		
+	</tr>
+<?php endfor;?>
+	</table>	
+	<a href="#topo">		
+		<div class=".scrollWrapper">
+			<button class="button is-primary" style="width: 100%; display: table;">Ir Ao Topo</button>		
+		</div>
+	</a>
+	<br/>
+	<div class="table__wrapper">			
+		<div class="field-body">
+			<div class="field is-grouped">
+				<div class="control">
+					<a href="feedback-query.php"><input name="Limpar" type="submit" class="button is-primary" value="Nova consulta"/></a>
+				</div>									
+			</div>						
+		</div>
+	</div>
+</section>
+<?php endif;?>	
+</body>
+</html>
+<?php
