3c80f7723d8d6e42e5d40a09ddae42fe474f42f8
Lwcyano <lwcyano.will@evino.com.br>
2019-12-18 18:05:21 -0300

    Adição de código e restrição p/ exportação.

--- backup.php
+++ backup.php
@@ -1,3 +1,22 @@
 <?php
+if($_SESSION["permissao"]==4){
+$DBUSER="id8414870_admin";
+$DBPASSWD="ProjetoAlfa@2019";
+$DATABASE="id8414870_projetoalfa";
 
+$filename = "backup-" . date("d-m-Y") . ".sql";
+$mime = "application/x-gzip";
+//header( "Content-Type: " . $mime );
+header( 'Content-Disposition: attachment; filename="' . $filename . '"' );
+
+$cmd = "mysqldump -u $DBUSER --password=$DBPASSWD $DATABASE";   
+
+passthru( $cmd );
+
+exit(0);
+}
+else{
+	echo "<script>alert('Usuário sem permissão')</script>";
+	header("Refresh: 1;url=home.php");
+}
 ?>
