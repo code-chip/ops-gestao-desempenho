78be54bef75dd422f9f6512a787d5fe6a3843786
Lwcyano <lwcyano.will@evino.com.br>
2020-01-24 09:18:19 -0300

    Criação de layout e designer.

---
+++ feedback-evaluation.php
@@ -0,0 +1,310 @@
+<?php
+$menuMeta="is-active";
+include('menu.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gestão de Desempenho - Avaliação</title>
+	<script type="text/javascript" src="/js/lib/dummy.js"></script>
+    <link rel="stylesheet" type="text/css" href="/css/result-light.css">   
+</head>
+<body>
+<div>	
+	<section class="section">
+	<div class="container">	
+	<form id="form1" action="" method="POST">
+	<div class="box">
+		<div><strong>Avaliação de Desempenho - Operação ( evino )</strong></div>
+	</div>
+	Operador I <br>
+	<div class="box">
+		<div><strong>Auto-avaliação</strong> <br>Aqui você deverá fazer  a sua auto avaliação sobre cada item abordado:</div>
+	</div>
+	<div class="box">	
+		<div class="field is-horizontal">
+			<div class="text">Executa movimentos ágeis e com naturalidade em todos as áreas (checkout,
+separação, caixas e recebimento).				
+			</div>				
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question">1 - Não atende
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question">2 - Atende parcialmente
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question">3 - Conforme esperado
+		    	</label>		    	
+		  	</div>
+		</div>	
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question">4 - Acima do esperado
+		    	</label>		    	
+		  	</div>
+		</div>
+	</div>
+	<div class="box">	
+		<div class="field is-horizontal">
+			<div class="text">Cumpre e/ou colabora para o cumprimento das metas e resultados relacionados
+à velocidade de produção e entregas dentro do prazo.				
+			</div>				
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question2">1 - Não atende
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question2">2 - Atende parcialmente
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question2">3 - Conforme esperado
+		    	</label>		    	
+		  	</div>
+		</div>	
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question2">4 - Acima do esperado
+		    	</label>		    	
+		  	</div>
+		</div>
+	</div>
+	<div class="box">	
+		<div class="field is-horizontal">
+			<div class="text">Transmite segurança na conferência de itens (no estoque, mesa e/ou
+recebimento), fazendo-o de forma consistente e assertiva.				
+			</div>				
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question3">1 - Não atende
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question3">2 - Atende parcialmente
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question3">3 - Conforme esperado
+		    	</label>		    	
+		  	</div>
+		</div>	
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question3">4 - Acima do esperado
+		    	</label>		    	
+		  	</div>
+		</div>
+	</div>
+	<div class="box">	
+		<div class="field is-horizontal">
+			<div class="text">Demonstra atenção aos detalhes e qualidade ao lidar com produtos, caixas,
+ferramentas de trabalho e processos em geral.				
+			</div>				
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question4">1 - Não atende
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question4">2 - Atende parcialmente
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question4">3 - Conforme esperado
+		    	</label>		    	
+		  	</div>
+		</div>	
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question4">4 - Acima do esperado
+		    	</label>		    	
+		  	</div>
+		</div>
+	</div>
+	<div class="box">	
+		<div class="field is-horizontal">
+			<div class="text">Demonstra que conhece e respeita os procedimentos e padrões da Evino em
+todas as áreas em que atua, sem cometer equívocos ou gerar falhas processuais.				
+			</div>				
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question5">1 - Não atende
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question5">2 - Atende parcialmente
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question5">3 - Conforme esperado
+		    	</label>		    	
+		  	</div>
+		</div>	
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question5">4 - Acima do esperado
+		    	</label>		    	
+		  	</div>
+		</div>
+	</div>
+	<div class="box">	
+		<div class="field is-horizontal">
+			<div class="text">Demonstra zelo e cuidado pela integridade e higiene do local e instrumentos de
+trabalho.				
+			</div>				
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question6">1 - Não atende
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question6">2 - Atende parcialmente
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question6">3 - Conforme esperado
+		    	</label>		    	
+		  	</div>
+		</div>	
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question6">4 - Acima do esperado
+		    	</label>		    	
+		  	</div>
+		</div>
+	</div>
+	<div class="box">	
+		<div class="field is-horizontal">
+			<div class="text">Colabora para a manutenção de uma organização eficiente e de acordo com os
+padrões. Mantém cada coisa em seu lugar e no melhor lugar possível
+(instrumentos de trabalho, caixas, pallets, etiquetas, notas, ondas, etc).				
+			</div>				
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question7">1 - Não atende
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question7">2 - Atende parcialmente
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question7">3 - Conforme esperado
+		    	</label>		    	
+		  	</div>
+		</div>	
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question7">4 - Acima do esperado
+		    	</label>		    	
+		  	</div>
+		</div>
+	</div>
+	<div class="box">	
+		<div class="field is-horizontal">
+			<div class="text">Demonstra facilidade em aprender e entender os direcionamentos que são
+passados, bem como assimilar novos processos.				
+			</div>				
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question8">1 - Não atende
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question8">2 - Atende parcialmente
+		    	</label>		    	
+		  	</div>
+		</div>
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question8">3 - Conforme esperado
+		    	</label>		    	
+		  	</div>
+		</div>	
+		<div class="field">
+		  	<div class="control">
+		    	<label class="radio">
+		      		<input type="radio" name="question8">4 - Acima do esperado
+		    	</label>		    	
+		  	</div>
+		</div>
+	</div>									
+	</form>
+	</div>
+	</section>	
+</section>	
+</body>
+</html>
