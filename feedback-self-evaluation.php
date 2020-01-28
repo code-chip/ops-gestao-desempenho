ca2e0c50ed6869f270c284e65e9cb85792bbb80f
Lwcyano <lwcyano.will@evino.com.br>
2020-01-28 12:32:53 -0300

    Atualização e adição de lógica p/ salvar e carregar as respostas selecionadas.

--- feedback-self-evaluation.php
+++ feedback-self-evaluation.php
@@ -1,11 +1,10 @@
 <?php
 $menuFeedback="is-active";
 include('menu.php');
-$info="SELECT C.NOME AS CARGO FROM USUARIO U JOIN CARGO C ON C.ID=U.CARGO_ID WHERE U.ID=1".$_SESSION["userId"];
+$houveResposta=false;
+$info="SELECT C.NOME AS CARGO FROM USUARIO U JOIN CARGO C ON C.ID=U.CARGO_ID WHERE U.ID=".$_SESSION["userId"];
 $cx=mysqli_query($phpmyadmin, $info);
 $cargo=$cx->fetch_array();
-echo $cargo["CARGO"];
-echo $_SESSION["userId"];
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,7 +18,7 @@ echo $_SESSION["userId"];
 <div>	
 	<section class="section">
 	<div class="container">	
-	<form id="form1" action="" method="POST">
+	<form id="form" action="" method="POST">
 	<div class="box" style="margin-bottom: -30px; background-color: rgb(64,224,208)"></div>
 	<div class="box">
 		<div class="is-size-1-desktop"><strong>Avaliação de Desempenho - Operação ( evino )</strong></div>
@@ -31,19 +30,32 @@ echo $_SESSION["userId"];
 	$y=1;	
 	$getPergunta="SELECT ID, PERGUNTA FROM AVAL_PERGUNTA ORDER BY ORDEM;";
 	$cnx=mysqli_query($phpmyadmin, $getPergunta);
-	while ($pergunta=$cnx->fetch_array()):{ $questao="questao".$y; $idPergunta[$y-1]=$pergunta["ID"];?>
+	while ($pergunta=$cnx->fetch_array()):{ $questao="questao".$y; $idPergunta[$y-1]=$pergunta["ID"];
+		//CHECK SE A PERGUNTA FOI RESPONDIDA;
+		$verifResposta="SELECT AVAL_RESPOSTA_ID FROM AVAL_REALIZADA WHERE USUARIO_ID=".$_SESSION["userId"]." AND AVAL_PERGUNTA_ID=".$idPergunta[$y-1]." AND REGISTRO='".date('Y-m-d')."';";
+		$cnx2=mysqli_query($phpmyadmin, $verifResposta);
+		if(mysqli_num_rows($cnx2)>0){
+			$houveResposta=true;
+			$getRes=$cnx2->fetch_array();
+			$getIdResposta=$getRes["AVAL_RESPOSTA_ID"];
+			$selecao="CHECKED";
+		}
+		else{
+			$selecao=null;
+		}
+		?>
 	<div class="box">	
 		<div class="field is-horizontal">
 			<div class="text"><?php echo $pergunta["PERGUNTA"];?></div>				
 		</div>
 		<?php $getResposta="SELECT ID, RESPOSTA FROM AVAL_RESPOSTA WHERE SITUACAO='Ativo';";
 		$x=0;
-		$cnx2=mysqli_query($phpmyadmin, $getResposta);
-		while ($resposta= $cnx2->fetch_array()){?>
+		$cnx3=mysqli_query($phpmyadmin, $getResposta);
+		while ($resposta= $cnx3->fetch_array()){?>
 		<div class="field ">
 		  	<div class="control">
 		    	<label class="radio">
-		      		<input type="radio" name="<?php echo $questao;?>" value="<?php echo $resposta["ID"];?>"><?php echo $resposta["RESPOSTA"];?>
+		      		<input type="radio" name="<?php echo $questao;?>" value="<?php echo $resposta["ID"];?>" <?php if($getIdResposta==$resposta["ID"]){ echo $selecao;}?>><?php echo $resposta["RESPOSTA"];?>
 		    	</label>		    	
 		  	</div>
 		</div>
@@ -62,11 +74,13 @@ echo $_SESSION["userId"];
 		    	<button class="button is-link">Cancelar</button>
 		  	</div>
 		  	<div class="control">
-		    	<button class="button is-link is-light" name="proxima">Próxima</button>
+		    	<button class="button is-link is-light" name="proxima" type="submit">Próxima</button>
 		  	</div>
 		</div>
 	</div>									
 	</form>
+	<script>
+</script>
 	</div>
 	</section>	
 </section>	
@@ -74,17 +88,46 @@ echo $_SESSION["userId"];
 </html>
 <?php 
 if(isset($_POST['proxima'])){
-	$z=1;
-	while ($z <= $y) {//ARMAZENAS AS RESPOSTAS NO VETOR.
+	$z=1; $respostaNula=0;
+	while ($z <= $y) {//ARMAZENA AS RESPOSTAS NO VETOR.
 		$resposta[$z-1]=$_POST["questao".$z];
-		$z++;
+		if($resposta[$z-1]==null){			
+			$respostaNula++;
+			$resposta[$z-1]=5;//ID REFERENTE AO TEXTO AGUARDANDO RESPOSTA.
+		}
+		$z++;			
 	}
-	for($i=0 ;$i<sizeof($resposta);$i++){//INSERE NO BANCO OS ID'S DAS PERGUNTAS E RESPOSTAS.
-		echo $resposta[$i];
-		$salvar="INSERT INTO AVAL_REALIZADA(USUARIO_ID, AVAL_PERGUNTA, AVAL_RESPOSTA, REGISTRO) VALUES(".$_SESSION['userId'].", ".$idPergunta[$i].",".$resposta[$i].", '".date('Y-m-d')."');";
-		echo $salvar;
-		//$cnx=mysqli_query($phpmyadmin, $salvar);
+	if($houveResposta==true){//CHECK SE HÁ ALGUMA PERGUNTA RESPONDIDA.		
+		for($i=0 ;$i<sizeof($resposta);$i++){//PERCORRE PELAS QUESTÕES.
+			$verifResposta="SELECT ID FROM AVAL_REALIZADA WHERE USUARIO_ID=".$_SESSION["userId"]." AND AVAL_PERGUNTA_ID=".$idPergunta[$i]." AND REGISTRO='".date('Y-m-d')."';";	
+			$cnx4=mysqli_query($phpmyadmin, $verifResposta);
+			if(mysqli_num_rows($cnx4)==1){			
+				$getRes=$cnx4->fetch_array();
+				$atualiza="UPDATE AVAL_REALIZADA SET AVAL_RESPOSTA_ID=".$resposta[$i]." WHERE ID=".$getRes["ID"].";";
+				echo $atualiza;
+			}
+			else{
+				$atualiza="INSERT INTO AVAL_REALIZADA(USUARIO_ID, AVAL_PERGUNTA_ID, AVAL_RESPOSTA_ID, REGISTRO) VALUES(".$_SESSION['userId'].", ".$idPergunta[$i].",".$resposta[$i].", '".date('Y-m-d')."');";
+				echo "<br>"." INSERT";
+			}
+			$cnx=mysqli_query($phpmyadmin, $atualiza);				
+		}
 	}
-	//header("refresh: 1; url=feedback-techinnal.php");
+	else{
+		for($i=0 ;$i<sizeof($resposta);$i++){//INSERE NO BANCO OS ID'S DAS PERGUNTAS E RESPOSTAS.
+		$salvar="INSERT INTO AVAL_REALIZADA(USUARIO_ID, AVAL_PERGUNTA_ID, AVAL_RESPOSTA_ID, REGISTRO) VALUES(".$_SESSION['userId'].", ".$idPergunta[$i].",".$resposta[$i].", '".date('Y-m-d')."');";
+		$cnx5=mysqli_query($phpmyadmin, $salvar);
+		}
+	}
+	if($respostaNula>0){?>
+		<script type="text/javascript">
+			alert('Atenção a respostas de todas perguntas são obrigatórias!');
+			window.location.href=window.location.href;
+		</script><?php	
+	}
+	else{
+		header("refresh: 1; url=feedback-techninal-evaluation.php");
+	}
+	
 }
 ?>
