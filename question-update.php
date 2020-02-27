0d691a12406901720f5d856333bde26881815437
Lwcyano <lwcyano.will@evino.com.br>
2020-02-27 13:07:54 -0300

    Criação dos arquivos.

---
+++ question-update.php
@@ -0,0 +1,19 @@
+<?php 
+$menuConfiguracoes="is-active";
+include('menu.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-widht, initial-scale=1">
+	<title>Gestão de Desempenho - Atualizar Pergunta</title>
+	<link rel="shortcut icon" href="img\favicon_codechip.ico"/>
+	<link rel="stylesheet" href="css/login.css" />
+	<link rel="stylesheet" href="css/bulma.min.css"/>
+	<script defer scr="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
