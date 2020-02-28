9a3e28b005cc0ef14c816b0eeed7269018f71dba
Lwcyano <lwcyano.will@evino.com.br>
2020-02-28 12:31:30 -0300

    Criação da funcionalidade opções.

---
+++ options.php
@@ -0,0 +1,84 @@
+<?php 
+$menuConfiguracoes="is-active";
+include('menu.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-widht, initial-scale=1">
+	<title>Gestão de Desempenho - Opções de Configurações</title>
+	<link rel="shortcut icon" href="img\favicon_codechip.ico"/>
+	<link rel="stylesheet" href="css/login.css" />
+	<link rel="stylesheet" href="css/personal.css">
+	<link rel="stylesheet" href="css/bulma.min.css"/>
+	<script defer scr="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+<body>
+	<div class="hero is-fullheight is-primary has-background">
+	  	<img alt="Fill Murray" class="hero-background is-transparent" src="img/wallpaper/options.jpg" />
+	  	<div class="hero-body">
+	    	<div class="container">
+	    		<div class="box transparenci">		     	
+				<div class="field">
+					<div class="control">
+					    <label class="is-size-5">
+					     Dashboard
+					    </label>
+					</div>					
+  					<div class="control">
+					    <input id="switch-shadow0" class="switch switch--shadow" type="checkbox" CHECKED>
+				  		<label for="switch-shadow0"></label>				    
+					</div>
+					<div class="control">
+					    <label class="is-size-5">
+					     Meta
+					    </label>
+					</div>					
+  					<div class="control">
+					    <input id="switch-flat0" class="switch switch--flat" type="checkbox">
+				  		<label for="switch-flat0"></label>			    
+					</div>
+					<div class="control">
+					    <label class="is-size-5">
+					     Desempenho
+					    </label>
+					</div>					
+  					<div class="control">
+					    <input id="switch-shadow1" class="switch switch--shadow" type="checkbox" CHECKED>
+				  		<label for="switch-shadow1"></label>				    
+					</div>
+					<div class="control">
+					    <label class="is-size-5">
+					     Feedback
+					    </label>
+					</div>					
+  					<div class="control">
+					    <input id="switch-flat1" class="switch switch--flat" type="checkbox">
+				  		<label for="switch-flat1"></label>			    
+					</div>
+					<div class="control">
+					    <label class="is-size-5">
+					     Avaliação
+					    </label>
+					</div>					
+  					<div class="control">
+					    <input id="switch-shadow2" class="switch switch--shadow" type="checkbox" CHECKED>
+				  		<label for="switch-shadow2"></label>				    
+					</div>
+					<div class="control">
+					    <label class="is-size-5">
+					     Relatórios
+					    </label>
+					</div>					
+  					<div class="control">
+					    <input id="switch-flat2" class="switch switch--flat" type="checkbox">
+				  		<label for="switch-flat2"></label>			    
+					</div>
+				</div>
+				</div>
+	    	</div>
+	  	</div>
+	</div>
+</body>
+</html>
