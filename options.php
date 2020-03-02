4862f03c46a1e6ad9ec7b45ce1c9d7b6d3437050
Lwcyano <lwcyano.will@evino.com.br>
2020-03-02 12:22:37 -0300

    Adição de javascript para desabilitar Itens de Menu do Menu desabilitado.

--- options.php
+++ options.php
@@ -13,24 +13,7 @@ include('menu.php');
 	<link rel="stylesheet" href="css/personal.css">
 	<link rel="stylesheet" href="css/bulma.min.css"/>
 	<script defer scr="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script type="text/javascript">
-		$(".meses").click(function(){
-		  let meses = document.querySelectorAll(".meses");
-		  for (let i = 0; i < meses.length; i++) {
-		    if (meses[i].checked && i+1 < meses.length){
-		      meses[i+1].removeAttribute("disabled");
-		    } else {
-		      if (i+1 < meses.length) {
-		        for (let j = i+1; j < meses.length; j++) {
-		          meses[j].checked = false;
-		          meses[j].setAttribute("disabled", "true");
-		        }
-		      }
-		    }
-		  }
-		});
-	</script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
 </head>
 <body>
 	<div class="hero is-fullheight is-primary has-background">
@@ -40,60 +23,60 @@ include('menu.php');
 	    		<div class="box">
 	    		<div class="columns">
 					<div class="column">
-					    <label class="meses is-size-5"><strong>Meta</strong></label>
-					    <input id="switch-shadow0" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <label class="is-size-5"><strong>Meta</strong></label>
+					    <input id="switch-shadow0" class="meta switch switch--shadow" type="checkbox">
 				  		<label for="switch-shadow0"></label>				    
-					    <label class="meses is-size-5">Consultar</label>
-					    <input id="switch-flat0" class="switch switch--flat" type="checkbox">
+					    <label class="is-size-5">Consultar</label>
+					    <input id="switch-flat0" class="meta switch switch--flat" disabled="disabled" type="checkbox">
 				  		<label for="switch-flat0"></label>			    
 					    <label class="meses is-size-5">Inserir</label>
-					    <input id="switch-shadow1" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow1" class="meta switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow1"></label>   
 				  		<label class="meses is-size-5">Atualizar</label>
-					    <input id="switch-flat1" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat1" class="meta switch switch--flat" type="checkbox">
 				  		<label for="switch-flat1"></label>			    
 					    <label class="meses is-size-5">Remover</label>
-					    <input id="switch-shadow2" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow2" class="meta switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow2"></label>    
 					</div>
 					<div class="column">
 					    <label class="is-size-5"><strong>Desempenho</strong></label>
-					    <input id="switch-shadow3" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow3" class="desempenho switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow3"></label>				    
 					    <label class="is-size-5">Consultar</label>
-					    <input id="switch-flat3" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat3" class="desempenho switch switch--flat" type="checkbox">
 				  		<label for="switch-flat3"></label>			    
 					    <label class="is-size-5">Inserir</label>
-					    <input id="switch-shadow4" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow4" class="desempenho switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow4"></label>   
 				  		<label class="is-size-5">Atualizar</label>
-					    <input id="switch-flat4" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat4" class="desempenho switch switch--flat" type="checkbox">
 				  		<label for="switch-flat4"></label>			    
 					    <label class="is-size-5">Remover</label>
-					    <input id="switch-shadow6" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow6" class="desempenho switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow6"></label>				    
 					    <label class="is-size-5">Pedente</label>
-					    <input id="switch-flat6" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat6" class="desempenho switch switch--flat" type="checkbox">
 				  		<label for="switch-flat6"></label>			    
 					</div>
 					<div class="column">
 					    <label class="is-size-5"><strong>Feedback</strong></label>
-					    <input id="switch-shadow7" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow7" class="feedback switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow7"></label>				    
 					    <label class="is-size-5">Aprovar</label>
-					    <input id="switch-flat7" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat7" class="feedback switch switch--flat" type="checkbox">
 				  		<label for="switch-flat7"></label>			    
 					    <label class="is-size-5">Enviar</label>
-					    <input id="switch-shadow8" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow8" class="feedback switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow8"></label>   
 				  		<label class="is-size-5">Solicitar</label>
-					    <input id="switch-flat8" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat8" class="feedback switch switch--flat" type="checkbox">
 				  		<label for="switch-flat8"></label>			    
 					    <label class="is-size-5">Consultar</label>
-					    <input id="switch-shadow9" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow9" class="feedback switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow9"></label>				    
 					    <label class="is-size-5">Avaliação</label>
-					    <input id="switch-flat9" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat9" class="feedback switch switch--flat" type="checkbox">
 				  		<label for="switch-flat9"></label>			    
 					</div>
 					<div class="column">
@@ -101,33 +84,33 @@ include('menu.php');
 					    <input id="switch-shadow10" class="switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow10"></label>				    
 					    <label class="is-size-5"><strong>Relatórios</strong></label>
-					    <input id="switch-flat10" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat10" class="relatorios switch switch--flat" type="checkbox" CHECKED>
 				  		<label for="switch-flat10"></label>			    
 					    <label class="is-size-5">Gestão</label>
-					    <input id="switch-shadow11" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow11" class="relatorios switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow11"></label>   
 				  		<label class="is-size-5">Individual</label>
-					    <input id="switch-flat11" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat11" class="relatorios switch switch--flat" type="checkbox">
 				  		<label for="switch-flat11"></label>
 				  		<label class="is-size-5">SQL</label>
-					    <input id="switch-shadow12" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow12" class="relatorios switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow12"></label>			    
 					</div>
 					<div class="column">
 					    <label class="is-size-5"><strong>Configurações</strong></label>
-					    <input id="switch-shadow13" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow13" class="configuracoes switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow13"></label>				    
 					    <label class="is-size-5">Cadastro</label>
-					    <input id="switch-flat12" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat12" class="configuracoes switch switch--flat" type="checkbox">
 				  		<label for="switch-flat12"></label>			    
 					    <label class="is-size-5">Opções</label>
-					    <input id="switch-shadow14" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow14" class="configuracoes switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow14"></label>   
 				  		<label class="is-size-5">Backup</label>
-					    <input id="switch-flat13" class="switch switch--flat" type="checkbox">
+					    <input id="switch-flat13" class="configuracoes switch switch--flat" type="checkbox">
 				  		<label for="switch-flat13"></label>			    
 					    <label class="is-size-5">Reportar</label>
-					    <input id="switch-shadow15" class="switch switch--shadow" type="checkbox" CHECKED>
+					    <input id="switch-shadow15" class="configuracoes switch switch--shadow" type="checkbox" CHECKED>
 				  		<label for="switch-shadow15"></label>
 					</div>
 				</div>
@@ -135,5 +118,82 @@ include('menu.php');
 	    	</div>	
 	  	</div>
 	</div>
+	<script type="text/javascript">		
+		$(".meta").click(function(){
+		  let meta = document.querySelectorAll(".meta");		 
+		  for (let i = 0; i < meta.length; i++) {
+		    if (meta[0].checked && i+1 < meta.length){
+		      meta[i+1].removeAttribute("disabled");
+		    } else {
+		      if (i+1 < meta.length) {
+		        for (let j = i+1; j < meta.length; j++) {
+		          meta[j].checked = false;
+		          meta[j].setAttribute("disabled", "true");
+		        }
+		      }
+		    }
+		  }
+		});
+		$(".desempenho").click(function(){
+		  let desempenho = document.querySelectorAll(".desempenho");		 
+		  for (let i = 0; i < desempenho.length; i++) {
+		    if (desempenho[0].checked && i+1 < desempenho.length){
+		      desempenho[i+1].removeAttribute("disabled");
+		    } else {
+		      if (i+1 < desempenho.length) {
+		        for (let j = i+1; j < desempenho.length; j++) {
+		          desempenho[j].checked = false;
+		          desempenho[j].setAttribute("disabled", "true");
+		        }
+		      }
+		    }
+		  }
+		});
+		$(".feedback").click(function(){
+		  let feedback = document.querySelectorAll(".feedback");		 
+		  for (let i = 0; i < feedback.length; i++) {
+		    if (feedback[0].checked && i+1 < feedback.length){
+		      feedback[i+1].removeAttribute("disabled");
+		    } else {
+		      if (i+1 < feedback.length) {
+		        for (let j = i+1; j < feedback.length; j++) {
+		          feedback[j].checked = false;
+		          feedback[j].setAttribute("disabled", "true");
+		        }
+		      }
+		    }
+		  }
+		});
+		$(".relatorios").click(function(){
+		  let relatorios = document.querySelectorAll(".relatorios");		 
+		  for (let i = 0; i < relatorios.length; i++) {
+		    if (relatorios[0].checked && i+1 < relatorios.length){
+		      relatorios[i+1].removeAttribute("disabled");
+		    } else {
+		      if (i+1 < relatorios.length) {
+		        for (let j = i+1; j < relatorios.length; j++) {
+		          relatorios[j].checked = false;
+		          relatorios[j].setAttribute("disabled", "true");
+		        }
+		      }
+		    }
+		  }
+		});
+		$(".configuracoes").click(function(){
+		  let configuracoes = document.querySelectorAll(".configuracoes");		 
+		  for (let i = 0; i < configuracoes.length; i++) {
+		    if (configuracoes[0].checked && i+1 < configuracoes.length){
+		      configuracoes[i+1].removeAttribute("disabled");
+		    } else {
+		      if (i+1 < configuracoes.length) {
+		        for (let j = i+1; j < configuracoes.length; j++) {
+		          configuracoes[j].checked = false;
+		          configuracoes[j].setAttribute("disabled", "true");
+		        }
+		      }
+		    }
+		  }
+		});
+	</script>
 </body>
 </html>
