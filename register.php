44b57c95b6b7d2888545630dbebfad475190854c
Lwcyano <lwcyano.will@evino.com.br>
2020-03-02 12:44:36 -0300

    Adição de botões de cadastro de menu.

--- register.php
+++ register.php
@@ -71,7 +71,16 @@ include('menu.php');
 		<a href="question-remove.php"><span class="button is-media is-primary is-outlined mw12">Remover Pergunta</span></a>&nbsp&nbsp&nbsp		
 	</div>	
 </div>
+<div class="field has-addons has-addons-centered">
+	<div class="buttons">								
+		<a href="wait.php"><span class="button is-media is-primary is-outlined mw12">Inserir Menu</span></a>&nbsp&nbsp&nbsp
+		<a href="wait.php"><span class="button is-media is-primary is-outlined mw12">Consultar Menu</span></a>&nbsp&nbsp&nbsp
+		<a href="wait.php"><span class="button is-media is-primary is-outlined mw12">Atualizar Menu</span></a>&nbsp&nbsp&nbsp
+		<a href="wait.php"><span class="button is-media is-primary is-outlined mw12">Remover Menu</span></a>&nbsp&nbsp&nbsp		
+	</div>	
+</div>
 <!--<br/>
+
 <div class="field has-addons has-addons-centered">
 	<div class="buttons">								
 		<a href="wait.php"><span class="button is-media is-primary is-outlined mw12">Inserir Permissão</span></a>&nbsp&nbsp&nbsp
