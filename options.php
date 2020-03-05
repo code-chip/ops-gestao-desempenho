3cb0fcf0e6a4423facb93edcf7d8dc1ad7cfe19b
Lwcyano <lwcyano.will@evino.com.br>
2020-03-05 16:10:38 -0300

    Adição query para desabilitar menu e item de menu.

--- options.php
+++ options.php
@@ -1,6 +1,11 @@
 <?php 
 $menuAtivo="Configurações";
 include('menu.php');
+//function AtualizarMenu($menu){
+//	$upMenu="UPDATE MENU SET ATIVO='n' WHERE MENU=".$menu;
+//	$cnx=mysqli_query($phpmyadmin, $upMenu);
+//	echo "CHAMADA DE FUNCAO";
+//}
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,109 +26,38 @@ include('menu.php');
 	  	<div class="hero-body">
 	    	<div class="container">
 	    		<div class="box">
-	    		<div class="columns">
-					<div class="column">
-					    <label class="is-size-5"><strong>Meta</strong></label>
-					    <input id="switch-shadow0" class="meta switch switch--shadow" type="checkbox">
-				  		<label for="switch-shadow0"></label>				    
-					    <label class="is-size-5">Consultar</label>
-					    <input id="switch-flat0" class="meta switch switch--flat" disabled="disabled" type="checkbox">
-				  		<label for="switch-flat0"></label>			    
-					    <label class="meses is-size-5">Inserir</label>
-					    <input id="switch-shadow1" class="meta switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow1"></label>   
-				  		<label class="meses is-size-5">Atualizar</label>
-					    <input id="switch-flat1" class="meta switch switch--flat" type="checkbox">
-				  		<label for="switch-flat1"></label>			    
-					    <label class="meses is-size-5">Remover</label>
-					    <input id="switch-shadow2" class="meta switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow2"></label>    
-					</div>
-					<div class="column">
-					    <label class="is-size-5"><strong>Desempenho</strong></label>
-					    <input id="switch-shadow3" class="desempenho switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow3"></label>				    
-					    <label class="is-size-5">Consultar</label>
-					    <input id="switch-flat3" class="desempenho switch switch--flat" type="checkbox">
-				  		<label for="switch-flat3"></label>			    
-					    <label class="is-size-5">Inserir</label>
-					    <input id="switch-shadow4" class="desempenho switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow4"></label>   
-				  		<label class="is-size-5">Atualizar</label>
-					    <input id="switch-flat4" class="desempenho switch switch--flat" type="checkbox">
-				  		<label for="switch-flat4"></label>			    
-					    <label class="is-size-5">Remover</label>
-					    <input id="switch-shadow6" class="desempenho switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow6"></label>				    
-					    <label class="is-size-5">Pedente</label>
-					    <input id="switch-flat6" class="desempenho switch switch--flat" type="checkbox">
-				  		<label for="switch-flat6"></label>			    
-					</div>
-					<div class="column">
-					    <label class="is-size-5"><strong>Feedback</strong></label>
-					    <input id="switch-shadow7" class="feedback switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow7"></label>				    
-					    <label class="is-size-5">Aprovar</label>
-					    <input id="switch-flat7" class="feedback switch switch--flat" type="checkbox">
-				  		<label for="switch-flat7"></label>			    
-					    <label class="is-size-5">Enviar</label>
-					    <input id="switch-shadow8" class="feedback switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow8"></label>   
-				  		<label class="is-size-5">Solicitar</label>
-					    <input id="switch-flat8" class="feedback switch switch--flat" type="checkbox">
-				  		<label for="switch-flat8"></label>			    
-					    <label class="is-size-5">Consultar</label>
-					    <input id="switch-shadow9" class="feedback switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow9"></label>				    
-					    <label class="is-size-5">Avaliação</label>
-					    <input id="switch-flat9" class="feedback switch switch--flat" type="checkbox">
-				  		<label for="switch-flat9"></label>			    
-					</div>
-					<div class="column">
-					    <label class="is-size-5"><strong>Dashboard</strong></label>
-					    <input id="switch-shadow10" class="switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow10"></label>				    
-					    <label class="is-size-5"><strong>Relatórios</strong></label>
-					    <input id="switch-flat10" class="relatorios switch switch--flat" type="checkbox" CHECKED>
-				  		<label for="switch-flat10"></label>			    
-					    <label class="is-size-5">Gestão</label>
-					    <input id="switch-shadow11" class="relatorios switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow11"></label>   
-				  		<label class="is-size-5">Individual</label>
-					    <input id="switch-flat11" class="relatorios switch switch--flat" type="checkbox">
-				  		<label for="switch-flat11"></label>
-				  		<label class="is-size-5">SQL</label>
-					    <input id="switch-shadow12" class="relatorios switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow12"></label>			    
-					</div>
-					<div class="column">
-					    <label class="is-size-5"><strong>Configurações</strong></label>
-					    <input id="switch-shadow13" class="configuracoes switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow13"></label>				    
-					    <label class="is-size-5">Cadastro</label>
-					    <input id="switch-flat12" class="configuracoes switch switch--flat" type="checkbox">
-				  		<label for="switch-flat12"></label>			    
-					    <label class="is-size-5">Opções</label>
-					    <input id="switch-shadow14" class="configuracoes switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow14"></label>   
-				  		<label class="is-size-5">Backup</label>
-					    <input id="switch-flat13" class="configuracoes switch switch--flat" type="checkbox">
-				  		<label for="switch-flat13"></label>			    
-					    <label class="is-size-5">Reportar</label>
-					    <input id="switch-shadow15" class="configuracoes switch switch--shadow" type="checkbox" CHECKED>
-				  		<label for="switch-shadow15"></label>
-					</div>
+	    		<div class="columns">	    			
+					<?php $x=0; $y=0;
+						$checkMenu="SELECT ID, MENU, TAG, ATIVO FROM MENU WHERE ID NOT IN(1,2,3,4,29,30,31,32) GROUP BY MENU ORDER BY POSICAO";
+						$cnx=mysqli_query($phpmyadmin, $checkMenu);						
+						while($loadMenu=$cnx->fetch_array()):{ ?>
+						<div class="column">							
+							<label class="is-size-5"><strong><?php echo $loadMenu["MENU"]?></strong></label>
+					    	<input id="switch-shadow<?php echo $x?>" class="<?php echo $loadMenu["TAG"];?> switch switch--shadow" <?php if($loadMenu["ATIVO"]=="s"){ echo "CHECKED";}?> type="checkbox">
+				  			<label for="switch-shadow<?php echo $x?>"></label><?php
+							$checkItem="SELECT * FROM MENU_ITEM WHERE MENU_ID IN(SELECT ID FROM MENU WHERE MENU='".$loadMenu["MENU"]."') GROUP BY ITEM ORDER BY POSICAO;";
+							$cnx2=mysqli_query($phpmyadmin, $checkItem);
+							while ($loadItem=$cnx2->fetch_array()):{ ?>
+								<label class="is-size-5"><?php echo $loadItem["ITEM"]?></label>
+					    		<input id="switch-flat<?php echo $y?>" class="<?php echo $loadMenu["TAG"];?> switch switch--flat" <?php if($loadItem["ATIVO"]=="s"){ echo "CHECKED";}?> type="checkbox">
+				  				<label for="switch-flat<?php echo $y?>"></label>
+							<?php $y++; }endwhile; ?>
+						</div><?php	$x++;
+						}endwhile;
+					?>					    
 				</div>
 	    		</div>
 	    	</div>	
 	  	</div>
 	</div>
-	<script type="text/javascript">		
+	<script type="text/javascript">
+		var tag2;		
 		$(".meta").click(function(){
 		  let meta = document.querySelectorAll(".meta");		 
 		  for (let i = 0; i < meta.length; i++) {
 		    if (meta[0].checked && i+1 < meta.length){
 		      meta[i+1].removeAttribute("disabled");
+		      tag='meta';
 		    } else {
 		      if (i+1 < meta.length) {
 		        for (let j = i+1; j < meta.length; j++) {
@@ -131,9 +65,11 @@ include('menu.php');
 		          meta[j].setAttribute("disabled", "true");
 		        }
 		      }
-		    }
-		  }
-		});
+		    }		     
+		  }<?php $tag="meta";?>
+		  tag2="meta";
+		  atualizaMenu();		  
+		});		
 		$(".desempenho").click(function(){
 		  let desempenho = document.querySelectorAll(".desempenho");		 
 		  for (let i = 0; i < desempenho.length; i++) {
@@ -147,7 +83,9 @@ include('menu.php');
 		        }
 		      }
 		    }
-		  }
+		  }<?php $tag="desempenho";?>
+		  tag2="desempenho";
+		  atualizaMenu();	
 		});
 		$(".feedback").click(function(){
 		  let feedback = document.querySelectorAll(".feedback");		 
@@ -162,13 +100,16 @@ include('menu.php');
 		        }
 		      }
 		    }
-		  }
+		  }<?php $tag="feedback";?>
+		  tag2="feedback";
+		  atualizaMenu();	
 		});
 		$(".relatorios").click(function(){
 		  let relatorios = document.querySelectorAll(".relatorios");		 
 		  for (let i = 0; i < relatorios.length; i++) {
 		    if (relatorios[0].checked && i+1 < relatorios.length){
 		      relatorios[i+1].removeAttribute("disabled");
+		      tag="relatorios";
 		    } else {
 		      if (i+1 < relatorios.length) {
 		        for (let j = i+1; j < relatorios.length; j++) {
@@ -176,14 +117,17 @@ include('menu.php');
 		          relatorios[j].setAttribute("disabled", "true");
 		        }
 		      }
-		    }
-		  }
+		    }		    
+		  }<?php $tag="relatorios";?>
+		  tag2="relatorios";
+		  atualizaMenu();	
 		});
 		$(".configuracoes").click(function(){
 		  let configuracoes = document.querySelectorAll(".configuracoes");		 
 		  for (let i = 0; i < configuracoes.length; i++) {
 		    if (configuracoes[0].checked && i+1 < configuracoes.length){
 		      configuracoes[i+1].removeAttribute("disabled");
+		      <?php $tag="configuracoes2";?>
 		    } else {
 		      if (i+1 < configuracoes.length) {
 		        for (let j = i+1; j < configuracoes.length; j++) {
@@ -191,9 +135,35 @@ include('menu.php');
 		          configuracoes[j].setAttribute("disabled", "true");
 		        }
 		      }
-		    }
+		    }		    
 		  }
+		  tag2="configuracoes";
+		  atualizaMenu();		  
 		});
+		function atualizaMenu(){
+			alert('<?php echo $tag;?>');
+			alert(tag2);			
+			<?php
+			$test=?>tag2<?php
+			var_dump($test); 
+			$upMenu="UPDATE MENU SET ATIVO='s' WHERE TAG='".$test."'";
+			$cnx=mysqli_query($phpmyadmin, $upMenu);
+			$upItem="UPDATE MENU_ITEM SET ATIVO='s' WHERE MENU_ID IN(SELECT ID FROM MENU WHERE TAG='".$test."');";
+			$cnx=mysqli_query($phpmyadmin, $upItem); echo "TESTE";
+			?>						
+			//location.replace(location.href);
+		}
 	</script>
 </body>
 </html>
+<?php 
+function AtualizarMenu2(){
+	$upMenu="UPDATE MENU SET ATIVO='n' WHERE MENU=Meta";
+	$cnx=mysqli_query($phpmyadmin, $upMenu);
+	echo "CHAMADA DE FUNCAO";
+}
+function meuA(){
+	echo "<script>alert('TESTE55')</script>";
+}
+
+?>
