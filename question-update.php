8cb8b025aecd7d4594bac26c0efcdc2083ce75d0
Lwcyano <lwcyano.will@evino.com.br>
2020-03-19 16:10:34 -0300

    Adição de verificação se há perguntas p/ atualizar.

--- question-update.php
+++ question-update.php
@@ -1,6 +1,6 @@
 <?php 
-$menuAtivo="Configurações";
-include('menu.php');
+$menuAtivo="configuracoes";
+require('menu.php');
 if($_SESSION["permissao"]==1){
 	echo "<script>alert('Usuário sem permissão')</script>";
 	header("Refresh:1;url=home.php");
@@ -17,7 +17,8 @@ else{
 <body>
 	<section class="section">
 	  	<div class="container">
-	   		<form action="question-insert.php" method="POST">
+	  		<?php if(isset($_POST["consultar"])==null):?>
+	   		<form action="question-update.php" method="POST">
 	    		<div class="field is-horizontal">
 					<div class="field-label is-normal">
 						<label class="label">Tipo:</label>
@@ -68,50 +69,172 @@ else{
 							<div class="control">
 								<div class="select">
 									<select name="situacao" id="situacao">
-										<option selected="selected" value="Ativo">Ativo</option>
-										<option value="Inativo">Inativo</option>
+										<option selected="selected" value="AND SITUACAO='Ativo'">Ativo</option>
+										<option value="AND SITUACAO='Inativo'">Inativo</option>
+										<option value="">Todas</option>
 									</select>	
 								</div>
 							</div>
 							<div class="control">
 								<button name="consultar" type="submit" class="button is-primary" value="Filtrar">Consultar</button>
-							</div>						
+							</div>
+							<div>
+						  		<a href="register.php" class="button is-primary" type="button">Voltar</a>
+						  	</div>						
 						</div>
 					</div>					
 				</div>
-	     	</form>	     	
+	     	</form><?php endif;
+	     	if(isset($_POST["consultar"])!=null){
+				$tipo=trim($_POST['tipo']);
+				$cargo=trim($_POST['cargo']);
+				$situacao=trim($_POST['situacao']);
+				if($tipo==1){//OS ID REFERÊNCIA P/ TÉCNICO E COMPORTAMENTAL.
+					$tecnica=1;
+					$comportamental=2;
+				}
+				else{
+					$tecnica=3;
+					$comportamental=4;
+				}
+				if($cargo!="" ):{?>
+					
+					<form id="form2" action="" method="POST">	
+					<div class="box">Técnicas</div><?php $x=0; $y=0;		
+					$getPergunta="SELECT ID, PERGUNTA, SITUACAO FROM AVAL_PERGUNTA WHERE AVAL_TIPO_PERGUNTA_ID=".$tecnica." AND CARGO_ID=".$cargo." ".$situacao." ORDER BY ORDEM;";//VERIFICA SE HÁ PERGUNTA P/ CARGO DO USUÁRIO.
+					$cnx=mysqli_query($phpmyadmin, $getPergunta);
+					if(mysqli_num_rows($cnx)>0):{
+					while ($pergunta=$cnx->fetch_array()):{ ?>
+						<div class="box">
+							<input type="hidden" name="idTec[]" value="<?php echo $pergunta["ID"];?>">
+							<div class="field">						
+								<div class="control">
+									<input name="tecnica[]" type="text" class="input" placeholder="<?php echo $pergunta["PERGUNTA"];?>" maxlength="1000" value="<?php echo $pergunta["PERGUNTA"];?>">
+								</div>			
+							</div>							
+							<div class="control">
+								<label>Situação:</label>
+								<div class="select">
+									<select name="situacaoTec[]" id="situacao">
+										<?php if($pergunta["SITUACAO"]=="Ativo"):{?>
+										<option selected="selected" value="Ativo">Ativo</option>
+										<option value="Inativo">Inativo</option><?php }endif; ?>
+										<?php if($pergunta["SITUACAO"]=="Inativo"):{?>
+										<option selected="selected" value="Inativo">Inativo</option>
+										<option value="Ativo">Ativo</option><?php }endif; ?>
+									</select>	
+								</div>
+							</div>																				
+						</div>
+					<?php $x++;}endwhile; }endif; ?>	<!--PERGUNTA COMPORTAMENTAL-->
+					<div class="box">Comportamentais</div><?php
+					$getPergunta="SELECT ID, PERGUNTA, SITUACAO FROM AVAL_PERGUNTA WHERE AVAL_TIPO_PERGUNTA_ID=".$comportamental." AND CARGO_ID=".$cargo." ".$situacao." ORDER BY ORDEM;";
+					$cnx2=mysqli_query($phpmyadmin, $getPergunta);
+					if(mysqli_num_rows($cnx2)>0):{
+					while ($pergunta=$cnx2->fetch_array()):{ ?>
+						<div class="box">
+							<input type="hidden" name="idCom[]" value="<?php echo $pergunta["ID"];?>">
+							<div class="field">						
+								<div class="control">
+									<input name="comportamental[]" type="text" class="input" placeholder="<?php echo $pergunta["PERGUNTA"];?>" maxlength="1000" value="<?php echo $pergunta["PERGUNTA"];?>">
+								</div>			
+							</div>
+							<div class="control">
+								<label>Situação:</label>
+								<div class="select">
+									<select name="situacaoCom[]" id="situacao">
+										<?php if($pergunta["SITUACAO"]=="Ativo"):{?>
+										<option selected="selected" value="Ativo">Ativo</option>
+										<option value="Inativo">Inativo</option><?php }endif; ?>
+										<?php if($pergunta["SITUACAO"]=="Inativo"):{?>
+										<option selected="selected" value="Inativo">Inativo</option>
+										<option value="Ativo">Ativo</option><?php }endif; ?>
+									</select>	
+								</div>
+							</div>																				
+						</div>
+					<?php $y++;}endwhile; }endif; //<!--FINAL PERGUNTA COMPORTAMENTAL-->
+					$getPerComentario="SELECT ID, PERGUNTA, SITUACAO FROM AVAL_PERGUNTA_COM WHERE AVAL_TIPO_ID=2 ".$situacao;
+					$cnx3=mysqli_query($phpmyadmin, $getPerComentario);
+					$getComentario=$cnx3->fetch_array();			
+					?>
+					<div class="box">
+						<label>Comentário</label>
+					</div>	
+					<div class="box">
+						<input type="hidden" name="idCome" value="<?php echo $getComentario["ID"];?>">	
+						<div class="field">
+							<input type="text" name="comentario" class="input" value="<?php echo $getComentario["PERGUNTA"];?>">
+						</div>
+						<div class="control">
+								<label>Situação:</label>
+								<div class="select">
+									<select name="situacaoCome" id="situacao">
+										<?php if($getComentario["SITUACAO"]=="Ativo"):{?>
+										<option selected="selected" value="Ativo">Ativo</option>
+										<option value="Inativo">Inativo</option><?php }endif; ?>
+										<?php if($getComentario["SITUACAO"]=="Inativo"):{?>
+										<option selected="selected" value="Inativo">Inativo</option>
+										<option value="Ativo">Ativo</option><?php }endif; ?>
+									</select>	
+								</div>
+							</div>						
+					</div>
+					<div class="field is-grouped">						  	
+						<div class="control">
+						   	<button class="button is-link is-light" name="atualizar" type="submit">Atualizar</button>
+						</div>
+					 	<div  class="control">
+					  		<a href="question-update.php" class="button is-primary" type="button">Voltar</a>
+					  	</div>
+					</div>									
+					</form>
+				<?php }endif;				
+				if($cargo==null){
+					echo "<script>alert('A seleção do Cargo é obrigatório!!'); window.location.href='question-update.php';</script>";
+				}
+				else{
+					if(mysqli_num_rows($cnx)==0 && mysqli_num_rows($cnx2)==0 && mysqli_num_rows($cnx3)==0){					
+					echo "<script>alert('Nenhuma pergunta foi encontrada com o filtro aplicado!'); window.location.href='question-query.php';</script>";
+
+					}
+					else if(mysqli_num_rows($cnx)==0 && mysqli_num_rows($cnx2)==0 && mysqli_num_rows($cnx3)>0){					
+						echo "<script>alert('Nenhuma pergunta Técnica ou Comportamental encontrada com o filtro aplicado!');</script>";
+					}
+				}	
+			}
+	     	?>	     	
 	   	</div>
 	</section>	 	
 </body>
-</html><?php 
-if(isset($_POST["inserirPergunta"])!=null){
-$tipo=trim($_POST['tipo']);
-$classificacao=trim($_POST['classificacao']);
-$resposta=trim($_POST['resposta']);
-$cargo=trim($_POST['cargo']);
-$pergunta=trim($_POST['pergunta']);
-$situacao=trim($_POST['situacao']);
-	if($cargo!="" && $pergunta!=""){
-		if($resposta=="checkbox"){
-			if($tipo==2){
-				$classificacao=$classificacao+2;
+</html><?php
+	if(isset($_POST["atualizar"])!=null){
+		$tecnicas=array_filter($_POST['tecnica']);
+		$comportamentais=array_filter($_POST['comportamental']);
+		$situacaoTec=array_filter($_POST['situacaoTec']);
+		$situacaoCom=array_filter($_POST['situacaoCom']);
+		$idTec=array_filter($_POST['idTec']);
+		$idCom=array_filter($_POST['idCom']);
+		$idCome=trim($_POST['idCome']);
+		$comentario=trim($_POST['comentario']);
+		$situacaoCome=trim($_POST['situacaoCome']);
+		$x=0; $y=0;
+		if($idTec!=null){
+			while ($x < sizeof($idTec)) {
+				$upPerguntaTec="UPDATE AVAL_PERGUNTA SET PERGUNTA='".$tecnicas[$x]."', SITUACAO='".$situacaoTec[$x]."' WHERE ID=".$idTec[$x].";";
+				mysqli_query($phpmyadmin, $upPerguntaTec);
+				$x++;
 			}
-			$insPergunta="INSERT INTO AVAL_PERGUNTA(AVAL_TIPO_PERGUNTA_ID, CARGO_ID, PERGUNTA, SITUACAO) VALUES(".$classificacao.",".$cargo.",'".$pergunta."','".$situacao."');";			
 		}
-		else{
-			$insPergunta="INSERT INTO AVAL_PERGUNTA_COM(AVAL_TIPO_ID, CARGO_ID, PERGUNTA, SITUACAO) VALUES(".$tipo.",'".$pergunta."','".$situacao."');";
-		}
-		echo $insPergunta;
-		$cnx= mysqli_query($phpmyadmin, $insPergunta);		
-		echo "<script>alert('Pegunta cadastrada com sucesso.')</script>";	
+		if($idTec!=null){
+			while ($y < sizeof($idCom)) {
+				$upPerguntaCom="UPDATE AVAL_PERGUNTA SET PERGUNTA='".$comportamentais[$y]."', SITUACAO='".$situacaoCom[$y]."' WHERE ID=".$idCom[$y].";";
+				mysqli_query($phpmyadmin, $upPerguntaCom);
+				$y++;
+			}
+		}	
+		$upComentario="UPDATE AVAL_PERGUNTA_COM SET PERGUNTA='".$comentario."', SITUACAO='".$situacaoCome."' WHERE ID=".$idCome.";";
+		mysqli_query($phpmyadmin, $upComentario);
+		echo "<script>alert('Atualização realizada com sucesso.')</script>";		
 	}
-	else if($cargo==""){
-		echo "<script>alert('A seleção do Cargo é obrigatório!!')</script>";
-	}
-	else{
-		echo "<script>alert('Preencher o campo Pergunta é obrigatório!!')</script>";
-	}	
-}
-
-
 }//ELSE - caso o usuário tenha permissão.
