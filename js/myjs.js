8d6969a1acb40458cfa8db2cd851bfe86dd6a413
Lwcyano <lwcyano.will@evino.com.br>
2020-03-27 15:40:07 -0300

    Adição de função para validar cadastro de endereço.

--- js/myjs.js
+++ js/myjs.js
@@ -1,15 +1,21 @@
 function upPlaceholder(selecao){//função usada no cadastro de usuário.
 	if(selecao=="MATRICULA="){
 		document.getElementById("filtro").placeholder="629";
+		//document.getElementById("filtro").classList.add("numero");//adição de mascara restrigir a número.
 	}
 	else if(selecao=="LOGIN="){
-		document.getElementById("filtro").placeholder="harry.will";
+		id=document.getElementById("filtro");
+		//id.classList.remove("numero");//adição de mascara restrigir a número.
+		id.placeholder="harry.will";
+		
 	}
 	else if(selecao=="NOME LIKE"){
 		document.getElementById("filtro").placeholder="Harry Will ou Harry%";
+		document.getElementById("filtro").classList.remove("numero");//adição de mascara restrigir a número.
 	}
 	else{
 		document.getElementById("filtro").placeholder="willvix@outlook.com ou will%";
+		document.getElementById("filtro").classList.remove("numero");//adição de mascara restrigir a número.
 	}
 }
 function validacaoEmail(field) {//função usada no cadastro de usuário.
@@ -29,7 +35,9 @@ function validacaoEmail(field) {//função usada no cadastro de usuário.
 	}
 	else{
 		document.getElementById("msgemail").style.display="block";
-		document.getElementById("email").classList.add("is-danger");		
+		document.getElementById("email").classList.add("is-danger");
+		document.getElementById("iconMail").classList.remove("fas fa-envelope");
+		document.getElementById("iconMail").classList.add("fas fa-fw");		
 	}
 
 }
@@ -38,4 +46,18 @@ function addLoadField(id){//função p/ adicionar icone de carregamento ao digit
 }
 function rmvLoadField(id){//função p/ remover icone de carregamento ao parar de digitar.
     document.getElementById(""+id).classList.remove("is-loading");
+}
+function checkAdress(field, msgOk, msgNok){//função p/ ativar ou desativar icones e cores nos campos.
+	if(field.value.length>0){
+		document.getElementById(""+msgOk).style.display="block";
+		document.getElementById(""+msgNok).style.display="none";
+		document.getElementById(""+field.id).classList.remove("is-danger");
+		document.getElementById(""+field.id).classList.add("is-success");
+	}
+	else{
+		document.getElementById(""+msgNok).style.display="block";
+		document.getElementById(""+msgOk).style.display="none";
+		document.getElementById(""+field.id).classList.remove("is-success");
+		document.getElementById(""+field.id).classList.add("is-danger");
+	}
 }
