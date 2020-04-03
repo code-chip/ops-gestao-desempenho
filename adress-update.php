c64d3f733cbb74c1844fb5465d9babf1d7066b13
Lwcyano <lwcyano.will@evino.com.br>
2020-04-03 12:58:49 -0300

    Atualização de icone no topo da página.

--- adress-update.php
+++ adress-update.php
@@ -56,7 +56,7 @@ if($car==1){
 	<main>
 	<form id="form1" action="adress-update.php" method="POST" onsubmit="return checkForm(form1.cidade.value)">
 	<div class="layout">
-	<h3 class="title"><i class="fas fa-plus-square"></i>&nbsp&nbsp  ENDEREÇO</h3>
+	<h3 class="title"><i class="fas fa-edit"></i>&nbsp&nbsp  ENDEREÇO</h3>
 	<h3 class="label"><?php echo $_SESSION["filter"][2];?></h3>
 	<hr>	
 	<div class="field is-horizontal" style="margin-bottom: -7px;"><!--DIVISÃO EM HORIZONTAL-->
