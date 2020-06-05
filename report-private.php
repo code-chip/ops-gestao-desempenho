10c766ab7464c62e754d1769af54dba05d7c6da5
code-chip <willvix@outlook.com>
2020-06-04 22:50:00 -0300

    Code optimization

--- report-private.php
+++ report-private.php
@@ -1,36 +1,41 @@
 <?php
-$menuAtivo="Relatórios";
-include('menu.php');
+
+$menuAtivo = "relatorios";
+require('menu.php');
 $contador = 0;
-$totalDesempenho=0;
-?>
-<!DOCTYPE html>
+$totalDesempenho = 0;
+
+?><!DOCTYPE html>
 <html>
 <head>
 	<title>Gestão de Desempenho - Relatório Individual</title>
+	<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 <body>
 <span id="topo"></span>
 <section class="section">
 <div class="container">
-	<form id="form1" action="report-private.php" method="GET" >
+	<form id="form1" action="report-private.php" method="POST" >
 		<div class="field is-horizontal">
 			<div class="field-label is-normal"><!--SELEÇÃO PERÍODO-->
 				<label class="label" for="periodo">Período:</label>
 			</div>
 			<div class="field-body">
 				<div class="field is-grouped">							
-					<div class="control">
-						<div class="select">
-							<select name="periodo">
-								<option value="<?php echo date('Y-m', strtotime("+1 months"))?>"><?php echo date('m/Y', strtotime("+1 months"))?></option>
-								<option selected="selected" value="<?php echo date('Y-m')?>"><?php echo date('m/Y')?></option>
-								<option value="<?php echo date('Y-m', strtotime("-1 months"))?>"><?php echo date('m/Y', strtotime("-1 months"))?></option>
-								<option value="<?php echo date('Y-m', strtotime("-2 months"))?>"><?php echo date('m/Y', strtotime("-2 months"))?></option>
-								<option value="<?php echo date('Y-m', strtotime("-3 months"))?>"><?php echo date('m/Y', strtotime("-3 months"))?></option>
-								<option value="<?php echo date('Y-m', strtotime("-4 months"))?>"><?php echo date('m/Y', strtotime("-4 months"))?></option>
-								<option value="<?php echo date('Y-m', strtotime("-5 months"))?>"><?php echo date('m/Y', strtotime("-5 months"))?></option>
-							</select>		
+					<div class="control has-icons-left">
+						<div class="select is-size-7-touch">
+							<select name="periodo" id="saveDate" style="width: 10em">
+								<option selected="selected" value="<?php echo date('Y-m')?>"><?php echo date('m/Y', strtotime("+1 months"))?></option>
+								<option value="<?php echo date('Y-m', strtotime("-1 months"))?>"><?php echo date('m/Y')?></option>
+								<option value="<?php echo date('Y-m', strtotime("-2 months"))?>"><?php echo date('m/Y', strtotime("-1 months"))?></option>
+								<option value="<?php echo date('Y-m', strtotime("-3 months"))?>"><?php echo date('m/Y', strtotime("-2 months"))?></option>
+								<option value="<?php echo date('Y-m', strtotime("-4 months"))?>"><?php echo date('m/Y', strtotime("-3 months"))?></option>
+								<option value="<?php echo date('Y-m', strtotime("-5 months"))?>"><?php echo date('m/Y', strtotime("-4 months"))?></option>
+								<option value="<?php echo date('Y-m', strtotime("-6 months"))?>"><?php echo date('m/Y', strtotime("-5 months"))?></option>
+							</select>
+							<span class="icon is-small is-left">
+								<i class="fas fa-calendar-alt"></i>
+							</span>	
 						</div>
 					</div>
 				</div>
@@ -40,7 +45,7 @@ $totalDesempenho=0;
 			</div>
 			<div class="field-body">
 				<div class="field is-grouped">							
-					<div class="control">
+					<div class="control has-icons-left">
 						<div class="select">
 							<select name="atividade">
 								<option selected="selected" value="">Todas</option>	
@@ -50,6 +55,9 @@ $totalDesempenho=0;
 									<option value="AND A.ID=<?php echo $vtId[$x]; ?>"><?php echo $vtNome[$x] = $atividade["NOME"]; ?></option>
 								<?php $x;} endwhile;?>								
 							</select>
+							<span class="icon is-small is-left">
+								<i class="fas fa-diagnoses"></i>
+							</span>
 						</div>
 					</div>					
 				</div>						
@@ -76,24 +84,26 @@ $totalDesempenho=0;
 				</div>						
 			</div>
 		</div>
-	</form>		
-</div>
-<!--FINAL DO FORMULÁRIO-->
-<?php
+	</form><!--FINAL DO FORMULÁRIO-->		
+</div><?php
+
 $periodo = trim($_REQUEST['periodo']);
 $atividade = trim($_REQUEST['atividade']);
 $meta = trim($_REQUEST['meta']);
-if( $periodo != ""){
-	$consulta="SELECT U.NOME AS NOME, A.NOME AS ATIVIDADE, P.NOME AS PRESENCA, D.META, D.ALCANCADO, D.DESEMPENHO, D.REGISTRO, D.OBSERVACAO, D.PRESENCA_ID FROM DESEMPENHO D
-	INNER JOIN USUARIO U ON U.ID=D.USUARIO_ID
-	INNER JOIN ATIVIDADE A ON A.ID=D.ATIVIDADE_ID
-	INNER JOIN PRESENCA P ON P.ID=D.PRESENCA_ID
-	WHERE D.USUARIO_ID=".$_SESSION["userId"]." AND REGISTRO>=DATE_SUB('".$periodo."-21', INTERVAL 1 MONTH) AND REGISTRO<='".$periodo."-20'".$atividade."".$meta."
-	ORDER BY REGISTRO;";	
-	$con = mysqli_query($phpmyadmin, $consulta);
-	$x=0; $falta=0; $folga=0; $menor=1000; $maior=0; $atestado=0; $treinamento=0;
-	while($dado = $con->fetch_array()){
-		$nome= $dado["NOME"];		
+
+if ( $periodo != "") {
+	
+	$con = mysqli_query($phpmyadmin, "SELECT U.NOME AS NOME, A.NOME AS ATIVIDADE, P.NOME AS PRESENCA, D.META, D.ALCANCADO, D.DESEMPENHO, D.REGISTRO, D.OBSERVACAO, D.PRESENCA_ID FROM DESEMPENHO D INNER JOIN USUARIO U ON U.ID=D.USUARIO_ID INNER JOIN ATIVIDADE A ON A.ID=D.ATIVIDADE_ID INNER JOIN PRESENCA P ON P.ID=D.PRESENCA_ID WHERE D.USUARIO_ID =" . $_SESSION["userId"] . " AND ANO_MES = '" . $periodo . "'" . $atividade . "" . $meta . " ORDER BY REGISTRO;");
+	$x = 0; 
+	$falta = 0; 
+	$folga = 0; 
+	$menor = 1000; 
+	$maior = 0; 
+	$atestado = 0; 
+	$treinamento = 0;
+	
+	while ($dado = $con->fetch_array()) {
+		$nome = $dado["NOME"];		
 		$vetorAtividade[$x] = $dado["ATIVIDADE"];
 		$vetorIdPresenca[$x] = $dado["PRESENCA_ID"];
 		$vetorPresenca[$x] = $dado["PRESENCA"];
@@ -103,35 +113,35 @@ if( $periodo != ""){
 		$totalDesempenho=$totalDesempenho+$dado["DESEMPENHO"];
 		$vetorRegistro[$x] = $dado["REGISTRO"];
 		$vetorObservacao[$x] = $dado["OBSERVACAO"];
-		if($vetorIdPresenca[$x]==2){
+		
+		if ($vetorIdPresenca[$x] == 2) {
 			$falta++;
-		}
-		else if($vetorIdPresenca[$x]==3){
+		} else if ($vetorIdPresenca[$x] == 3) {
 			$folga++;
-		}
-		else if($vetorIdPresenca[$x]==4){
+		} else if ($vetorIdPresenca[$x] == 4) {
 			$atestado++;
-		}
-		else if($vetorIdPresenca[$x]==5){
+		} else if ($vetorIdPresenca[$x] == 5) {
 			$treinamento++;
 		}
-		if($maior<$vetorDesempenho[$x]){
-			$maior=$vetorDesempenho[$x];
+
+		if ($maior < $vetorDesempenho[$x]) {
+			$maior = $vetorDesempenho[$x];
 		}
-		if($menor>$vetorDesempenho[$x] && $vetorDesempenho[$x]>0){
-			$menor=$vetorDesempenho[$x];
+
+		if ($menor > $vetorDesempenho[$x] && $vetorDesempenho[$x] > 0) {
+			$menor = $vetorDesempenho[$x];
 		}
+
 		$contador++;
 		$x++;		
 	}
-	if($contador==0){
-		?><script type="text/javascript">alert('Nenhum resultado encontrado!');</script><?php
+
+	if ($contador == 0) {
+		echo "<script>alert('Nenhum resultado encontrado!'); </script>";
 	}
-}
-else{
-	
-}?>
-<?php if( $periodo !="" && $contador !=0) : ?>
+} 
+
+if ( $periodo != "" && $contador != 0) : ?>
 <hr/>
 	<div class="table__wrapper">
 	<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth is-size-7-touch table__wrapper .scrollWrapper">
@@ -147,26 +157,33 @@ else{
 	</table>
 	<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth is-size-7-touch table__wrapper .scrollWrapper">	
 	<tr>
-		<th style='width:55px;'>Atividade</th>
-		<?php if($falta>0 || $folga>0 || $atestado>0): ?><th style='width:55px;'>Presença</th><?php endif;?>		
-		<th style='width:30px;'>Meta</th>
+		<th style='width:55px;'>Atividade</th><?php
+		if ($falta > 0 || $folga > 0 || $atestado > 0) { 
+			echo "<th style='width:55px;'>Presença</th>";
+		}		
+		
+		echo "<th style='width:30px;'>Meta</th>
 		<th style='width:40px;'>Alacançado</th>
 		<th style='width:45px;'>Desempenho</th>
 		<th style='width:70px;'>Data</th>
 		<th style='width:350px;'>Observação</th>
-	</tr>
-<?php for( $i = 0; $i < sizeof($vetorAtividade); $i++ ) : ?>
-	<tr>
-		<td><?php echo $vetorAtividade[$i]?></td>
-		<?php if($falta>0 || $folga>0 || $atestado>0): ?><td><?php echo $vetorPresenca[$i];?></td><?php endif;?>			
-		<td><?php echo $vetorMeta[$i]?></td>
-		<td><?php echo $vetorAlcancado[$i]?></td>
-		<td><?php echo $vetorDesempenho[$i]."%"?></td>
-		<td><?php echo $vetorRegistro[$i]?></td>
-		<td><?php echo $vetorObservacao[$i]?></td>			
-	</tr>
-<?php endfor; ?>
-	</table>
+	</tr>";
+
+	for ( $i = 0; $i < sizeof($vetorAtividade); $i++ ) {
+		echo "<tr>
+			<td>" . $vetorAtividade[$i] . "</td>";
+			if ($falta > 0 || $folga > 0 || $atestado > 0) { 
+				echo "<td>" . $vetorPresenca[$i] . "</td>";
+			}
+			echo "<td>" . $vetorMeta[$i] . "</td>
+			<td>" . $vetorAlcancado[$i] . "</td>
+			<td>" . $vetorDesempenho[$i]."%" . "</td>
+			<td>" . $vetorRegistro[$i] . "</td>
+			<td>" . $vetorObservacao[$i] . "</td>			
+		</tr>";
+	}
+
+	?></table>
 	<a href="#topo">
 		<div class=".scrollWrapper">
 			<button class="button is-primary is-fullwidth is-size-7-touch">Ir Ao Topo</button>		
