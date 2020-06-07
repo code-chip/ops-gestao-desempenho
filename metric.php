990a242e793fb08ed62f683744eb621532dc0caa
Code Chip <willvix@outlook.com>
2020-06-07 15:16:42 -0300

    Update link button.

--- metric.php
+++ metric.php
@@ -63,7 +63,7 @@ array_push($_SESSION["filter"], trim($_GET['descricao']), trim($_GET['link']));
 <div class="field has-addons has-addons-centered">
 	<div class="buttons">								
 		<button class="button is-white is-primary is-outlined mw12" value="goal-weight-insert.php" onclick="loadPage(this.value)">Inserir Peso</button>
-		<button class="button is-white is-primary is-outlined mw12" value="goal-weight-query.php, Consultar Peso" onclick="redirect(this.value)">Consultar Peso</button>
+		<button class="button is-white is-primary is-outlined mw12" value="goal-weight-query.php" onclick="loadPage(this.value)">Consultar Peso</button>
 		<button class="button is-white is-primary is-outlined mw12" value="goal-weight-update.php" onclick="loadPage(this.value)">Atualizar Peso</button>
 		<?php if($_SESSION["permissao"]!=1):?>
 		<button class="button is-white is-primary is-outlined mw12" value="goal-weight-remove.php, Remover Peso" onclick="redirect(this.value)">Remover Peso</button><?php endif;?>			
