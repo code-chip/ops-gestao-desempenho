ff2939a2f8dbeff0e4b0b8dd2395f0ee9f5da24e
Code Chip <willvix@outlook.com>
2020-07-10 18:10:57 -0300

    Add code.

--- penalty-update.php
+++ penalty-update.php
@@ -28,8 +28,8 @@ if ($_SESSION["permissao"] == 1){
 </head>
 <body>
 	<section class="section">
+	<?php if (!isset($_POST['pesquisar'])) : ?>	
 	  	<div class="container">
-	  		<?php if (!isset($POST['consultar'])) : ?>
 	  		<form action="penalty-update.php" method="POST" id="form1">
 	  			<div class="field is-horizontal">
 					<div class="field-label is-normal">
@@ -107,7 +107,7 @@ if ($_SESSION["permissao"] == 1){
 					<div class="field-body">
 						<div class="field is-grouped">							
 							<div class="control">
-								<button name="inserirPenalidade" type="submit" class="button is-primary" value="inserir">Pesquisar</button>
+								<button name="pesquisar" type="submit" class="button is-primary" value="pesquisar">Pesquisar</button>
 							</div>
 							<div class="control">
 								<button name="limpar" type="reset" class="button is-primary">Limpar</button>
@@ -119,54 +119,7 @@ if ($_SESSION["permissao"] == 1){
 					</div>
 				</div>
 	  		</form>
-	  		<?php endif ?>
-	   			    			
-				<div class="field is-horizontal">
-					<div class="field-label is-normal">
-						<label class="label">Penalidade:</label>
-					</div>
-					<div class="field-body">
-						<div class="field" >							
-							<div class="control has-icons-left">
-								<div class="select is-size-7-touch">
-									<select name="penalidade" style="width:24.2em;"><?php
-									$con = mysqli_query($phpmyadmin ,"SELECT ID, TIPO FROM PENALIDADE_TIPO WHERE SITUACAO = 's' ORDER BY TIPO");  
-									while($penalidade = $con->fetch_array()){
-										echo "<option value=" . $penalidade["ID"] . ">" . $penalidade["TIPO"] . "</option>";
-									} ?>
-									</select>
-									<span class="icon is-small is-left">
-							   		<i class="fas fa-angle-double-down"></i>
-								   	</span>
-								</div>
-							</div>						
-						</div>
-					</div>
-				</div>
-				
-			<form action="penalty-update.php" method="POST" onsubmit="return check()" id="form2">
-				<table>
-					
-				</table>>
-				<div class="field is-horizontal">
-					<div class="field-label is-normal">
-					</div>
-					<div class="field-body">
-						<div class="field is-grouped">							
-							<div class="control">
-								<button name="inserirPenalidade" type="submit" class="button is-primary" value="inserir">Inserir</button>
-							</div>
-							<div class="control">
-								<button name="limpar" type="reset" class="button is-primary">Limpar</button>
-							</div>
-							<div class="control">
-								<a href="home.php" class="button is-primary">Cancelar</a>
-							</div>						
-						</div>
-					</div>
-				</div>	
-	     	</form>
-	     	<script type="text/javascript" scr="https://www.google.com/jsapi"></script>
+	  		<script type="text/javascript" scr="https://www.google.com/jsapi"></script>
 	     	<script type="text/javascript">
 	     		google.loader("jquery", "1.4.2");
 	     	</script>
@@ -191,18 +144,114 @@ if ($_SESSION["permissao"] == 1){
 				});
 			</script>
 	   	</div>
-	</section>	 	
+	<?php echo "</section>";
+	
+	endif;
+
+	if (isset($_POST['pesquisar'])) {
+		if($_POST['usuario'] != null) {
+			$filter = "SELECT P.*, PT.TIPO, PT.PENALIDADE, U.NOME FROM PENALIDADE P INNER JOIN PENALIDADE_TIPO PT ON PT.ID = P.PENALIDADE_TIPO_ID INNER JOIN USUARIO U ON U.ID = P.USUARIO_ID WHERE USUARIO_ID = " . $_POST['usuario'] . " AND ANO_MES = '" . $_POST['anoMes'] . "';";
+		} else {
+			$filter = "SELECT P.*, PT.TIPO, PT.PENALIDADE, U.NOME FROM PENALIDADE P INNER JOIN PENALIDADE_TIPO PT ON PT.ID = P.PENALIDADE_TIPO_ID WHERE USUARIO_ID IN (SELECT ID FROM USUARIO INNER JOIN USUARIO U ON U.ID = P.USUARIO_ID WHERE SETOR_ID = " . $_POST['setor'] . ") AND ANO_MES = '" . $_POST['anoMes'] . "';";
+		}
+		
+		$cnx = mysqli_query($phpmyadmin, $filter);
+
+		if (isset($_POST['pesquisar']) && mysqli_num_rows($cnx) == 0) {
+			echo "<script>alert('Nenhum registrado encontrado com o filtro utilizado.'); window.location.href='penalty-update.php';</script>";
+		}
+
+		$x = 0;
+		while ($dice = $cnx->fetch_array()) {
+			$id[$x] = $dice['ID'];
+			$idType[$x] = $dice['PENALIDADE_TIPO_ID'];
+			$idUser[$x] = $dice['USUARIO_ID'];
+			$name[$x] = $dice['NOME'];
+			$occurence[$x] = $dice['OCORRENCIA'];
+			$penalty[$x] = $dice['PENALIDADE'];			
+			$penaltyTotal[$x] = $dice['PENALIDADE_TOTAL'];
+			$yearMonth[$x] = $dice['ANO_MES'];
+			$observation[$x] = $dice['OBSERVACAO'];
+			$register[$x] = $dice['REGISTRO'];
+			$type[$x] = $dice['TIPO'];
+			$x++;
+		}
+	}
+	$cnx2 = mysqli_query($phpmyadmin, "SELECT ID, TIPO FROM PENALIDADE_TIPO");
+	while ($dice = $cnx2->fetch_array()) {
+		$idType[$y] = $dice['ID'];
+		$nameType[$y] = $dice['TIPO'];
+	}
+
+	if ($x > 0) : { ?>
+		<form action="penalty-update.php" method="POST" onsubmit="return check()" id="form2">
+			<table class="table__wrapper table is-bordered is-striped is-narrow is-hoverable is-fullwidth is-size-7-touch scrollWrapper">
+				<tr><td>N°</td>
+				<td>Nome</td>
+				<td>Tipo</td>
+				<td>Penalidade</td>
+				<td>Ocorrencia's</td>
+				<td>Total</td>
+				<td>Registro</td>
+				<td>Observação</td></tr>
+				<?php
+
+				$i = 0;
+				$z = 0;
+				while ( $i < $x) {
+					if ($type[$i] == $idType[$z]) {
+						echo "<tr><td>" . $idUser[$i] . "</td>
+						<td>" . $name[$i] . "</td>
+						<td>" . $type[$i] . "</td>
+						<td>" . $penalty[$i] . "</td>
+						<td>" . $penaltyTotal[$i] . "</td>
+						<td>" . $occurence[$i] . "</td>
+						<td>" . $register[$i] . "</td>
+						<td>" . $observation[$i] . "</td></tr>";
+						$i++;
+						$z = 0;
+					} else {
+						$z++;
+					}	
+				}
+					
+			?>
+			</table>
+			<div class="field is-horizontal">
+				<div class="field-label is-normal">
+				</div>
+				<div class="field-body">
+					<div class="field is-grouped">							
+						<div class="control">
+							<button name="inserirPenalidade" type="submit" class="button is-primary" value="inserir">Atualizar</button>
+						</div>
+						<div class="control">
+							<button name="limpar" type="reset" class="button is-primary">Limpar</button>
+						</div>
+						<div class="control">
+							<a href="home.php" class="button is-primary">Cancelar</a>
+						</div>						
+					</div>
+				</div>
+			</div>	
+	    </form>
+	    <?php
+	
+	} endif;
+
+	?>
 </body>
-</html><?php
+</html>
+<?php
 
 if (isset($_POST["inserirPenalidade"]) != null) {
-	$cnx= mysqli_query($phpmyadmin, "INSERT INTO PENALIDADE(PENALIDADE_TIPO_ID, USUARIO_ID, OCORRENCIA, PENALIDADE_TOTAL, ANO_MES, OBSERVACAO, REGISTRO) VALUES(" . $_POST['penalidade'] . ", " . $_POST['usuario'] . "," . $_POST['ocorrencia'] . ",(SELECT PENALIDADE*" . $_POST['ocorrencia'] . " FROM PENALIDADE_TIPO WHERE ID=" . $_POST['penalidade'] . " ), '" . $_POST['anoMes'] . "', '" . $_POST['descricao'] . "', '" . date('Y-m-d') . "');");
+	$cnx = mysqli_query($phpmyadmin, "INSERT INTO PENALIDADE(PENALIDADE_TIPO_ID, USUARIO_ID, OCORRENCIA, PENALIDADE_TOTAL, ANO_MES, OBSERVACAO, REGISTRO) VALUES(" . $_POST['penalidade'] . ", " . $_POST['usuario'] . "," . $_POST['ocorrencia'] . ",(SELECT PENALIDADE*" . $_POST['ocorrencia'] . " FROM PENALIDADE_TIPO WHERE ID = " . $_POST['penalidade'] . " ), '" . $_POST['anoMes'] . "', '" . $_POST['descricao'] . "', '" . date('Y-m-d') . "');");
 		
 	$erro = mysqli_error($phpmyadmin);
 	if ($erro == null) {
 		echo "<script>alert('Penalidade inserida com sucesso!')</script>";	
 	} else {
-		?><script>var erro="<?php echo $erro;?>";  alert('Erro ao cadastrar: '+erro)</script><?php
+		?><script>var erro = "<?php echo $erro;?>";  alert('Erro ao cadastrar: '+erro)</script><?php
 	}
 }
 
