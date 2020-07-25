d45f88b400bc8489dbdd3c13cab46566a67e87cf
Code Chip <willvix@outlook.com>
2020-07-25 14:49:52 -0300

    Code optimization

--- user-list.php
+++ user-list.php
@@ -1,46 +1,42 @@
-<style type="text/css"></style>
+<style type="text/css"></style>  
 <?php
-$menuAtivo="configuracoes";
-include('menu.php');
-if($_SESSION["permissao"]==1){
-	echo "<script>alert('Usuário sem permissão')</script>";
-	header("Refresh:1;url=home.php");
+$menuAtivo = 'configuracoes';
+require('menu.php');
+
+if ($_SESSION['permissao'] == 1) {
+	echo "<script>alert('Usuário sem permissão'); window.location.href='home.php';</script>";
 }
-else{
+
 $contador = 0;
-$query="SELECT U.MATRICULA, U.NOME, C.NOME AS CARGO,T.NOME AS TURNO, G.NOME AS GESTOR, U.EFETIVACAO, P.NOME AS PERMISSAO, U.SITUACAO FROM USUARIO U
-INNER JOIN TURNO T ON T.ID=U.TURNO_ID
-INNER JOIN GESTOR G ON G.ID=U.GESTOR_ID
-INNER JOIN CARGO C ON C.ID=U.CARGO_ID
-INNER JOIN PERMISSAO P ON P.ID=U.PERMISSAO_ID WHERE U.SITUACAO<>'Desligado'
-ORDER BY U.SITUACAO, U.TURNO_ID, U.NOME";
-	$x=0;
-	$cnx=mysqli_query($phpmyadmin, $query);
-	while($operadores= $cnx->fetch_array()){
-		$vtNome[$x]=$operadores["NOME"];
-		$vtMatricula[$x]=$operadores["MATRICULA"];
-		$vtCargo[$x]=$operadores["CARGO"];
-		$vtTurno[$x]=$operadores["TURNO"];
-		$vtGestor[$x]=$operadores["GESTOR"];
-		$vtEfetivacao[$x]=$operadores["EFETIVACAO"];
-		$vtPermissao[$x]=$operadores["PERMISSAO"];
-		$vtSituacao[$x]=$operadores["SITUACAO"];					
-		$x++;
-		$contador=$x;
-	}
-	if(mysqli_num_rows($cnx)==0){
-		?><script type="text/javascript">			
-			alert('Nenhum registrado encontrado nesta consulta!');
-			window.location.href=window.location.href;
-		</script> <?php		
-	}
+$x = 0;
+
+$query = "SELECT U.MATRICULA, U.NOME, C.NOME AS CARGO,T.NOME AS TURNO, UG.NOME AS GESTOR, U.EFETIVACAO, P.NOME AS PERMISSAO, U.SITUACAO FROM USUARIO U INNER JOIN TURNO T ON T.ID=U.TURNO_ID INNER JOIN USUARIO UG ON UG.ID=U.GESTOR_ID INNER JOIN CARGO C ON C.ID=U.CARGO_ID INNER JOIN PERMISSAO P ON P.ID=U.PERMISSAO_ID WHERE U.SITUACAO<>'Desligado' ORDER BY U.SITUACAO, U.TURNO_ID, U.NOME";
+
+$cnx = mysqli_query($phpmyadmin, $query);
+while ($user = $cnx->fetch_array()){
+	$name[$x] = $user['NOME'];
+	$registration[$x] = $user['MATRICULA'];
+	$role[$x] = $user['CARGO'];
+	$shift[$x] = $user['TURNO'];
+	$leader[$x] = $user['GESTOR'];
+	$effectuation[$x] = $user['EFETIVACAO'];
+	$permission[$x] = $user['PERMISSAO'];
+	$situation[$x] = $user['SITUACAO'];					
+	$x++;
+	$contador = $x;
+}
+
+if (mysqli_num_rows($cnx) == 0) {
+	echo "<script>alert('Nenhum registrado encontrado nesta consulta!'); window.location.href=window.location.href; </script>";		
+}
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Gestão de Desempenho - Lista de Usuários</title>
 	<script type="text/javascript" src="/js/lib/dummy.js"></script>
-    <link rel="stylesheet" type="text/css" href="/css/result-light.css">   
+    <link rel="stylesheet" type="text/css" href="/css/result-light.css">
 </head>
 <body>	
 <hr/>
@@ -59,26 +55,36 @@ ORDER BY U.SITUACAO, U.TURNO_ID, U.NOME";
 		<th>Situação</th> 			
 	</tr>
 	<?php
- 	for( $i = 0; $i < sizeof($vtNome); $i++ ) : ?>
-	<?php $z=$i; $registro=1; while($vtNome[$z]==$vtNome[$z+1]){
-		$registro++;
-		$repeat=$registro;
-		$z++;
+ 	
+ 	for ( $i = 0; $i < sizeof($name); $i++ ){
+	 	$z = $i; 
+	 	$registro = 1; 
+
+	 	while ($name[$z] == $name[$z + 1]) {
+			$registro++;
+			$repeat = $registro;
+			$z++;
+		}
+		
+		if ($repeat > 0) { 
+			$repeat--;
+		}	
+		
+		echo "
+		<tr>
+			<td>" . $i . "</td>
+			<td>" . $name[$i] . "</td>
+			<td>" . $registration[$i] . "</td>
+			<td>" . $role[$i] . "</td>
+			<td>" . $shift[$i] . "</td>
+			<td>" . $leader[$i] . "</td>
+			<td>" . $effectuation[$i] . "</td>			
+			<td>" . $permission[$i] . "</td>
+			<td>" . $situation[$i] . "</td>
+		</tr>";
 	}
-	if($repeat>0){ $repeat--;}	
-	?>
-	<tr>
-		<td><?php echo $i+1;?></td>
-		<td><?php echo $vtNome[$i]?></td>
-		<td><?php echo $vtMatricula[$i]?></td>
-		<td><?php echo $vtCargo[$i]?></td>
-		<td><?php echo $vtTurno[$i]?></td>
-		<td><?php echo $vtGestor[$i]?></td>
-		<td><?php echo $vtEfetivacao[$i]?></td>			
-		<td><?php echo $vtPermissao[$i]?></td>
-		<td><?php echo $vtSituacao[$i]?></td>
-	</tr>
-<?php endfor;?>
+
+ 	?>
 	</table>	
 	<a href="#topo">		
 		<div class=".scrollWrapper">
@@ -97,4 +103,4 @@ ORDER BY U.SITUACAO, U.TURNO_ID, U.NOME";
 	</div>
 </section>	
 </body>
-</html><?php }//ELSE - caso o usuário tenha permissão.
+</html>
