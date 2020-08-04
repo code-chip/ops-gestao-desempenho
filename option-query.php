c8480b26974049697aca97e076f10956f1bddec7
Code Chip <willvix@outlook.com>
2020-08-04 16:58:49 -0300

    Adjusted layout.

--- option-query.php
+++ option-query.php
@@ -18,6 +18,13 @@ $nome=trim($_POST['nome']);
 	<link rel="stylesheet" href="css/login.css" />
 	<link rel="stylesheet" href="css/bulma.min.css"/>
 	<script defer scr="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+	<script type="text/javascript" src="js/myjs.js"></script>
+	<link rel="stylesheet" type="text/css" href="/css/personal.css">
+    <style type="text/css">
+    	.w24-2{
+    		width:24.2em;
+    	}
+    </style>
 </head>
 <body>
 	<section class="section">
@@ -29,8 +36,8 @@ $nome=trim($_POST['nome']);
 						<label class="label">Nome:</label>
 					</div>
 					<div class="field-body">
-						<div class="field" style="max-width:17em;">							
-							<div class="control">
+						<div class="field">							
+							<div class="control w24-2">
 								<input type="text" class="input" name="nome">
 							</div>
 						</div>
@@ -42,9 +49,9 @@ $nome=trim($_POST['nome']);
 					</div>
 					<div class="field-body">
 						<div class="field" >							
-							<div class="control" style="max-width:17em;">
+							<div class="control">
 								<div class="select">
-									<select name="opcao">
+									<select name="opcao" class="w24-2">
 										<option selected="selected" value="">Selecione</option>
 										<option value="ATIVIDADE">Atividade</option>
 										<option value="CARGO">Cargo</option>
@@ -62,10 +69,15 @@ $nome=trim($_POST['nome']);
 				<div class="field is-horizontal">
 					<div class="field-label"></div>
 						<div class="field-body">
-							<div class="field">
+							<div class="field is-grouped">
 								<div class="control">
-									<button name="voltar" class="button is-primary" value="Voltar"><a href="register.php">Voltar</a></button>
-									<button name="consultarOpcao" type="submit" class="button is-primary" value="Filtrar">Consultar</button>
+									<button name="consultarOpcao" type="submit" class="button is-primary btn128" value="Filtrar">Consultar</button>
+								</div>
+								<div class="control">
+									<button name="consultarOpcao" type="reset" class="button is-primary btn128">Limpar</button>
+								</div>
+								<div class="control">
+									<a href="register.php" class="button is-primary btn128">Cancelar</a>
 								</div>
 							</div>
 						</div>
@@ -87,9 +99,9 @@ $nome=trim($_POST['nome']);
 						<label class="label">Nome:</label>
 					</div>
 					<div class="field-body">
-						<div class="field" style="max-width:17em;">							
-							<div class="control">
-								<input type="text" class="input" name="upNome" value="<?php echo $dado["NOME"]?>">
+						<div class="field">							
+							<div class="control w24-2">
+								<input type="text" class="input" name="upNome" value="<?php echo $dado["NOME"]?>" disabled>
 							</div>
 						</div>
 					</div>
@@ -100,8 +112,8 @@ $nome=trim($_POST['nome']);
 					</div>
 					<div class="field-body">
 						<div class="field" >							
-							<div class="control" style="max-width:17em;">
-								<input type="text" class="input" name="opcao" value="<?php echo mb_convert_case($opcao, MB_CASE_TITLE, 'UTF-8');?>">
+							<div class="control w24-2">
+								<input type="text" class="input" name="opcao" value="<?php echo mb_convert_case($opcao, MB_CASE_TITLE, 'UTF-8');?>" disabled>
 							</div>						
 						</div>
 					</div>
@@ -111,9 +123,9 @@ $nome=trim($_POST['nome']);
 						<label class="label">Status:</label>
 					</div>
 					<div class="field-body">
-						<div class="field" style="max-width:17em;">							
-							<div class="control">
-								<input type="text" class="input" name="situacao" value="<?php echo $dado["SITUACAO"]?>">
+						<div class="field">							
+							<div class="control w24-2">
+								<input type="text" class="input" name="situacao" value="<?php echo $dado["SITUACAO"]?>" disabled>
 							</div>						
 						</div>
 					</div>
@@ -121,9 +133,12 @@ $nome=trim($_POST['nome']);
 				<div class="field is-horizontal">
 					<div class="field-label"></div>
 						<div class="field-body">
-							<div class="field">
+							<div class="field is-grouped">
 								<div class="control">
-									<button name="voltar" class="button is-primary" value="Voltar">Voltar</button>									
+									<button name="voltar" class="button is-primary btn128" value="Voltar">Voltar</button>									
+								</div>
+								<div class="control">
+									<a href="register.php" class="button is-primary btn128">Cancelar</a>									
 								</div>
 							</div>
 						</div>
