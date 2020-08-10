07b99b12718e6af5ee5c89424a84a9c84d61d34d
Code Chip <willvix@outlook.com>
2020-08-10 17:49:01 -0300

    Optimization of code and adjusted layout.

--- report-remove.php
+++ report-remove.php
@@ -1,31 +1,22 @@
-<style type="text/css">
-</style>
 <?php
-$menuAtivo="Desempenho";
-include('menu.php');
-if($_SESSION["permissao"]==1){
-	echo "<script>alert('Usuário sem permissão')</script>";
-	header("Refresh:1;url=home.php");
+
+$menuAtivo = 'desempenho';
+require('menu.php');
+
+if ($_SESSION["permissao"] == 1) {
+	echo "<script>alert('Usuário sem permissão'); window.location.href='home.php'; </script>";
 }
-else{
-$periodo= trim($_REQUEST['periodo']);
-$setor= trim($_REQUEST['setor']);
-$nome= trim($_REQUEST['nome']);
-$contador = 0;
-$totalAlcancado=0;
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-widht, initial-scale=1">
+	<script type="text/javascript" src="js/myjs.js"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+	<style type="text/css" src="css/personal.css"></style>
 	<title>Gestão de Desempenho - Remover Desempenho</title>
-	<script type="text/javascript" src="/js/lib/dummy.js"></script>
-    <link rel="stylesheet" type="text/css" href="/css/result-light.css">
-    <link rel="stylesheet" type="text/css" href="/css/personal.css">
-    <style type="text/css">
-    	.w24-2{
-    		width:24.2em;
-    	}
-    </style>
     <script type="text/javascript">
     	$(document).ready(function(){
 	    	$(window).scroll(function(){
@@ -43,83 +34,71 @@ $totalAlcancado=0;
     </script>   
 </head>
 <body>
-	<?php /*CONSULTAS PARA CARREGAS AS OPÇÕES DE SELEÇÃO DO CADASTRO.*/
-	$gdSetor="SELECT ID, NOME FROM SETOR WHERE SITUACAO='Ativo'";			
-	?>
-	<br/>
 	<span id="topo"></span>
 <div>	
-	<?php if($setor =="" && isset($_POST['consultar'])==null ): ?>
+	<?php if ($setor == "" && isset($_POST['query']) == null ): ?>
 	<section class="section">
 	<div class="container">	
 	<form id="form1" action="report-remove.php" method="POST">
-		<div class="field is-horizontal">
-			<div class="field-label is-normal">
-				<label class="label">Mês:</label>
+		<div class="field">
+			<label class="label is-size-7-touch">Mês*</label>
+			<div class="control has-icons-left">
+				<div class="select is-fullwidth norequired">
+			  		<select name="month" id="month" class="is-fullwidth norequired " autofocus>
+						<option selected="selected" value="<?php echo date('Y-m')?>"><?php echo date('m/Y')?></option>
+						<option value="<?php echo date('Y-m', strtotime("-1 months"))?>"><?php echo date('m/Y', strtotime("-1 months"))?></option>
+						<option value="<?php echo date('Y-m', strtotime("-2 months"))?>"><?php echo date('m/Y', strtotime("-2 months"))?></option>
+						<option value="<?php echo date('Y-m', strtotime("-3 months"))?>"><?php echo date('m/Y', strtotime("-3 months"))?></option>
+						<option value="<?php echo date('Y-m', strtotime("-4 months"))?>"><?php echo date('m/Y', strtotime("-4 months"))?></option>
+						<option value="<?php echo date('Y-m', strtotime("-5 months"))?>"><?php echo date('m/Y', strtotime("-5 months"))?></option>
+						<option value="<?php echo date('Y-m', strtotime("-6 months"))?>"><?php echo date('m/Y', strtotime("-6 months"))?></option>
+					</select>
+				</div>
+				<span class="icon is-small is-left" >
+				  	<i class="fas fa-calendar-alt"></i>
+				</span>
 			</div>
-				<div class="field-body">
-					<div class="field">							
-						<div class="control">
-							<div class="select">
-								<select name="periodo" class="w24-2">
-									<option value="<?php echo date('Y-m', strtotime("+1 months"))?>"><?php echo date('m/Y', strtotime("+1 months"))?></option>
-									<option selected="selected" value="<?php echo date('Y-m')?>"><?php echo date('m/Y')?></option>
-									<option value="<?php echo date('Y-m', strtotime("-1 months"))?>"><?php echo date('m/Y', strtotime("-1 months"))?></option>
-									<option value="<?php echo date('Y-m', strtotime("-2 months"))?>"><?php echo date('m/Y', strtotime("-2 months"))?></option>
-									<option value="<?php echo date('Y-m', strtotime("-3 months"))?>"><?php echo date('m/Y', strtotime("-3 months"))?></option>
-									<option value="<?php echo date('Y-m', strtotime("-4 months"))?>"><?php echo date('m/Y', strtotime("-4 months"))?></option>
-									<option value="<?php echo date('Y-m', strtotime("-5 months"))?>"><?php echo date('m/Y', strtotime("-5 months"))?></option>
-								</select>	
-							</div>
-						</div>
-					</div>
-				</div>
+		</div>
+		<div class="field">
+			<label class="label is-size-7-touch">Setor*</label>
+			<div class="control has-icons-left">
+				<div class="select is-fullwidth" id="sector3">
+					<select name="sector" id="sector" class="norequired is-fullwidth "><?php
+						$con = mysqli_query($phpmyadmin , "SELECT ID, NOME FROM SETOR WHERE SITUACAO='Ativo'");
+						while ($sector = $con->fetch_array()) {
+							echo "<option value=" . $sector["ID"] . ">". $sector["NOME"] . "</option>";
+						}
+						?>
+					</select>
+					<span class="icon is-small is-left">
+						<i class="fas fa-door-open"></i>
+					</span>
+				</div>						
 			</div>
-			<div class="field is-horizontal">
-				<div class="field-label is-normal">
-					<label class="label">Setor:</label>
+		</div>								
+		<div class="field">
+			<label class="label is-size-7-touch">Nome</label>
+			<div class="control has-icons-left">
+				<div class="select is-fullwidth" id="name">
+					<input name="name" type="text" onkeypress="addLoadField('name')" onkeyup="rmvLoadField('name')" class="input norequired" placeholder="Ana Clara ou deixe em branco p/ consultar todos." id="input3">
+					<span class="icon is-left">
+						<i class="fas fa-user-circle"></i>
+					</span>
+					<div id="msgOk3" style="display:none;"></div>	
 				</div>
-				<div class="field-body">
-					<div class="field">							
-						<div class="control">
-							<div class="select">
-								<select name="setor" class="w24-2">								
-								<?php $con = mysqli_query($phpmyadmin , $gdSetor);
-								$x=0; 
-								while($setor = $con->fetch_array()):{?>
-									<option value="<?php echo $vtId[$x]=$setor["ID"]; ?>"><?php echo $vtNome[$x] = $setor["NOME"]; ?></option>
-								<?php $x;} endwhile;?>	
-								</select>
-							</div>
-						</div>
-					</div>
+			</div>					
+		</div>
+		<div class="field-body is-fullwidth">
+			<div class="field is-grouped is-fullwidth">
+				<div class="control is-fullwidth">
+					<button name="query" type="submit" class="button is-primary btn128" value="filter">Consultar</button>
 				</div>
-			</div>
-			<div class="field is-horizontal">
-				<div class="field-label is-normal">
-					<label class="label">Nome:</label>
-				</div>
-				<div class="field-body">
-					<div class="field">							
-						<div class="control">
-							<div class="select w24-2"><!--SELEÇÃO OU PESQUISA DE NOME-->
-							<input name="nome" type="text" class="input" placeholder="Ana Clara">
-						</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="field is-horizontal">
-				<div class="field-label"></div>
-				<div class="field-body">
-					<div class="field">
-						<div class="control">
-							<button name="consultar" type="submit" class="button is-primary btn128">Consultar</button>
-						</div>
-					</div>
+				<div class="control">
+					<button name="clear" type="reset" class="button is-primary btn128" onclick="clearForm();">Limpar</button>
 				</div>
 			</div>
-		</div>						
+		</div>
+	</form>					
 	</form>
 	</div>
 	</section>	
@@ -127,46 +106,36 @@ $totalAlcancado=0;
 </div>
 <?php
 
-if( $nome != ""){	
-	$query="SELECT D.ID AS ID, U.NOME AS USUARIO, D.PRESENCA_ID AS PRESENCA_ID,P.NOME AS PRESENCA, D.ATIVIDADE_ID AS ATIVIDADE_ID, A.NOME AS ATIVIDADE, D.META, D.ALCANCADO AS ALCANCADO, D.DESEMPENHO, D.REGISTRO, D.OBSERVACAO FROM DESEMPENHO D 
-	INNER JOIN USUARIO U ON U.ID=D.USUARIO_ID
-	INNER JOIN PRESENCA P ON P.ID=D.PRESENCA_ID
-	INNER JOIN ATIVIDADE A ON A.ID=D.ATIVIDADE_ID
-	WHERE SETOR_ID=".$setor." AND USUARIO_ID IN(SELECT ID FROM USUARIO WHERE NOME LIKE '%".$nome."%')
-	AND D.REGISTRO>=DATE_SUB(CONCAT('".$periodo."','-21'), interval 1 month) AND D.REGISTRO<= CONCAT('".$periodo."', '-20');";
-	$x=0;
+if( isset($_POST['query'])){
+	$count = 0;
+	$totalAlcancado = 0;
+
+	$query = "SELECT D.ID AS ID, U.NOME AS USUARIO, D.PRESENCA_ID AS PRESENCA_ID,P.NOME AS PRESENCA, D.ATIVIDADE_ID AS ATIVIDADE_ID, A.NOME AS ATIVIDADE, D.META, D.ALCANCADO AS ALCANCADO, D.DESEMPENHO, D.REGISTRO, D.OBSERVACAO FROM DESEMPENHO D INNER JOIN USUARIO U ON U.ID=D.USUARIO_ID INNER JOIN PRESENCA P ON P.ID=D.PRESENCA_ID INNER JOIN ATIVIDADE A ON A.ID=D.ATIVIDADE_ID WHERE SETOR_ID=".$_REQUEST['sector']." AND USUARIO_ID IN(SELECT ID FROM USUARIO WHERE NOME LIKE '%".$_REQUEST['name']."%') AND D.ANO_MES='".$_REQUEST['month']."';";
+	$x=0;	
 	$cnx=mysqli_query($phpmyadmin, $query);
-	while($operadores= $cnx->fetch_array()){
-		$vtId[$x]=$operadores["ID"];
-		$vtNome[$x]=$operadores["USUARIO"];
-		$vtIdPresenca[$x]=$operadores["PRESENCA_ID"];
-		$vtPresenca[$x]=$operadores["PRESENCA"];
-		$vtIdAtividade[$x]=$operadores["ATIVIDADE_ID"];
-		$vtAtividade[$x]=$operadores["ATIVIDADE"];
-		$vtMeta[$x]=$operadores["META"];
-		$vtAlcancado[$x]=$operadores["ALCANCADO"];
-		$vtDesempenho[$x]=$operadores["DESEMPENHO"];
-		$vtRegistro[$x]=$operadores["REGISTRO"];
-		$vtObservacao[$x]=$operadores["OBSERVACAO"];					
+
+	while($user= $cnx->fetch_array()) {
+		$id[$x] = $user["ID"];
+		$name[$x] = $user["USUARIO"];
+		$presenceId[$x] = $user["PRESENCA_ID"];
+		$presence[$x] = $user["PRESENCA"];
+		$activityId[$x] = $user["ATIVIDADE_ID"];
+		$activity[$x] = $user["ATIVIDADE"];
+		$goal[$x] = $user["META"];
+		$reached[$x] = $user["ALCANCADO"];
+		$performance[$x] = $user["DESEMPENHO"];
+		$date[$x] = $user["REGISTRO"];
+		$note[$x] = $user["OBSERVACAO"];					
 		$x++;
-		$contador=$x;
+		$count = $x;
 	}
-	if(mysqli_num_rows($cnx)==0){
-		?><script type="text/javascript">			
-			alert('Nenhum registrado encontrado nesta consulta!');
-			window.location.href=window.location.href;
-		</script> <?php		
+
+	if (mysqli_num_rows($cnx) == 0) {
+		echo "<script>alert('Nenhum registrado encontrado nesta consulta!'); window.location.href=window.location.href; </script>";		
 	}	
 }
-else if(isset($_POST['consultar'])){
-	?><script type="text/javascript">
-		alert('Preencha o campo Nome!');
-		window.location.href=window.location.href;
-	</script><?php
-}	
-?>
-<!--FINAL DO FORMULÁRIO DE FILTRAGEM-->
-<?php if(isset($_POST['consultar']) && $contador!=0) : ?>
+
+if (isset($_POST['query']) && $count != 0) : ?>
 <hr/>
 <section class="section">
 <form id="form2" action="report-remove.php" method="POST">	
@@ -183,29 +152,38 @@ else if(isset($_POST['consultar'])){
 		<th>Data</th>
 		<th>Observação</th> 			
 	</tr>
-<?php for( $i = 0; $i < sizeof($vtNome); $i++ ) : ?>
-	<?php $z=$i; $registro=1; while($vtNome[$z]==$vtNome[$z+1]){
+	<?php 
+
+	for ( $i = 0; $i < sizeof($name); $i++ ) :
+		$z = $i; 
+		$registro = 1; 
+
+		while ($name[$z] == $name[$z+1]) {
 		$registro++;
-		$repeat=$registro;
+		$repeat = $registro;
 		$z++;
 	}
-	if($repeat>0){ $repeat--;}	
+
+	if ($repeat > 0) { 
+		$repeat--;
+	}	
+	
 	?>
 	<tr>
 		<td><?php echo $i+1;?></td>
 		<td>
 			<label class="checkbox">
-  				<input name="id[]" type="checkbox" value="<?php echo $vtId[$i]?>">
+  				<input name="id[]" type="checkbox" value="<?php echo $id[$i]?>">
   				Sim
   			</label>  			
 		</td>	
-		<td><?php echo $vtNome[$i]?></td>		
-		<td><?php echo $vtPresenca[$i];?></td>			
-		<td><?php echo $vtAtividade[$i];?></td>
-		<td><?php echo $vtMeta[$i]?></td>
-		<td><?php echo $vtAlcancado[$i]?></td>
-		<td><?php echo $vtRegistro[$i]?></td>
-		<td><?php echo $vtObservacao[$i]?></td>
+		<td><?php echo $name[$i]?></td>		
+		<td><?php echo $presence[$i];?></td>			
+		<td><?php echo $activity[$i];?></td>
+		<td><?php echo $goal[$i]?></td>
+		<td><?php echo $reached[$i]?></td>
+		<td><?php echo $date[$i]?></td>
+		<td><?php echo $note[$i]?></td>
 	</tr>
 <?php endfor;?>
 	</table>
@@ -238,31 +216,22 @@ else if(isset($_POST['consultar'])){
 </body>
 </html>
 <?php
-if(isset($_POST['removerDados'])){
-	$ids= array_filter($_POST['id']);
-	$upCount=0;
-	for( $i = 0; $i < sizeof($ids); $i++ ){
-		$delete="DELETE FROM DESEMPENHO WHERE ID=".$ids[$i].";";
-		$cnx=mysqli_query($phpmyadmin, $delete);
-		$upCount=$upCount+1;			 
-	}	
-	if(mysqli_error($phpmyadmin)==null && $upCount>0){	
-		?><script type="text/javascript">
-			alert('<?php echo $upCount;?> registro(s) de desempenho deletado(s)');
-			window.location.href=window.location.href;		
-		</script><?php
+
+if (isset($_POST['removerDados'])) {
+	$ids = array_filter($_POST['id']);
+	$upCount = 0;
+
+	for ( $i = 0; $i < sizeof($ids); $i++ ) {
+		$cnx = mysqli_query($phpmyadmin, "DELETE FROM DESEMPENHO WHERE ID=".$ids[$i].";");
+		$upCount = $upCount+1;			 
 	}
-	else if($upCount==0){
-		?><script type="text/javascript">
-			alert('Nenhuma Informação foi marcada p/ ser deletada!!');
-		</script><?php
-	}
-	else{
-		?><script type="text/javascript">
-			alert('Erro ao deletar registro de Desempenho!!');
-			window.location.href=window.location.href;
-		</script><?php
+
+	if (mysqli_error($phpmyadmin) == null && $upCount > 0) {	
+		echo "<script>alert('" . $upCount . " registro(s) de desempenho deletado(s)'); window.location.href=window.location.href; </script>";
+	} else if ($upCount == 0) {
+		echo "<script>alert('Nenhuma Informação foi marcada p/ ser deletada!!'); </script>";
+	} else {
+		echo "<script>alert('Erro ao deletar registro de Desempenho!!'); window.location.href=window.location.href; </script>";
 	}
 }
-}//ELSE - caso o usuário não tenha permissão.
 ?>
