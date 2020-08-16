1883438af4e9a9e3e1b720da85b2527fe614318e
Code Chip <willvix@outlook.com>
2020-08-16 01:39:52 -0300

    Optimization of code and adjusted layout.

--- feedback-query.php
+++ feedback-query.php
@@ -1,159 +1,218 @@
 <?php
-$menuAtivo="Feedback";
-include('menu.php');
-$tipo= trim($_POST["tipo"]);
-$feedback= trim($_POST["feedback"]);
-$contador = 0;
-if(isset($_POST['consultar']) && $tipo=="feedback"){
-	if($feedback=="REMETENTE_ID"){
-		$query="SELECT F.ID, U.NOME AS REMETENTE, U2.NOME AS DESTINATARIO, F.TIPO, F.FEEDBACK, F.EXIBICAO, F.PROFISSIONAL, F.COMPORTAMENTAL, F.DESEMPENHO, F.SITUACAO FROM FEEDBACK F INNER JOIN USUARIO U ON U.ID=F.REMETENTE_ID INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID WHERE ".$feedback."=".$_SESSION["userId"]." ORDER BY F.REGISTRO DESC";
-	}
-	else{
-		$query="SELECT F.ID, U.NOME AS REMETENTE, U2.NOME AS DESTINATARIO, F.TIPO, F.FEEDBACK, F.EXIBICAO, F.PROFISSIONAL, F.COMPORTAMENTAL, F.DESEMPENHO, F.SITUACAO FROM FEEDBACK F INNER JOIN USUARIO U ON U.ID=F.REMETENTE_ID INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID WHERE ".$feedback."=".$_SESSION["userId"]." AND F.SITUACAO IN('Aprovado','Lido') ORDER BY F.REGISTRO DESC";
-	}	
-	$x=0;
-	$cnx=mysqli_query($phpmyadmin, $query);
-	while($feed= $cnx->fetch_array()){
-		$vtId[$x]=$feed["ID"];
-		$vtRemetente[$x]=$feed["REMETENTE"];
-		$vtDestinatario[$x]=$feed["DESTINATARIO"];
-		$vtTipo[$x]=$feed["TIPO"];
-		$vtProfissional[$x]=$feed["PROFISSIONAL"];
-		$vtComportamental[$x]=$feed["COMPORTAMENTAL"];
-		$vtDesempenho[$x]=$feed["DESEMPENHO"];
-		$vtExibicao[$x]=$feed["EXIBICAO"];
-		$vtSituacao[$x]=$feed["SITUACAO"];
-		$vtFeedback[$x]=$feed["FEEDBACK"];
-		$x++;
-		$contador=$x;
-	}
-	if(mysqli_num_rows($cnx)==0){
-		?><script type="text/javascript">			
-			alert('Nenhum feedback encontrado nesta consulta!');
-			window.location.href=window.location.href;
-		</script> <?php		
-	}	
-}
-else if(isset($_POST['consultar']) && $tipo=="solicitacao"){
-	if($feedback=="REMETENTE_ID"){
-		$query="SELECT S.ID, U.NOME, S.MENSAGEM, S.SITUACAO FROM SOLICITACAO S INNER JOIN USUARIO U ON U.ID=S.DESTINATARIO_ID WHERE ".$feedback."=".$_SESSION["userId"]." ORDER BY S.REGISTRO DESC";
-	}
-	else{
-		$query="SELECT S.ID, U.NOME, S.MENSAGEM, S.SITUACAO FROM SOLICITACAO S INNER JOIN USUARIO U ON U.ID=S.REMETENTE_ID WHERE ".$feedback."=".$_SESSION["userId"]." ORDER BY S.REGISTRO DESC";
-	}
-	$x=0;
-	$cnx=mysqli_query($phpmyadmin, $query);
-	while($feed= $cnx->fetch_array()){
-		$vtId[$x]=$feed["ID"];
-		$vtDestinatario[$x]=$feed["NOME"];		
-		$vtFeedback[$x]=$feed["MENSAGEM"];
-		$vtSituacao[$x]=$feed["SITUACAO"];
-		$x++;
-		$contador=$x;
-	}
-	if(mysqli_num_rows($cnx)==0){
-		?><script type="text/javascript">			
-			alert('Nenhuma Solicitação encontrada nesta consulta!');
-			window.location.href=window.location.href;
-		</script> <?php		
-	}
-}	
+$menuAtivo = 'feedback';
+require('menu.php');
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-widht, initial-scale=1">
+	<script type="text/javascript" src="js/myjs.js"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+	<style type="text/css" src="css/personal.css"></style>
 	<title>Gestão de Desempenho - Consultar Feedback</title>
-	<script type="text/javascript" src="/js/lib/dummy.js"></script>
-    <link rel="stylesheet" type="text/css" href="/css/result-light.css">   
 </head>
 <body>	
-<hr/>
 <section class="section" id="topo">
-<?php if(isset($_POST['consultar'])==null ): ?>	
+<?php if (isset($_POST['query']) == null ) { ?>
+	<div class="container">	
 	<form id="form1" action="feedback-query.php" method="POST">
-		<div class="field is-horizontal">
-			<div class="field is-horizontal">
-				<div class="field-label is-normal">
-					<label class="label">Tipo:</label>
-				</div>
-				<div class="field-body">
-					<div class="field" style="max-width:17em;">							
-						<div class="control">
-							<div class="select">
-								<select name="tipo">								
-									<option value="feedback">Feedback</option>
-									<option value="solicitacao">Solicitação</option>
-								</select>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div> &nbsp;&nbsp;&nbsp;
-			<div class="field is-horizontal">
-				<div class="field-label is-normal">
-					<label class="label">Feedback:</label>
-				</div>
-				<div class="field-body">
-					<div class="field" style="max-width:17em;">							
-						<div class="control">
-							<div class="select">
-								<select name="feedback">								
-									<option value="REMETENTE_ID">Enviado(s)</option>
-									<option value="DESTINATARIO_ID">Recebido(s)</option>
-								</select>
-							</div>
-						</div>
-					</div>
+		<div class="field">
+			<label class="label is-size-7-touch">Ano/Mês*</label>
+			<div class="control has-icons-left">
+				<div class="select is-fullwidth">
+					<select name="yearMonth"><?php
+						$cnx = mysqli_query($phpmyadmin, "SELECT ANO_MES FROM FEEDBACK WHERE DESTINATARIO_ID= " . $_SESSION['userId'] . " GROUP BY 1 ORDER BY ANO_MES DESC LIMIT 24");
+						while ( $date = $cnx->fetch_array()) {
+							echo "<option value=" . $date["ANO_MES"] . ">" . $date["ANO_MES"] . "</option>";
+						} ?>
+					</select>
+					<span class="icon is-small is-left">
+						<i class="fas fa-calendar-alt"></i>
+					</span>
 				</div>
 			</div>
-			<div class="field is-horizontal">
-				<div class="field-label"></div>
-				<div class="field-body">
-					<div class="field">
-						<div class="control">
-							<button name="consultar" type="submit" class="button is-primary">Consultar</button>
-						</div>
-					</div>
+		</div>
+		<div class="field">
+			<label class="label is-size-7-touch">Tipo*</label>
+			<div class="control has-icons-left">
+				<div class="select is-fullwidth">
+					<select name="type">								
+						<option value="feedback">Feedback</option>
+						<option value="solicitacao">Solicitação</option>
+					</select>
+					<span class="icon is-small is-left">
+						<i class="fas fa-exchange-alt"></i>
+					</span>
 				</div>
 			</div>
-		</div>						
+		</div>
+		<div class="field">
+			<label class="label is-size-7-touch">Origem*</label>
+			<div class="control has-icons-left">
+				<div class="select is-fullwidth">
+					<select name="origin">								
+						<option value="REMETENTE_ID">Enviado(s)</option>
+						<option value="DESTINATARIO_ID">Recebido(s)</option>
+					</select>
+					<span class="icon is-small is-left">
+						<i class="fas fa-share-square"></i>
+					</span>
+				</div>
+			</div>
+		</div>
+		<div class="field">
+			<label class="label is-size-7-touch">Visualização</label>
+			<div class="control has-icons-left">
+				<div class="select is-fullwidth">
+					<select name="situation">
+						<option value="AND F.SITUACAO IN('Aprovado','Lido')">Ambos</option>								
+						<option value="AND F.SITUACAO = 'Aprovado'">Novo</option>
+						<option value="AND F.SITUACAO = 'Lido'">Lido</option>
+					</select>
+					<span class="icon is-small is-left">
+						<i class="fas fa-book-reader"></i>
+					</span>
+				</div>
+			</div>
+		</div>
+		<div class="field-body">
+			<div class="field is-grouped">
+				<div class="control">
+					<button name="query" type="submit" class="button is-primary btn128">Consultar</button>
+				</div>
+			</div>
+		</div>
 	</form>
-<?php endif; ?>
-<?php if(isset($_POST['consultar']) && $contador!=0) : ?>	
-	<div class="table__wrapper">
-	<table class="table is-bordered pricing__table is-fullwidth is-size-7-touch">	
+	</div>
+<?php }
+	
+if (isset($_POST['query']) && $_POST["type"] == 'feedback') {
+	if ($_POST["origin"]== 'REMETENTE_ID') {
+		$query = "SELECT F.ID, U.NOME AS REMETENTE, U2.NOME AS DESTINATARIO, FRP.RESPOSTA AS PRO, FRC.RESPOSTA AS COMP, FRD.RESPOSTA AS DES, F.TIPO, F.FEEDBACK, F.EXIBICAO, F.SITUACAO FROM FEEDBACK F INNER JOIN FEEDBACK_RESPOSTA FRP ON FRP.ID=F.PROFISSIONAL INNER JOIN FEEDBACK_RESPOSTA FRC ON FRC.ID=F.COMPORTAMENTAL INNER JOIN FEEDBACK_RESPOSTA FRD ON FRD.ID=F.DESEMPENHO INNER JOIN USUARIO U ON U.ID=F.REMETENTE_ID INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID WHERE ".$_POST["origin"]."=".$_SESSION["userId"]." AND ANO_MES = '" . $_POST['yearMonth'] . "' " . $_POST['situation'] . " ORDER BY F.REGISTRO DESC";
+	} else {
+		$query = "SELECT F.ID, U.NOME AS REMETENTE, U2.NOME AS DESTINATARIO, FRP.RESPOSTA AS PRO, FRC.RESPOSTA AS COMP, FRD.RESPOSTA AS DES ,F.TIPO, F.FEEDBACK, F.EXIBICAO, F.SITUACAO FROM FEEDBACK F INNER JOIN FEEDBACK_RESPOSTA FRP ON FRP.ID=F.PROFISSIONAL INNER JOIN FEEDBACK_RESPOSTA FRC ON FRC.ID=F.COMPORTAMENTAL INNER JOIN FEEDBACK_RESPOSTA FRD ON FRD.ID=F.DESEMPENHO INNER JOIN USUARIO U ON U.ID=F.REMETENTE_ID INNER JOIN USUARIO U2 ON U2.ID=F.DESTINATARIO_ID WHERE ".$_POST["origin"]."=".$_SESSION["userId"]." AND ANO_MES = '" . $_POST['yearMonth'] . "' " . $_POST['situation'] . " ORDER BY F.REGISTRO DESC";
+	}
+	
+	$x = 0;
+	$cnx = mysqli_query($phpmyadmin, $query);
+	
+	while ($feed = $cnx->fetch_array()) {
+		$vtId[$x] = $feed["ID"];
+		$vtRemetente[$x] = $feed["REMETENTE"];
+		$vtDestinatario[$x] = $feed["DESTINATARIO"];
+		$vtTipo[$x] = $feed["TIPO"];
+		$vtProfissional[$x] = $feed["PRO"];
+		$vtComportamental[$x] = $feed["COMP"];
+		$vtDesempenho[$x] = $feed["DES"];
+		$vtExibicao[$x] = $feed["EXIBICAO"];
+		$vtSituacao[$x] = $feed["SITUACAO"];
+		$vtFeedback[$x] = $feed["FEEDBACK"];
+		$x++;
+		$count = $x;
+	}
+
+	if ($x == 0) {
+		echo "<script>alert('Nenhum feedback encontrado nesta consulta!'); window.location.href=window.location.href; </script>";		
+	}
+
+} else if (isset($_POST['query']) && $_POST["type"] == "solicitacao") {
+	if ($_POST["origin"]== "REMETENTE_ID") {
+		$query = "SELECT S.ID, U.NOME, S.MENSAGEM, S.SITUACAO FROM SOLICITACAO S INNER JOIN USUARIO U ON U.ID=S.DESTINATARIO_ID WHERE ".$_POST["origin"]."=".$_SESSION["userId"]." ORDER BY S.REGISTRO DESC";
+	} else {
+		$query = "SELECT S.ID, U.NOME, S.MENSAGEM, S.SITUACAO FROM SOLICITACAO S INNER JOIN USUARIO U ON U.ID=S.REMETENTE_ID WHERE ".$_POST["origin"]."=".$_SESSION["userId"]." ORDER BY S.REGISTRO DESC";
+	}
+
+	$x = 0;
+	$cnx = mysqli_query($phpmyadmin, $query);
+	while ($feed = $cnx->fetch_array()) {
+		$vtId[$x] = $feed["ID"];
+		$vtDestinatario[$x] = $feed["NOME"];		
+		$vtFeedback[$x] = $feed["MENSAGEM"];
+		$vtSituacao[$x] = $feed["SITUACAO"];
+		$x++;
+		$count = $x;
+	}
+	
+	if ($x == 0) {
+		echo "<script>alert('Nenhuma Solicitação encontrada nesta consulta!'); window.location.href=window.location.href; </script>";		
+	}
+}
+
+if (isset($_POST['query']) && $x != 0) { 
+	echo '	
+	<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth is-size-7-touch">	
 	<tr>
-		<th>N°</th>
-		<?php if($feedback=="DESTINATARIO_ID"):?><th>Remetente</th><?php endif;?>
-		<?php if($feedback=="REMETENTE_ID"):?><th>Destinatário</th><?php endif;?>
-		<?php if($tipo=="feedback"): ?>
-		<th>Tipo</th>
-		<th>Pro</th>
-		<th>Com</th>
-		<th>Des</th>
-		<th>Feedback</th>
-		<?php endif;?>
-		<?php if($tipo=="solicitacao"): ?>
-		<th>Mensagem</th>		
-		<?php endif;?>
+		<th>N°</th>';
+		if ($_POST["origin"] == "DESTINATARIO_ID" ) { 
+			echo "<th>Remetente</th>";
+		} else {
+			echo "<th>Destinatário</th>";	
+		}
+
+		if ($_POST["type"] == "feedback") {
+			echo "
+			<th>Tipo</th>
+			<th>Profissional</th>
+			<th>Compartamental</th>
+			<th>Desempenho</th>";
+		}
+
+	echo "	
 		<th>Situação</th>
+	</tr>";
+
+ 	for ($i = 0; $i < sizeof($vtId); $i++ ) {
+		echo "
+		<tr>
+		<td rowspan='2'>" . $i . "</td>";		
+
+		if ($_POST["origin"] == "DESTINATARIO_ID") {
+			if ($vtExibicao[$i] == 1) {
+				echo "<td>" . $vtRemetente[$i] . "</td>";
+			} elseif ($_POST["type"] == "solicitacao") {
+				echo "<td>" . $vtDestinatario[$i] . "</td>";
+			} else {
+				echo "<td>Anônimo</td>";
+			} 
+		}
+			
+		if ($_POST["origin"] == "REMETENTE_ID") {
+			echo "<td>" . $vtDestinatario[$i] . "</td>";
+		}
+		
+		if ($_POST["type"] == "feedback") {
+			echo "
+			<td>" . $vtTipo[$i] . "</td>
+			<td>" . $vtProfissional[$i] . "</td> 
+			<td>" . $vtComportamental[$i] . "</td> 
+			<td>" . $vtDesempenho[$i] . "</td>";
+		}
+		
+  		echo "<td>" . $vtSituacao[$i] . "</td>  		
 	</tr>
-	<?php
- 	for( $i = 0; $i < sizeof($vtId); $i++ ) : ?>
 	<tr>
-		<td><?php echo $i+1;?></td>		
-		<?php if($feedback=="DESTINATARIO_ID"):?><td><?php if($vtExibicao[$i]==1){echo $vtRemetente[$i];} else if($tipo=="solicitacao"){echo $vtDestinatario[$i];} else{echo "Anônimo";}?></td><?php endif;?>
-		<?php if($feedback=="REMETENTE_ID"):?><td><?php echo $vtDestinatario[$i]?></td><?php endif;?>
-		<?php if($tipo=="feedback"): ?>
-		<td><?php echo $vtTipo[$i]?></td>
-		<td><?php echo $vtProfissional[$i]?></td> 
-		<td><?php echo $vtComportamental[$i]?></td> 
-		<td><?php echo $vtDesempenho[$i]?></td> 
-		<?php endif;?>
-		<td><?php echo $vtFeedback[$i]?></td>
-  		<td><?php echo $vtSituacao[$i]?></td>  		
-	</tr>
-<?php endfor;?>
+		<td colspan='6'>" . $vtFeedback[$i] . "</td>
+	</tr>";
+	
+	}
+
+	if ($_POST["origin"] == "DESTINATARIO_ID" ) {//Update status to read.
+		$x = 0;
+		$table = "FEEDBACK";
+		
+		if ($_POST["type"] == "solicitacao") {
+			$table = "SOLICITACAO";
+		}
+
+		while ($x < sizeof($vtId)) {
+			$cnx = mysqli_query($phpmyadmin, "UPDATE " . $table . " SET SITUACAO = 'Lido' WHERE ID =" . $vtId[$x]);
+			$x++;
+		}
+	}
+
+	?>
 	</table>	
 	<a href="#topo">		
 		<div class=".scrollWrapper">
@@ -171,22 +230,6 @@ else if(isset($_POST['consultar']) && $tipo=="solicitacao"){
 		</div>
 	</div>
 </section>
-<?php endif;?>	
+<?php } ?>	
 </body>
 </html>
-<?php
-if(isset($_POST['consultar']) && $feedback=="DESTINATARIO_ID" && $contador>0){
-	$x=0;
-	if($tipo=="feedback"){
-		$tabela="FEEDBACK";
-	}
-	else{
-		$tabela="SOLICITACAO";
-	}
-	while( $x< sizeof($vtId)) {
-		$upFeedback="UPDATE ".$tabela." SET SITUACAO='Lido' WHERE ID=".$vtId[$x];	
-		$cnx=mysqli_query($phpmyadmin, $upFeedback);
-		$x++;
-	}
-}
-?>
