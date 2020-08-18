53c85c50c164d2877df2a2f78f6577cfec1de546
Code Chip <willvix@outlook.com>
2020-08-18 17:01:36 -0300

    Add url in function.

--- report/index.php
+++ report/index.php
@@ -5,11 +5,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 $mpdf = new \Mpdf\Mpdf();
  $mpdf->SetDisplayMode('fullpage');
 $stylesheet = file_get_contents('../css/bulma.min.css');
-$stylesheet2 = file_get_contents('style.css');
+$stylesheet2 = file_get_contents('../css/evaluation.css');
 
 //$html = file_get_contents ('http://www.localizecargas.com.br/');
-$html = file_get_contents ('frame.html');
+$html = file_get_contents ('http://localhost/ops-gestao-desempenho/report/layout-evaluation.php');
 $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($stylesheet2,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($html,\Mpdf\HTMLParserMode::HTML_BODY);
+//$mpdf->WritePHP
 $mpdf->Output();
+//$mpdf->Addpage();
+//$mpdf->PDF_resume_page(pdfdoc, optlist);
