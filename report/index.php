0431a7c3aceea831938918798ca2b8691dd30cec
Code Chip <willvix@outlook.com>
2020-08-20 13:25:07 -0300

    Rename variable CSS and add function for numbers of page.

--- report/index.php
+++ report/index.php
@@ -4,13 +4,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $mpdf = new \Mpdf\Mpdf();
  $mpdf->SetDisplayMode('fullpage');
-$stylesheet = file_get_contents('../css/bulma.min.css');
-$stylesheet2 = file_get_contents('../css/evaluation.css');
+$bulma = file_get_contents('../css/bulma.min.css');
+$evaluation = file_get_contents('../css/evaluation.css');
+
+// Set a simple Footer including the page number
+$mpdf->setFooter('{PAGENO}');
 
 //$html = file_get_contents ('http://www.localizecargas.com.br/');
 $html = file_get_contents ('http://localhost/ops-gestao-desempenho/report/layout-evaluation.php');
-$mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
-$mpdf->WriteHTML($stylesheet2,\Mpdf\HTMLParserMode::HEADER_CSS);
+$mpdf->WriteHTML($bulma,\Mpdf\HTMLParserMode::HEADER_CSS);
+$mpdf->WriteHTML($evaluation,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($html,\Mpdf\HTMLParserMode::HTML_BODY);
 //$mpdf->WritePHP
 $mpdf->Output();
