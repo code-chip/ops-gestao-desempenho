475a9d83b9c14032bfed9735ce7f26e54bb302cc
Code Chip <willvix@outlook.com>
2020-08-20 15:52:30 -0300

    Optimization code.

--- goal-insert.php
+++ goal-insert.php
@@ -7,7 +7,6 @@ if ($_SESSION["permissao"] == 1) {
 	echo "<script>alert('Usuário sem permissão'); window.location.href='home.php'; </script>";
 }
 
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,11 +21,11 @@ if ($_SESSION["permissao"] == 1) {
 <body>
 	<section class="section">
 	  	<div class="container">
-	   		<form action="goal-insert.php" method="POST">
+	   		<form action="goal-insert.php" method="POST" onsubmit="return check()">
 	    		<div class="field">
 					<label class="label is-size-7-touch">Meta*</label>
 					<div class="control has-icons-left">
-						<input type="text" class="input desempenho" name="meta">
+						<input type="text" class="input desempenho required" name="goal">
 						<span class="icon is-small is-left">
 							<i class="fas fa-bullseye"></i>
 						</span>
@@ -36,7 +35,7 @@ if ($_SESSION["permissao"] == 1) {
 					<label class="label is-size-7-touch">Atividade*</label>
 					<div class="control has-icons-left">
 						<div class="select is-fullwidth">
-							<select name="atividade">
+							<select name="activity">
 								<?php $gdAtividade="SELECT ID, NOME FROM ATIVIDADE WHERE SITUACAO='Ativo' ORDER BY NOME";
 								$con = mysqli_query($phpmyadmin , $gdAtividade); 
 								while($atividade = $con->fetch_array()):{?>
@@ -49,11 +48,11 @@ if ($_SESSION["permissao"] == 1) {
 						</div>
 					</div>
 				</div>
-				<div class="fieldl">
+				<div class="field">
 					<label class="label is-size-7-touch">Setor*</label>
 					<div class="control has-icons-left">
 						<div class="select is-fullwidth">
-							<select name="setor" id="setor">
+							<select name="sector" id="sector" class="required">
 								<option value="">Selecione</option><?php
 								$cnx = mysqli_query($phpmyadmin, "SELECT ID, NOME FROM SETOR WHERE SITUACAO='Ativo' ORDER BY NOME");
 								while($reSetor = mysqli_fetch_assoc($cnx)) {
@@ -70,7 +69,7 @@ if ($_SESSION["permissao"] == 1) {
 					<label class="label is-size-7-touch">Colaborador*</label>
 					<div class="control has-icons-left">
 						<div class="select is-fullwidth">
-							<select name="usuario" id="usuario">
+							<select name="user" id="user" class="required">
 								<option selected="selected" value="Todos">Todos do Setor</option>
 							</select>
 							<span class="icon is-small is-left">
@@ -82,13 +81,13 @@ if ($_SESSION["permissao"] == 1) {
 				<div class="field">
 					<label class="label is-size-7-touch">Descrição*</label>
 					<div class="control">
-						<textarea name="descricao" class="textarea" maxlength="500"></textarea>
+						<textarea name="description" class="textarea required" maxlength="500"></textarea>
 					</div>
 				</div>
 				<div class="field">
 					<label class="label is-size-7-touch">Execução*</label>
 					<div class="control has-icons-left">
-						<input class="input registro" type="text" name="execucao" value="<?php echo date('Y-m-d',strtotime('+1 day'))?>">
+						<input class="input registro required" type="text" name="execution" value="<?php echo date('Y-m-d',strtotime('+1 day'))?>">
 						<span class="icon is-small is-left">
 							<i class="fas fa-calendar-alt"></i>
 						</span>
@@ -96,10 +95,11 @@ if ($_SESSION["permissao"] == 1) {
 				</div>
 				<div class="field-body">
 					<div class="field is-grouped">
-						<div class="field">
-							<div class="control">
-								<button name="inserirMeta" type="submit" class="button is-primary btn128" value="Filtrar">Inserir</button>
-							</div>
+						<div class="control">
+							<button name="insert" type="submit" class="button is-primary btn128" value="Insert">Inserir</button>
+						</div>
+						<div class="control">
+							<button name="clear" type="reset" class="button is-primary btn128" onclick="clearForm()">Limpar</button>
 						</div>
 					</div>
 				</div>
@@ -110,16 +110,16 @@ if ($_SESSION["permissao"] == 1) {
 	     	</script>
 	     	<script type="text/javascript">
 				$(function(){
-					$('#setor').change(function(){
+					$('#sector').change(function(){
 						if( $(this).val() ) {
-							$('#usuario').hide();
+							$('#user').hide();
 							$('.carregando').show();
 							$.getJSON('loading-users.php?search=',{setor: $(this).val(), ajax: 'true'}, function(j){
 								var options = '<option value="Todos">Todos do Setor</option>';	
 								for (var i = 0; i < j.length; i++) {
 									options += '<option value="' + j[i].id + '">' + j[i].nome_usuario + '</option>';
 								}	
-								$('#usuario').html(options).show();
+								$('#user').html(options).show();
 								$('.carregando').hide();
 							});
 						} else {
@@ -134,45 +134,21 @@ if ($_SESSION["permissao"] == 1) {
 </html>
 <?php
 
-$meta=trim($_POST['meta']);
-$atividade=trim($_POST['atividade']);
-$setor=trim($_POST['setor']);
-$descricao=trim($_POST['descricao']);
-$usuario=trim($_POST['usuario']);
-$execucao=trim($_POST['execucao']);
+if (isset($_POST["insert"]) != null) {
+	if ($_POST['user'] == "Todos") {
+		$cnx = mysqli_query($phpmyadmin, "SELECT ID FROM USUARIO WHERE SETOR_ID = " . $_POST['sector'] . " AND SITUACAO<>'Desligado';");
+		while ($idUsuario = $cnx->fetch_array()) {
+			$cx = mysqli_query($phpmyadmin, "INSERT INTO META(META, ATIVIDADE_ID, SETOR_ID, DESCRICAO, USUARIO_ID, EXECUCAO, CADASTRO_EM, DESEMPENHO) VALUES(".$_POST['goal'].",".$_POST['activity'].",".$_POST['sector'].",'".$_POST['description']."',".$idUsuario["ID"].",'".$_POST['execution']."','".date('Y-m-d')."',0);");
+		}
+	} else {
+		$cnx = mysqli_query($phpmyadmin, "INSERT INTO META(META, ATIVIDADE_ID, SETOR_ID, DESCRICAO, USUARIO_ID, EXECUCAO, CADASTRO_EM, DESEMPENHO) VALUES(".$_POST['goal'].",".$_POST['activity'].",".$_POST['sector'].",'".$_POST['description']."',".$_POST['user'].",'".$_POST['execution']."','".date('Y-m-d')."',0);");
+	}
 
-if(isset($_POST["inserirMeta"])!=null){
-	if($meta!="" && $descricao!="" && $execucao!=""){
-		if($usuario=="Todos"){
-			$listIds="SELECT ID FROM USUARIO WHERE SETOR_ID=".$setor." AND SITUACAO<>'Desligado';";
-			$cnx= mysqli_query($phpmyadmin, $listIds);
-			while ($idUsuario= $cnx->fetch_array()) {
-				$inserirMeta="INSERT INTO META(META, ATIVIDADE_ID, SETOR_ID, DESCRICAO, USUARIO_ID, EXECUCAO, CADASTRO_EM, DESEMPENHO) VALUES(".$meta.",".$atividade.",".$setor.",'".$descricao."',".$idUsuario["ID"].",'".$execucao."','".date('Y-m-d')."',0);";
-				$cx= mysqli_query($phpmyadmin, $inserirMeta);
-			}
-		}
-		else{
-			$inserirMeta="INSERT INTO META(META, ATIVIDADE_ID, SETOR_ID, DESCRICAO, USUARIO_ID, EXECUCAO, CADASTRO_EM, DESEMPENHO) VALUES(".$meta.",".$atividade.",".$setor.",'".$descricao."',".$usuario.",'".$execucao."','".date('Y-m-d')."',0);";			
-			$cnx= mysqli_query($phpmyadmin, $inserirMeta);
-			echo "<script>alert('Meta's cadastrada com sucesso!!')</script>";
-		}
-		$erro=mysqli_error($phpmyadmin);
-		if($erro==null){
-			echo "<script>alert('Meta cadastrada com sucesso!!')</script>";	
-		}
-		else{
-			?><script>var erro="<?php echo $erro;?>";  alert('Erro ao cadastrar: '+erro)</script><?php
-		}
+	if ($erro == null) {
+		echo "<script>alert('Meta cadastrada com sucesso!!'); </script>";	
+	} else {
+		echo "<script>alert('Erro ao cadastrar meta.'); </script>";
 	}
-	else if($meta==""){
-		echo "<script>alert('Preencher o campo Meta é obrigatório!!')</script>";
-	}
-	else if($descricao==""){
-		echo "<script>alert('Preencher o campo Descricao é obrigatório!!')</script>";
-	}
-	else{
-		echo "<script>alert('Preencher o campo Expiração é obrigatório!!')</script>";
-	}	
 }
 
 ?>
