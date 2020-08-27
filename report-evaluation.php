78c8a4296d6cec76f85b8ec5982f4ed86f6accd3
Code Chip <willvix@outlook.com>
2020-08-27 10:19:13 -0300

    Add functions javascript.

--- report-evaluation.php
+++ report-evaluation.php
@@ -7,8 +7,12 @@ require('menu.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Gestão de Desempenho - Relatório Individual</title>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-widht, initial-scale=1">
+	<script type="text/javascript" src="js/myjs.js"></script>
 	<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+	<style type="text/css" src="css/personal.css"></style>
+	<title>Gestão de Desempenho - Relatório Individual</title>
 </head>
 <body>
 <span id="topo"></span>
@@ -36,10 +40,10 @@ require('menu.php');
 			<label class="label is-size-7-touch">Avaliação*</label>
 			<div class="control has-icons-left">
 				<div class="select is-fullwidth">
-					<select name="atividade">
+					<select name="atividade" onchange="change(this.value)">
 						<option selected="selected" value="">Colaboradores</option>
-						<option value="">Líderes</option>
-						<option value="">Individual</option>								
+						<option value="lider">Líderes</option>
+						<option value="individual">Individual</option>								
 					</select>
 					<span class="icon is-small is-left">
 						<i class="fas fa-diagnoses"></i>
@@ -47,34 +51,36 @@ require('menu.php');
 				</div>						
 			</div>
 		</div>
-		<div class="field">
-			<label class="label is-size-7-touch">Setor*</label>
-			<div class="control has-icons-left">
-				<div class="select is-fullwidth">
-					<select name="sector" class="required" id="sector" autofocus>
-						<option value="">Selecione</option><?php
-						$cnx = mysqli_query($phpmyadmin, "SELECT ID, NOME FROM SETOR WHERE SITUACAO='Ativo' ORDER BY NOME");
-						while($reSetor = mysqli_fetch_assoc($cnx)) {
-							echo '<option value="'.$reSetor['ID'].'">'.$reSetor['NOME'].'</option>';
-						}
-						?>
-					</select>	
-				</div>
-				<span class="icon is-small is-left">
-					<i class="fas fa-door-open"></i>
-				</span>
-			</div>						
-		</div>
-		<div class="field">
-			<label class="label is-size-7-touch">Colaborador*</label>
-			<div class="control has-icons-left">
-				<div class="select is-fullwidth">
-					<select name="recipient" id="recipient" class="required">
-						<option selected="selected" value="Todos">Selecione</option>
-					</select>
+		<div class="field loadId" id="individual" style="display: none;">
+			<div class="field">
+				<label class="label is-size-7-touch">Setor*</label>
+				<div class="control has-icons-left">
+					<div class="select is-fullwidth">
+						<select name="sector" class="required" id="sector" autofocus>
+							<option value="">Selecione</option><?php
+							$cnx = mysqli_query($phpmyadmin, "SELECT ID, NOME FROM SETOR WHERE SITUACAO='Ativo' ORDER BY NOME");
+							while($reSetor = mysqli_fetch_assoc($cnx)) {
+								echo '<option value="'.$reSetor['ID'].'">'.$reSetor['NOME'].'</option>';
+							}
+							?>
+						</select>	
+					</div>
 					<span class="icon is-small is-left">
-						<i class="fas fa-user-circle"></i>
-					</span>	
+						<i class="fas fa-door-open"></i>
+					</span>
+				</div>						
+			</div>
+			<div class="field">
+				<label class="label is-size-7-touch">Colaborador*</label>
+				<div class="control has-icons-left">
+					<div class="select is-fullwidth">
+						<select name="user" id="user" class="required">
+							<option selected="selected" value="Todos">Selecione</option>
+						</select>
+						<span class="icon is-small is-left">
+							<i class="fas fa-user-circle"></i>
+						</span>	
+					</div>
 				</div>
 			</div>
 		</div>
@@ -84,11 +90,35 @@ require('menu.php');
 					<button name="query" type="submit" class="button is-primary btn128" value="query">Gerar</button>
 				</div>
 				<div class="control">
-					<button name="clear" type="submit" class="button is-primary btn128">Limpar</button>
+					<button name="reset" type="reset" class="button is-primary btn128" onclick="change()">Limpar</button>
 				</div>
 			</div>
 		</div>		
-	</form><!--FINAL DO FORMULÁRIO-->		
+	</form><!--FINAL DO FORMULÁRIO-->
+	<script type="text/javascript" scr="https://www.google.com/jsapi"></script>
+	<script type="text/javascript">
+		google.loader("jquery", "1.4.2");
+	</script>
+	<script type="text/javascript">
+		$(function(){
+			$('#sector').change(function(){
+				if( $(this).val() ) {
+					$('#user').hide();
+					$('.carregando').show();
+					$.getJSON('loading-users.php?search=',{setor: $(this).val(), ajax: 'true'}, function(j){
+						var options = '<option value="Todos">Todos do Setor</option>';	
+						for (var i = 0; i < j.length; i++) {
+							options += '<option value="' + j[i].id + '">' + j[i].nome_usuario + '</option>';
+						}	
+						$('#user').html(options).show();
+						$('.carregando').hide();
+					});
+				} else {
+					$('#usuario').html('<option value="">Todos do Setor</option>');
+				}
+			});
+		});
+	</script>		
 </div>
 </section>
 </body>
@@ -96,7 +126,6 @@ require('menu.php');
 <?php
 
 if (isset($_POST['query'])) {
-	$_SESSION['re-yearMonth'] = $_POST['yearMonth'];
 	echo "<script>window.open('report/index3.php?yearMonth=".$_POST['yearMonth']."', '_blank');</script>";
 	die();
 }
