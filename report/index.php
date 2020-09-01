d7d7fd5ddca412f2804b45998dc0cb8d54213f58
Code Chip <willvix@outlook.com>
2020-09-01 08:54:42 -0300

    Add method GET in variable yearMonth.

--- report/index.php
+++ report/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $mpdf = new \Mpdf\Mpdf();
+$mpdf->SetHeader($header);
  $mpdf->SetDisplayMode('fullpage');
 $bulma = file_get_contents('../css/bulma.min.css');
 $evaluation = file_get_contents('../css/evaluation.css');
@@ -11,10 +12,11 @@ $yearMonth = $_GET['yearMonth'];
 $mpdf->setFooter('{PAGENO}');
 
 //$html = file_get_contents ('http://www.localizecargas.com.br/');
-$html = file_get_contents ('http://localhost/ops-gestao-desempenho/report/layout-evaluation.php?yearMonth='.$yearMonth);
+$html = file_get_contents ('http://evino-gd.tk/report/layout-evaluation.php?yearMonth='.$yearMonth);
 $mpdf->WriteHTML($bulma,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($evaluation,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($html,\Mpdf\HTMLParserMode::HTML_BODY);
+$setAutoTopMargin = 100;
 //$mpdf->WritePHP
 // JAVASCRIPT FOR WHOLE DOCUMENT
 //$mpdf->SetJS('window.print()');
