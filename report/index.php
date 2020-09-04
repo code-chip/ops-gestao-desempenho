a0f2a04cd484a2388703f3171f4e15d68d6f9065
Code Chip <willvix@outlook.com>
2020-09-04 18:19:51 -0300

    Add variable of session.

--- report/index.php
+++ report/index.php
@@ -1,25 +1,16 @@
 <?php
-
+require('../login-check.php');
 require_once __DIR__ . '/vendor/autoload.php';
 
 $mpdf = new \Mpdf\Mpdf();
 $mpdf->SetHeader($header);
- $mpdf->SetDisplayMode('fullpage');
+$mpdf->SetDisplayMode('fullpage');
 $bulma = file_get_contents('../css/bulma.min.css');
 $evaluation = file_get_contents('../css/evaluation.css');
-$yearMonth = $_GET['yearMonth'];
 // Set a simple Footer including the page number
 $mpdf->setFooter('{PAGENO}');
-
-//$html = file_get_contents ('http://www.localizecargas.com.br/');
-$html = file_get_contents ('http://evino-gd.tk/report/evaluation-subordinate.php?yearMonth='.$yearMonth);
+$html = file_get_contents("http://localhost/ops-gestao-desempenho/report/evaluation-".$_SESSION['file'].".php?filter=".$_SESSION['filter']);
 $mpdf->WriteHTML($bulma,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($evaluation,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($html,\Mpdf\HTMLParserMode::HTML_BODY);
-$setAutoTopMargin = 100;
-//$mpdf->WritePHP
-// JAVASCRIPT FOR WHOLE DOCUMENT
-//$mpdf->SetJS('window.print()');
 $mpdf->Output();
-//$mpdf->Addpage();
-//$mpdf->PDF_resume_page(pdfdoc, optlist);
