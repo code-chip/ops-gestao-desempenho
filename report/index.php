f7ad3d440527fe0286fe269b67ef5ae379f8ace6
Code Chip <willvix@outlook.com>
2020-09-08 17:39:52 -0300

    Add title and author.

--- report/index.php
+++ report/index.php
@@ -9,6 +9,8 @@ $bulma = file_get_contents('../css/bulma.min.css');
 $evaluation = file_get_contents('../css/evaluation.css');
 // Set a simple Footer including the page number
 $mpdf->setFooter('{PAGENO}');
+$mpdf->setTitle('Relatório Avaliação de Desempenho');
+$mpdf->SetAuthor($_SESSION['nameUser']);
 $html = file_get_contents("http://localhost/ops-gestao-desempenho/report/evaluation-".$_SESSION['file'].".php?filter=".$_SESSION['filter']);
 $mpdf->WriteHTML($bulma,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($evaluation,\Mpdf\HTMLParserMode::HEADER_CSS);
