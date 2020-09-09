9b716e5eb45ff2aba0f8119e9885a62ebe8e8c57
Code Chip <willvix@outlook.com>
2020-09-09 14:51:33 -0300

    Code optimization.

--- register.php
+++ register.php
@@ -25,7 +25,7 @@ array_push($_SESSION['filter'], trim($_GET['descricao']), trim($_GET['link']));
 		}
 		.hero.has-background {
 			position: relative;
-			/*overflow: hidden;*/
+			overflow: hidden;
 		}
 		.hero-background {
 			position: absolute;
@@ -41,94 +41,118 @@ array_push($_SESSION['filter'], trim($_GET['descricao']), trim($_GET['link']));
 		.spc{
 			margin-right: 10px;
 		}
+		@media (max-width: 660px){
+			.mw12{
+				min-width:18.1em;
+			}
+		}
+		@media (max-width: 650px){
+			.mw12{
+				min-width:17em;
+			}
+		}
+		@media (max-width: 640px){
+			.mw12{
+				min-width:14em;
+			}
+		}
+		@media (max-width: 550px){
+			.mw12{
+				min-width: 100%;
+			}
+		}
+		
 	</style>
 </head>
 <body>
 <div class="hero is-fullheight is-primary has-background field has-addons">
 <img alt="Fill Murray" class="hero-background is-transparent" src="img/wallpaper/data-science8-min-black.jpg" />
-	<div class="hero-background capas capas2">
-		<div class="field has-addons has-addons-centered">
+<div class="hero-background">
 	<section class="section">
 	<div class="field has-addons has-addons-centered">
-	<div class="buttons">
-		<?php if($_SESSION["permissao"]!=1):?><a href="user-insert.php"><span class="button is-media is-primary is-outlined mw12">Inserir Usuário</span></a>&nbsp&nbsp&nbsp<?php endif;?>		
-		<a href="user-query.php"><span class="button is-media is-primary is-outlined mw12">Consultar Usuário</span></a>&nbsp&nbsp&nbsp
-		<a href="user-update.php"><span class="button is-media is-primary is-outlined mw12">Atualizar Usuário</span></a>&nbsp&nbsp&nbsp			
-		<?php if($_SESSION["permissao"]==4):?><a href="user-remove.php"><span class="button is-media is-primary is-outlined mw12">Remover Usuário</span></a>&nbsp<?php endif;?>		
-	</div>	
-</div>
-	<br/>
-	<div class="field has-addons has-addons-centered">
-		<div class="buttons">								
-			<button class="button is-white is-primary is-outlined mw12" value="adress-insert.php, Inserir Endereço" onclick="actionAdress(this.value)">Inserir Endereço</button>&nbsp
-			<button class="button is-white is-primary is-outlined mw12" value="adress-query.php, Consultar Endereço" onclick="actionAdress(this.value)">Consultar Endereço</button>&nbsp
-			<button class="button is-white is-primary is-outlined mw12" value="adress-update.php, Atualizar Endereço" onclick="actionAdress(this.value)">Atualizar Endereço</button>&nbsp
-			<?php if($_SESSION["permissao"]!=1):?>
-			<button class="button is-white is-primary is-outlined mw12" value="adress-remove.php, Remover Endereço" onclick="actionAdress(this.value)">Remover Endereço</button>&nbsp&nbsp<?php endif;?>			
-		</div>	
-	</div>
-	<br/>
-	<?php if ($_SESSION["permissao"] != 1):?>	
-	<div class="field has-addons has-addons-centered">
-		<div class="buttons">								
-			<a href="option-insert.php"><span class="button is-white is-primary is-outlined mw12">Inserir Opção</span></a>&nbsp&nbsp&nbsp
-			<a href="option-query.php"><span class="button is-media is-primary is-outlined mw12">Consultar Opção</span></a>&nbsp&nbsp&nbsp
-			<a href="option-update.php"><span class="button is-media is-primary is-outlined mw12">Atualizar Opção</span></a>&nbsp&nbsp&nbsp
-			<a href="option-remove.php"><span class="button is-media is-primary is-outlined mw12">Remover Opção</span></a>&nbsp&nbsp&nbsp		
+		<div class="buttons">
+			<?php if ($_SESSION["permissao"] != 1) { ?>
+			<a href="user-insert.php" class="button is-media is-primary is-outlined mw12">Inserir Usuário</a>
+			<?php } ?>		
+			<a href="user-query.php" class="button is-media is-primary is-outlined mw12">Consultar Usuário</a>
+			<a href="user-update.php" class="button is-media is-primary is-outlined mw12">Atualizar Usuário</a>			
+			<?php if ($_SESSION["permissao"] == 4) { ?>
+			<a href="user-remove.php" class="button is-media is-primary is-outlined mw12">Remover Usuário</a>
+			<?php } ?>		
 		</div>	
 	</div>
 	<br/>
 	<div class="field has-addons has-addons-centered">
 		<div class="buttons">								
-			<a href="user-list.php"><span class="button is-media is-primary is-outlined mw12">Listar Usuários</span></a>&nbsp&nbsp&nbsp
-			<a href="option-list.php"><span class="button is-media is-primary is-outlined mw12">Listar Opções</span></a>&nbsp&nbsp&nbsp
-			<a href="goal-list.php"><span class="button is-media is-primary is-outlined mw12">Listar Metas</span></a>&nbsp&nbsp&nbsp
-			<a href="user-list-off.php"><span class="button is-media is-primary is-outlined mw12">Listar Desligados</span></a>&nbsp&nbsp&nbsp		
+			<button class="button is-white is-primary is-outlined mw12" value="adress-insert.php, Inserir Endereço" onclick="actionAdress(this.value)">Inserir Endereço</button>
+			<button class="button is-white is-primary is-outlined mw12" value="adress-query.php, Consultar Endereço" onclick="actionAdress(this.value)">Consultar Endereço</button>
+			<button class="button is-white is-primary is-outlined mw12" value="adress-update.php, Atualizar Endereço" onclick="actionAdress(this.value)">Atualizar Endereço</button>
+			<?php if ($_SESSION["permissao"] != 1) { ?>
+			<button class="button is-white is-primary is-outlined mw12" value="adress-remove.php, Remover Endereço" onclick="actionAdress(this.value)">Remover Endereço</button>
+			<?php } ?>			
+		</div>	
+	</div>
+	<br/>
+	<?php if ($_SESSION["permissao"] != 1) { ?>	
+	<div class="field has-addons has-addons-centered">
+		<div class="buttons">								
+			<a href="option-insert.php" class="button is-white is-primary is-outlined mw12">Inserir Opção</a>
+			<a href="option-query.php" class="button is-media is-primary is-outlined mw12">Consultar Opção</a>
+			<a href="option-update.php" class="button is-media is-primary is-outlined mw12">Atualizar Opção</a>
+			<a href="option-remove.php" class="button is-media is-primary is-outlined mw12">Remover Opção</a>		
 		</div>	
 	</div>
 	<br/>
 	<div class="field has-addons has-addons-centered">
 		<div class="buttons">								
-			<a href="question-insert.php"><span class="button is-media is-primary is-outlined mw12">Inserir Pergunta</a>&nbsp&nbsp&nbsp
-			<a href="question-query.php"><span class="button is-media is-primary is-outlined mw12">Consultar Pergunta</span></a>&nbsp&nbsp&nbsp
-			<a href="question-update.php"><span class="button is-media is-primary is-outlined mw12">Atualizar Pergunta</span></a>&nbsp&nbsp&nbsp
-			<a href="question-remove.php"><span class="button is-media is-primary is-outlined mw12">Remover Pergunta</span></a>&nbsp&nbsp&nbsp		
+			<a href="user-list.php" class="button is-media is-primary is-outlined mw12">Listar Usuários</a>
+			<a href="option-list.php" class="button is-media is-primary is-outlined mw12">Listar Opções</a>
+			<a href="goal-list.php" class="button is-media is-primary is-outlined mw12">Listar Metas</a>
+			<a href="user-list-off.php" class="button is-media is-primary is-outlined mw12">Listar Desligados</a>		
 		</div>	
 	</div>
 	<br/>
 	<div class="field has-addons has-addons-centered">
 		<div class="buttons">								
-			<a href="menu-insert.php"><span class="button is-media is-primary is-outlined mw12">Inserir Menu</span></a>&nbsp&nbsp&nbsp
-			<a href="menu-query.php"><span class="button is-media is-primary is-outlined mw12">Consultar Menu</span></a>&nbsp&nbsp&nbsp
-			<a href="menu-update.php"><span class="button is-media is-primary is-outlined mw12">Atualizar Menu</span></a>&nbsp&nbsp&nbsp
-			<a href="menu-remove.php"><span class="button is-media is-primary is-outlined mw12">Remover Menu</span></a>&nbsp&nbsp&nbsp		
+			<a href="question-insert.php" class="button is-media is-primary is-outlined mw12">Inserir Pergunta</a>
+			<a href="question-query.php" class="button is-media is-primary is-outlined mw12">Consultar Pergunta</a>
+			<a href="question-update.php" class="button is-media is-primary is-outlined mw12">Atualizar Pergunta</a>
+			<a href="question-remove.php" class="button is-media is-primary is-outlined mw12">Remover Pergunta</a>		
 		</div>	
 	</div>
 	<br/>
 	<div class="field has-addons has-addons-centered">
 		<div class="buttons">								
-			<a href="wait.php"><span class="button is-media is-primary is-outlined mw12">Inserir Mensagem</span></a>&nbsp&nbsp&nbsp
-			<a href="wait.php"><span class="button is-media is-primary is-outlined mw12">Consultar Mensagem</span></a>&nbsp&nbsp&nbsp
-			<a href="wait.php"><span class="button is-media is-primary is-outlined mw12">Atualizar Mensagem</span></a>&nbsp&nbsp&nbsp
-			<a href="wait.php"><span class="button is-media is-primary is-outlined mw12">Remover Mensagem</span></a>&nbsp&nbsp&nbsp		
+			<a href="menu-insert.php" class="button is-media is-primary is-outlined mw12">Inserir Menu</a>
+			<a href="menu-query.php" class="button is-media is-primary is-outlined mw12">Consultar Menu</a>
+			<a href="menu-update.php" class="button is-media is-primary is-outlined mw12">Atualizar Menu</a>
+			<a href="menu-remove.php" class="button is-media is-primary is-outlined mw12">Remover Menu</a>		
+		</div>	
+	</div>
+	<br/>
+	<div class="field has-addons has-addons-centered">
+		<div class="buttons">								
+			<a href="wait.php" class="button is-media is-primary is-outlined mw12">Inserir Mensagem</a>
+			<a href="wait.php" class="button is-media is-primary is-outlined mw12">Consultar Mensagem</a>
+			<a href="wait.php" class="button is-media is-primary is-outlined mw12">Atualizar Mensagem</a>
+			<a href="wait.php" class="button is-media is-primary is-outlined mw12">Remover Mensagem</a>		
 		</div>	
 	</div>
 	<br/>
 	<?php if ($_SESSION["permissao"] == 4 || $_SESSION["cargo"] == 12) { ?>	
 	<div class="field has-addons has-addons-centered">
 		<div class="buttons">								
-			<a href="coupon-insert.php"><span class="button is-media is-primary is-outlined mw12">Inserir Cupom</span></a>&nbsp&nbsp&nbsp
-			<a href="coupon-list.php"><span class="button is-media is-primary is-outlined mw12">Listar Cupom</span></a>&nbsp&nbsp&nbsp			
+			<a href="coupon-insert.php" class="button is-media is-primary is-outlined mw12">Inserir Cupom</a>
+			<a href="coupon-list.php" class="button is-media is-primary is-outlined mw12">Listar Cupom</a>			
 		</div>	
 	</div>
 	<?php 
 
 	} 
 
-	?>
-	<?php endif;?>
+	} ?>
 	</section>
-	</div>
+</div>
 </div>	
 <script type="text/javascript">
 	function actionAdress(valor) {
