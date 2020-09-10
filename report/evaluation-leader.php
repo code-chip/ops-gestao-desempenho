c5d33447db5209e3dc4119f956496f1f64d68823
Code Chip <willvix@outlook.com>
2020-09-10 19:45:46 -0300

    Update query and tables.

--- report/evaluation-leader.php
+++ report/evaluation-leader.php
@@ -3,16 +3,6 @@
 require('../connection.php');
 $yearMonth = $_GET['filter'];
 
-$r = mysqli_query($phpmyadmin, "SELECT ROUND(AVG(D.DESEMPENHO),2) AS MEDIA, D.USUARIO_ID FROM DESEMPENHO D INNER JOIN USUARIO U ON U.ID=D.USUARIO_ID WHERE U.SITUACAO<>'Desligado' AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth' GROUP BY 2 ORDER BY 1 DESC;");
-
-$positions = mysqli_num_rows($r);
-
-$i = 0;
-while ($list = $r->fetch_array()) {
-	$ranking[$i] = $list["USUARIO_ID"];
-	$i++;
-}
-
 $query = "SELECT AI.USUARIO_ID, U.NOME, UG.NOME AS LIDER, C.NOME AS CARGO,
 TIMESTAMPDIFF(MONTH,U.EFETIVACAO,AI.REGISTRO ) AS MESES, TIMESTAMPDIFF(YEAR,U.NASCIMENTO,AI.REGISTRO ) 
 AS IDADE, AI.REGISTRO, 
@@ -20,16 +10,22 @@ AS IDADE, AI.REGISTRO,
 (SELECT COUNT(1) FROM DESEMPENHO INNER JOIN USUARIO UU ON UU.ID= USUARIO_ID WHERE PRESENCA_ID = 4 AND GESTOR_ID =U.ID AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth') AS ATESTADO,
 (SELECT COUNT(1) FROM DESEMPENHO INNER JOIN USUARIO UU ON UU.ID= USUARIO_ID WHERE PRESENCA_ID = 2 AND GESTOR_ID =U.ID AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth') AS FALTA,
 (SELECT COUNT(1) FROM PENALIDADE INNER JOIN USUARIO UU ON UU.ID= USUARIO_ID WHERE GESTOR_ID =U.ID AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth') AS PENALIDADE,
-(SELECT ROUND(MIN(DESEMPENHO),2) FROM DESEMPENHO WHERE USUARIO_ID=AI.USUARIO_ID AND DESEMPENHO>0 AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth') AS MINIMO,
-(SELECT ROUND(AVG(DESEMPENHO),2) FROM DESEMPENHO WHERE USUARIO_ID=AI.USUARIO_ID AND PRESENCA_ID NOT IN(3,5) AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth') AS MEDIA,
-(SELECT ROUND(MAX(DESEMPENHO),2) FROM DESEMPENHO WHERE USUARIO_ID=AI.USUARIO_ID AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth') AS MAXIMO,
+(SELECT ROUND(MIN(DESEMPENHO),2) FROM DESEMPENHO INNER JOIN USUARIO UU ON UU.ID= USUARIO_ID WHERE GESTOR_ID=AI.USUARIO_ID AND DESEMPENHO>0 AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth') AS MINIMO,
+(SELECT ROUND(AVG(DESEMPENHO),2) FROM DESEMPENHO INNER JOIN USUARIO UU ON UU.ID= USUARIO_ID WHERE GESTOR_ID=AI.USUARIO_ID AND PRESENCA_ID NOT IN(3,5) AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth') AS MEDIA,
+(SELECT ROUND(MAX(DESEMPENHO),2) FROM DESEMPENHO INNER JOIN USUARIO UU ON UU.ID= USUARIO_ID WHERE GESTOR_ID=AI.USUARIO_ID AND ANO_MES >=DATE_SUB('$yearMonth-01', INTERVAL 6 MONTH) AND ANO_MES <='$yearMonth') AS MAXIMO,
+(SELECT COUNT(1) FROM USUARIO WHERE GESTOR_ID=AI.USUARIO_ID) AS EQUIPE,
+(SELECT COUNT(1) FROM USUARIO WHERE GESTOR_ID=AI.USUARIO_ID AND SEXO='m') AS M,
+(SELECT COUNT(1) FROM USUARIO WHERE GESTOR_ID=AI.USUARIO_ID AND SEXO='f') AS F,
 (SELECT COUNT(1) FROM FEEDBACK WHERE REMETENTE_ID=AI.USUARIO_ID ) AS F_ENV,
 (SELECT COUNT(1) FROM FEEDBACK WHERE DESTINATARIO_ID =AI.USUARIO_ID ) AS F_REC,
 (SELECT COUNT(1) FROM SOLICITACAO WHERE DESTINATARIO_ID =AI.USUARIO_ID ) AS S_ENV,
 (SELECT COUNT(1) FROM SOLICITACAO WHERE REMETENTE_ID =AI.USUARIO_ID ) AS S_REC,
-(SELECT IFNULL(ROUND(AVG(FR.NOTA),2),0) FROM FEEDBACK INNER JOIN FEEDBACK_RESPOSTA FR ON FR.ID=COMPORTAMENTAL WHERE DESTINATARIO_ID =AI.USUARIO_ID ) AS F_COM,
-(SELECT IFNULL(ROUND(AVG(FR.NOTA),2),0) FROM FEEDBACK INNER JOIN FEEDBACK_RESPOSTA FR ON FR.ID=PROFISSIONAL WHERE DESTINATARIO_ID =AI.USUARIO_ID ) AS F_PRO,
-(SELECT IFNULL(ROUND(AVG(FR.NOTA),2),0) FROM FEEDBACK INNER JOIN FEEDBACK_RESPOSTA FR ON FR.ID=DESEMPENHO WHERE DESTINATARIO_ID =AI.USUARIO_ID ) AS F_DES,
+(SELECT IFNULL(ROUND(AVG(FR.NOTA),2),0) FROM FEEDBACK INNER JOIN FEEDBACK_RESPOSTA FR ON FR.ID=COMPORTAMENTAL WHERE DESTINATARIO_ID =AI.USUARIO_ID ) AS FL_COM,
+(SELECT IFNULL(ROUND(AVG(FR.NOTA),2),0) FROM FEEDBACK INNER JOIN FEEDBACK_RESPOSTA FR ON FR.ID=PROFISSIONAL WHERE DESTINATARIO_ID =AI.USUARIO_ID ) AS FL_PRO,
+(SELECT IFNULL(ROUND(AVG(FR.NOTA),2),0) FROM FEEDBACK INNER JOIN FEEDBACK_RESPOSTA FR ON FR.ID=DESEMPENHO WHERE DESTINATARIO_ID =AI.USUARIO_ID ) AS FL_DES,
+(SELECT IFNULL(ROUND(AVG(FR.NOTA),2),0) FROM FEEDBACK INNER JOIN FEEDBACK_RESPOSTA FR ON FR.ID=COMPORTAMENTAL WHERE DESTINATARIO_ID IN(SELECT ID FROM USUARIO WHERE GESTOR_ID=AI.USUARIO_ID) ) AS FT_COM,
+(SELECT IFNULL(ROUND(AVG(FR.NOTA),2),0) FROM FEEDBACK INNER JOIN FEEDBACK_RESPOSTA FR ON FR.ID=PROFISSIONAL WHERE DESTINATARIO_ID IN(SELECT ID FROM USUARIO WHERE GESTOR_ID=AI.USUARIO_ID) ) AS FT_PRO,
+(SELECT IFNULL(ROUND(AVG(FR.NOTA),2),0) FROM FEEDBACK INNER JOIN FEEDBACK_RESPOSTA FR ON FR.ID=DESEMPENHO WHERE DESTINATARIO_ID IN(SELECT ID FROM USUARIO WHERE GESTOR_ID=AI.USUARIO_ID) ) AS FT_DES,
 (SELECT IFNULL(ROUND(AVG(AR2.NOTA),2),0) FROM AVAL_REALIZADA AR
 INNER JOIN AVAL_INDICE AIN ON AIN.ID=AR.AVAL_INDICE_ID
 INNER JOIN AVAL_RESPOSTA AR2 ON AR2.ID=AR.AVAL_RESPOSTA_ID
@@ -65,25 +61,14 @@ $reports = mysqli_num_rows($cnx);
 $x = 1;
 
 while ($data = $cnx->fetch_array()) {
-	$avgFeed = ($data["F_COM"] + $data["F_PRO"] + $data["F_DES"])/3;
-	
+	$avgFeedL = ($data["FL_COM"] + $data["FL_PRO"] + $data["FL_DES"])/3;
+	$avgFeedT = ($data["FT_COM"] + $data["FT_PRO"] + $data["FT_DES"])/3;
 	if ($avgFeed > 0) {
 		$leaderGeneral = ($data["LIDER_AVAL_TEC"]+$data["LIDER_AVAL_COM"]+$avgFeed)/3;
 		$autoGeneral = ($data["AUTO_AVAL_TEC"]+$data["AUTO_AVAL_COM"]+$avgFeed)/3;
 	} else {
 		$leaderGeneral = ($data["LIDER_AVAL_TEC"]+$data["LIDER_AVAL_COM"])/2;
 		$autoGeneral = ($data["AUTO_AVAL_TEC"]+$data["AUTO_AVAL_COM"])/2;
-	}
-
-	$position = $positions;//Percorrer vetor p/ encontrar a posição entre os demais.
-	$o = 0;
-	
-	while ($o < $positions) {
-		if ($ranking[$o] == $data["USUARIO_ID"]) {
-			$position = $o+1;
-			break;
-		}
-		$o++;
 	}
 
 	$cnx2 = mysqli_query($phpmyadmin,"SELECT ATIVIDADE_ID, A.NOME, COUNT(ATIVIDADE_ID) AS VEZES FROM DESEMPENHO D INNER JOIN ATIVIDADE A ON A.ID = D.ATIVIDADE_ID WHERE ANO_MES >= DATE_SUB('".$data["REGISTRO"]."', INTERVAL 6 MONTH) AND ANO_MES <= '".$data["REGISTRO"]."' AND USUARIO_ID = ".$data["USUARIO_ID"]." GROUP BY ATIVIDADE_ID" );
@@ -121,7 +106,7 @@ while ($data = $cnx->fetch_array()) {
 	}
 
 	$c1 = mysqli_query($phpmyadmin, "SELECT A.ACESSOS, A.MES, A.ANO_MES FROM (SELECT SUM(ACESSO) AS ACESSOS, DATE_FORMAT(ULTIMO_LOGIN , '%b') AS MES, DATE_FORMAT(ULTIMO_LOGIN , '%Y-%m') AS ANO_MES FROM ACESSO 
-WHERE USUARIO_ID = ".$data["USUARIO_ID"]." AND DATE_FORMAT(ULTIMO_LOGIN , '%Y-%m')<='".$yearMonth."'
+WHERE USUARIO_ID IN(SELECT ID FROM USUARIO WHERE GESTOR_ID=".$data["USUARIO_ID"].") AND DATE_FORMAT(ULTIMO_LOGIN , '%Y-%m')<='".$yearMonth."'
 GROUP BY ANO_MES ORDER BY ANO_MES DESC LIMIT 6) AS A ORDER BY 3;");
 	
   	$i = 0;
@@ -138,7 +123,7 @@ GROUP BY ANO_MES ORDER BY ANO_MES DESC LIMIT 6) AS A ORDER BY 3;");
 
 	$a2 = mysqli_query($phpmyadmin, "SELECT A.MEDIA, A.REGISTRO FROM (SELECT ROUND(AVG(D.DESEMPENHO),2) AS MEDIA, REGISTRO FROM DESEMPENHO D 
 INNER JOIN USUARIO U ON U.ID=D.USUARIO_ID 
-WHERE U.GESTOR_ID=99 AND PRESENCA_ID NOT IN(3,5) GROUP BY REGISTRO ORDER BY REGISTRO DESC LIMIT 264) AS A
+WHERE U.GESTOR_ID=".$data["USUARIO_ID"]." AND PRESENCA_ID NOT IN(3,5) GROUP BY REGISTRO ORDER BY REGISTRO DESC LIMIT 264) AS A
 ORDER BY A.REGISTRO;");
 	$a2_activitys = mysqli_num_rows($a2);
 	
@@ -190,46 +175,59 @@ ORDER BY A.REGISTRO;");
 		</tr>
 		
 	</table>
-			<?php 
-		echo "<img src='http://chart.apis.google.com/chart?chs=300x150&cht=p&chd=".$occurrence."&chl=".$activity."&chtt=Distribuição+das+atividades' width='33%' height='30%'>";
-	
-		echo "<img src='http://chart.apis.google.com/chart?chs=300x150&cht=bvg&chxt=x,y&chm=N,000000,0,-1,11&chd=".$b1_avg."&chl=".$b1_month."&chds=a&chtt=Desempenho+no+mês' width='33%' height='30%'>";
+	<?php 
+		echo "<img src='http://chart.apis.google.com/chart?chs=400x100&cht=bvg&chxt=x,y&chm=N,000000,0,-1,11&chd=".$b1_avg."&chl=".$b1_month."&chds=a&chtt=Desempenho+no+mês' width='50%' height='20%'>";
 
-		echo "<img src='http://chart.apis.google.com/chart?chs=300x150&cht=lc&chxt=x,y&chm=N,000000,0,-1,11&chd=".$c1_access."&chl=".$c1_month."&chds=a&chtt=Acessos+no+sistema' width='33%' height='30%'>"; 
+		echo "<img src='http://chart.apis.google.com/chart?chs=300x150&cht=lc&chxt=x,y&chm=N,000000,0,-1,11&chd=".$c1_access."&chl=".$c1_month."&chds=a&chtt=Acessos+do+time' width='33%' height='30%'>"; 
 	?>
-	<table class="table is-bordered pricing__table is-fullwidth borda">	
+	<table class="table is-bordered pricing__table is-fullwidth borda">
+		<tr><td colspan="7"><b><center>Assiduidade do Time</center></b></td></tr>	
 		<tr>
 			<td><b>Folga's</b></td>
 			<td><b>Atestado's</b></td>
 			<td><b>Falta's</b></td>
-			<td><b>Penalidade's</b></td>
+			<td><b>Pena</b></td>
+			<td><b>Time</b></td>
+			<td><b>M</b></td>
+			<td><b>F</b></td>
 		</tr>
 		<tr>
 			<td><?php echo $data["FOLGA"]; ?></td>
 			<td><?php echo $data["ATESTADO"]; ?></td>
 			<td><?php echo $data["FALTA"]; ?></td>
 			<td><?php echo $data["PENALIDADE"]; ?></td>
+			<td><?php echo $data["EQUIPE"]; ?></td>
+			<td><?php echo $data["M"]; ?></td>
+			<td><?php echo $data["F"]; ?></td>
 		</tr>
 	</table>
 	<table class="table is-bordered pricing__table is-fullwidth borda">
-		<tr class="grey"><td colspan="4"><b><center>Desempenho</center></b></td></tr>	
+		<tr class="grey">
+			<td colspan="3"><b><center>Desempenho</center></b></td>
+			<td colspan="3"><b><center>Idade</center></b></td>
+		</tr>	
 		<tr>
 			<td><b>Mínimo</b></td>
 			<td><b>Média</b></td>
 			<td><b>Máximo</b></td>
-			<td><b>Posição</b></td>
+			<td><b>Mínimo</b></td>
+			<td><b>Média</b></td>
+			<td><b>Máximo</b></td>
 		</tr>
 		<tr>
 			<td><?php echo $data["MINIMO"]; ?></td>
 			<td><?php echo $data["MEDIA"]; ?></td>
 			<td><?php echo $data["MAXIMO"]; ?></td>
-			<td><?php echo $position."/".$positions; ?></td>
+			<td><?php echo $data["MINIMO"]; ?></td>
+			<td><?php echo $data["MEDIA"]; ?></td>
+			<td><?php echo $data["MAXIMO"]; ?></td>
 		</tr>
 		
 	</table>
-	<img src="http://chart.apis.google.com/chart?chs=700x150&cht=ls&chxt=x,y&chd=<?php echo $a2_avg?>&chl=&chds=a&chtt=Performance+da+equipe+liderada+nos+últimos+12+meses+-+média+diaria+de+<?php echo $a2_activitys?>+dias" width="100%" height="30%">
+	<img src="http://chart.apis.google.com/chart?chs=700x150&cht=ls&chxt=x,y&chd=<?php echo $a2_avg?>&chl=&chds=a&chtt=Performance+do+time+liderado+nos+últimos+12+meses+-+média+diaria+de+<?php echo $a2_activitys?>+dias" width="100%" height="30%">
 	<table class="table is-bordered pricing__table is-fullwidth borda">
 		<tr class="grey">
+			<td rowspan="2"><b><center>Amostra</center></b></td>
 			<td colspan="2"><b><center>Feedback</center></b></td>
 			<td colspan="2"><b><center>Solicitado</center></b></td>
 		</tr>	
@@ -240,6 +238,14 @@ ORDER BY A.REGISTRO;");
 			<td><b>Recebidos</b></td>
 		</tr>
 		<tr>
+			<td><b>Líder</b></td>
+			<td><?php echo $data["F_ENV"]; ?></td>
+			<td><?php echo $data["F_REC"]; ?></td>
+			<td><?php echo $data["S_ENV"]; ?></td>
+			<td><?php echo $data["S_REC"]; ?></td>
+		</tr>
+		<tr>
+			<td><b>Time</b></td>
 			<td><?php echo $data["F_ENV"]; ?></td>
 			<td><?php echo $data["F_REC"]; ?></td>
 			<td><?php echo $data["S_ENV"]; ?></td>
@@ -248,18 +254,27 @@ ORDER BY A.REGISTRO;");
 		
 	</table>
 	<table class="table is-bordered pricing__table is-fullwidth borda">
-		<tr class="grey"><td colspan="4"><b><center>Pilares Feedback</center></b></td></tr>	
+		<tr class="grey"><td colspan="5"><b><center>Pilares Feedback</center></b></td></tr>	
 		<tr>
+			<td><b>Amostra</b></td>
 			<td><b>Comportamental</b></td>
 			<td><b>Profissional</b></td>
 			<td><b>Desempenho</b></td>
 			<td><b>Geral</b></td>
 		</tr>
 		<tr>
-			<td><?php echo $data["F_COM"]; ?></td>
-			<td><?php echo $data["F_PRO"]; ?></td>
-			<td><?php echo $data["F_DES"]; ?></td>
-			<td><?php echo $avgFeed; ?></td>
+			<td><b>Líder</b></td>
+			<td><?php echo $data["FL_COM"]; ?></td>
+			<td><?php echo $data["FL_PRO"]; ?></td>
+			<td><?php echo $data["FL_DES"]; ?></td>
+			<td><?php echo $avgFeedL; ?></td>
+		</tr>
+		<tr>
+			<td><b>Time</b></td>
+			<td><?php echo $data["FT_COM"]; ?></td>
+			<td><?php echo $data["FT_PRO"]; ?></td>
+			<td><?php echo $data["FT_DES"]; ?></td>
+			<td><?php echo $avgFeedT; ?></td>
 		</tr>
 	</table>	
 	<br>	
@@ -272,15 +287,15 @@ ORDER BY A.REGISTRO;");
 		</tr>
 		<tr class="center">
 			<td><b>Líder</b></td>
-			<td><?php echo $data["LIDER_AVAL_TEC"]; ?></td>
-			<td><?php echo $data["LIDER_AVAL_COM"]; ?></td>
-			<td><?php echo round($leaderGeneral,2); ?></td>
-		</tr>
-		<tr>
-			<td><b>Auto</b></td>
 			<td><?php echo $data["AUTO_AVAL_TEC"]; ?></td>
 			<td><?php echo $data["AUTO_AVAL_COM"]; ?></td>
 			<td><?php echo round($autoGeneral,2); ?></td>
+		</tr>
+		<tr>
+			<td><b>Time</b></td>
+			<td><?php echo $data["LIDER_AVAL_TEC"]; ?></td>
+			<td><?php echo $data["LIDER_AVAL_COM"]; ?></td>
+			<td><?php echo round($leaderGeneral,2); ?></td>
 		</tr>	
 	</table>
 	<?php 
@@ -324,7 +339,7 @@ WHERE AI.USUARIO_ID = " . $data["USUARIO_ID"].  " AND AI.AVALIACAO_POR = " . $da
 		$a3_note_leader = "t:0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
 	}
 
-	echo "<img src='http://chart.apis.google.com/chart?chs=700x127&cht=ls&chxt=x,x,y,y&chd=".$a3_note_leader.$a3_note."&chco=0000FF,00FF00&chl=$a3_num&chxl=1:|Primeira+Pergunta|Última+Pergunta|3:|Desenvolver|Excelente&chdl=Líder|Auto&chds=a&chtt=Visão+geral+da+avaliação' width='100%' height='25%'>
+	echo "<img src='http://chart.apis.google.com/chart?chs=700x127&cht=ls&chxt=x,x,y,y&chd=".$a3_note_leader.$a3_note."&chco=0000FF,00FF00&chl=$a3_num&chxl=1:|Primeira+Pergunta|Última+Pergunta|3:|Desenvolver|Excelente&chdl=Time|Líder&chds=a&chtt=Visão+geral+da+avaliação' width='100%' height='25%'>
 	<div>
 	<table class='table is-bordered pricing__table is-fullwidth borda'>";
 	
