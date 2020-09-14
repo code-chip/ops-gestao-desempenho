570c2bdcc1b73fcc988448c5b02eb36e92c4242e
Code Chip <willvix@outlook.com>
2020-09-14 12:26:05 -0300

    Add class is-fullwidth in button Consultar.

--- report.php
+++ report.php
@@ -336,12 +336,12 @@ if ($contador != 0): ?>
 			echo "<td>" . $vtNome[$i] . "</td>";
 			
 			if ($registro > 1 && $repeat != 0 && $mesclaa == false) {
-				echo "<td rowspan='" . $registro . "'><a href='report-detailed.php?periodo=" . $periodo . "&idUsuario=" . $vtIdUsuario[$i] . "' target='_blank'><button class='button is-primary is-size-7-touch'>Consultar</button></a></td>";
+				echo "<td rowspan='" . $registro . "'><a href='report-detailed.php?periodo=" . $periodo . "&idUsuario=" . $vtIdUsuario[$i] . "' target='_blank'><button class='button is-primary is-size-7-touch is-fullwidth'>Consultar</button></a></td>";
 				$mesclaa = true;
 			}
 
 			if ($repeat == 0 && $vtNome[$i-1] != $vtNome[$i]) {
-				echo "<td width='1'><a href='report-detailed.php?periodo=" . $periodo . "&idUsuario=" . $vtIdUsuario[$i] . "' target='_blank'><button class='button is-primary is-size-7-touch'>Consultar</button></a></td>"; 
+				echo "<td width='1'><a href='report-detailed.php?periodo=" . $periodo . "&idUsuario=" . $vtIdUsuario[$i] . "' target='_blank'><button class='button is-primary is-size-7-touch is-fullwidth'>Consultar</button></a></td>"; 
 				$mesclaa = false;
 			}
 			
