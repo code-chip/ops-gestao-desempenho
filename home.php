54e72437a9878ee86c927ba8d1aa482bed10c819
Code Chip <willvix@outlook.com>
2020-09-23 17:08:35 -0300

    Code optimazation.

--- home.php
+++ home.php
@@ -1,25 +1,24 @@
 <?php 
-$menuAtivo="inicio";
-include('menu.php');
-$n=rand(1,25);
-$img="img/wallpaper/data-science".$n."-min.jpg";
-//print_r($_SESSION);exit();
-$query="SELECT MAX(DATE_FORMAT(ULTIMO_LOGIN,'%Y-%m-%d')) ULTIMO_LOGIN, MAX(ACESSO_TOTAL) TOTAL FROM ACESSO WHERE USUARIO_ID=".$_SESSION["userId"].";";
-$acesso=mysqli_query($phpmyadmin, $query);
-$cnx=$acesso->fetch_array();
-$acesso=$cnx["TOTAL"];
-$dataAtual=date_create();
-$ultimoLogin=date_create($cnx["ULTIMO_LOGIN"]);
+
+$menuAtivo = 'inicio';
+require('menu.php');
+
+$n = rand(1,25);
+$img = "img/wallpaper/data-science".$n."-min.jpg";
+
+$acesso=mysqli_query($phpmyadmin, "SELECT MAX(DATE_FORMAT(ULTIMO_LOGIN,'%Y-%m-%d')) ULTIMO_LOGIN, MAX(ACESSO_TOTAL) TOTAL FROM ACESSO WHERE USUARIO_ID=".$_SESSION["userId"].";");
+
+$cnx = $acesso->fetch_array();
+$acesso = $cnx["TOTAL"];
+$dataAtual = date_create();
+$ultimoLogin = date_create($cnx["ULTIMO_LOGIN"]);
 $resultado = date_diff($ultimoLogin, $dataAtual);
-$dias= date_interval_format($resultado, '%a');
+$dias = date_interval_format($resultado, '%a');
 list($nome, $sobrenome)=explode(' ', $_SESSION["nameUser"],2);
 
-$checkFeedback="SELECT ID FROM FEEDBACK WHERE DESTINATARIO_ID=".$_SESSION["userId"]." AND SITUACAO='Aprovado';";
-$cnx2=mysqli_query($phpmyadmin, $checkFeedback);
-$feedReceived=mysqli_num_rows($cnx2);
-$checkRequest="SELECT ID FROM SOLICITACAO WHERE DESTINATARIO_ID=".$_SESSION["userId"]." AND SITUACAO='Enviado';";
-$cnx3=mysqli_query($phpmyadmin, $checkRequest);
-$feedRequest=mysqli_num_rows($cnx3);
+
+$cnx2 = mysqli_query($phpmyadmin, "SELECT COUNT(1) AS FEEDBACK,(SELECT COUNT(1) FROM SOLICITACAO WHERE DESTINATARIO_ID=".$_SESSION["userId"]." AND SITUACAO='Enviado') AS SOLICITACAO FROM FEEDBACK WHERE DESTINATARIO_ID=".$_SESSION["userId"]." AND SITUACAO='Aprovado';");
+$feed = $cnx2->fetch_array();
 
 ?>
 <!DOCTYPE html>
@@ -41,56 +40,58 @@ $feedRequest=mysqli_num_rows($cnx3);
 	  	<div class="hero-body">
 	    	<div class="container">
 		     	<h1 class="title animated bounceInDown"><?php		     		
-		        	if($_SESSION["$visualizou"]==0){
+		        	
+		        	if ($_SESSION["$visualizou"] == 0) {
 		        		echo "Bem vindo";
-		        		$_SESSION["$visualizou"]=1;		        	
-		        	}
-		        	else if($_SESSION["$visualizou"]==1 && date('H', time())>="6" && date('H', time())<="11"){		        		
+		        		$_SESSION["$visualizou"] = 1;		        	
+		        	} else if ($_SESSION["$visualizou"] == 1 && date('H', time()) >= "6" && date('H', time()) <= "11") {		        		
 		        		echo "Bom dia";
-		        		$_SESSION["$visualizou"]=2;
-		        	}
-		        	else if($_SESSION["$visualizou"]==1 && date('H', time())>="12" && date('H', time())<="17"){		        		
+		        		$_SESSION["$visualizou"] = 2;
+		        	} else if ($_SESSION["$visualizou"] == 1 && date('H', time()) >= "12" && date('H', time()) <= "17") {		        		
 		        		echo "Boa tarde";
-		        		$_SESSION["$visualizou"]=2;
-		        	}
-		        	else if($_SESSION["$visualizou"]==1 && date('H', time())>="18" && date('H', time())<="23"){		        		
+		        		$_SESSION["$visualizou"] = 2;
+		        	} else if ($_SESSION["$visualizou"] == 1 && date('H', time()) >= "18" && date('H', time()) <= "23") {		        		
 		        		echo "Boa noite";
-		        		$_SESSION["$visualizou"]=2;
-		        	}
-		        	else if($_SESSION["$visualizou"]==1 && date('H', time())>="00" && date('H', time())<="05"){		        		
+		        		$_SESSION["$visualizou"] = 2;
+		        	} else if ($_SESSION["$visualizou"] == 1 && date('H', time()) >= "00" && date('H', time()) <= "05") {		        		
 		        		echo "Boa madrugada";
 		        		$_SESSION["$visualizou"]=2;
-		        	}
-		        	else{
+		        	} else {
 		        		echo "Olá";
 		        	}
+
 		        	?>
 		      	</h1>
 		      	<h3 class="subtitle animated bounceInRight"><?php
-		      		if($feedReceived>0){?>
+		      		
+		      		if ($feed["FEEDBACK"] > 0) {?>
 						<script>
-							var feedReceived=<?php echo $feedReceived ?>;
-							alert('Atenção! '+feedReceived+' feedback(s) recebido(s).');
+							var feedReceived = <?php echo $feed["FEEDBACK"]; ?>;
+							alert(''+feedReceived+' Feedback(s) recebido(s).');
 						</script><?php
 					}
-					if($feedRequest>0){?>
+
+					if ($feed["SOLICITACAO"] > 0) {?>
 						<script>
-							var feedRequest=<?php echo $feedRequest ?>;
-							alert('Atenção! '+feedRequest+' solicitaçõe(s) de Feedback recebido(s). Clique em Feedback->Consultar para maiores detalhes.');
+							var feedRequest = <?php echo $feed["SOLICITACAO"]; ?>;
+							if (feedRequest == 1) {
+								alert(feedRequest+' Solicitação de Feedback recebida. Leia clicando no item Consultar do menu Feedback.');
+							} else {
+								alert(feedRequest+' Solicitações de Feedback recebida. Leia clicando no item Consultar do menu Feedback.');
+							}
 						</script><?php
 					}
-		      		if($acesso==1){
+		      		
+		      		if ($acesso == 1) {
 		      			echo $nome.", notamos que este é o seu primeiro acesso, espero que goste das novidades.";
-		      		}
-		      		else if($dias>2 && $dias <7){
+		      		} else if ($dias > 2 && $dias < 7) {
 		        		echo "Seu último acesso foi há mais de 3 dias, sentimos sua falta ".$nome."!!";
-		        	}
-		        	else if($dias>6){ 
+		        	} else if ($dias > 6) { 
 		        		echo $nome.", notamos sua ausência de uma semana, nos alegramos com o seu retorno ;).";
-		        	}
-		        	else{
+		        	} else{
 		        		echo $nome;
 		        	}
+
 		        	?>
 		      	</h3>
 	    	</div>
